a256319b4bb9440bdafc0035b9e2985b66507215
Matthew McClintock <msm-oss@mcclintock.net>
2017-09-05 08:33:15 -0700

    tests: ping6: make ping command compatible with multiple versions
    This works with formatted output from different versions of ping
    
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>

--- tests/ping6.py
+++ tests/ping6.py
@@ -11,9 +11,9 @@ from devices import board, wan, lan, wlan, prompt
 class LanDevPing6Router(rootfs_boot.RootFSBootTest):
     '''Device on LAN can ping6 router.'''
     def runTest(self):
-        lan.sendline('\nping6 -i 0.2 -c 20 4aaa::1')
+        lan.sendline('\nping6 -c 20 4aaa::1')
         lan.expect('PING ')
-        lan.expect(' ([0-9]+) received')
+        lan.expect(' ([0-9]+) (packets )?received')
         n = int(lan.match.group(1))
         lan.expect(prompt)
         assert n > 0
@@ -22,9 +22,9 @@ class LanDevPing6WanDev(rootfs_boot.RootFSBootTest):
     '''Device on LAN can ping6 through router.'''
     def runTest(self):
         # Make Lan-device ping Wan-Device
-        lan.sendline('\nping6 -i 0.2 -c 20 5aaa::6')
+        lan.sendline('\nping6 -c 20 5aaa::6')
         lan.expect('PING ')
-        lan.expect(' ([0-9]+) received')
+        lan.expect(' ([0-9]+) (packets )?received')
         n = int(lan.match.group(1))
         lan.expect(prompt)
         assert n > 0
