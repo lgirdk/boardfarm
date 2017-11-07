4dcdbb0f50ea1c36c88ccf70455269e0aea6ded1
anil shetty <adshetty@libertyglobal.com>
2017-11-07 14:33:49 -0800

    tests: libs: installers: add installers for tdkb_e2e tests
    The following were added:
    
    +def install_java(device):
    +def install_telnet_server(device):
    +def install_tcl(device):
    +def install_telnet_client(device):
    +def install_expect(device):
    +def install_wget(device):
    +def install_ftp(device):
    +def install_xampp(device):
    
    Signed-off-by: anil shetty <adshetty@libertyglobal.com>
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>

--- tests/lib/installers.py
+++ tests/lib/installers.py
@@ -113,3 +113,141 @@ def install_python(device):
         device.expect(device.prompt)
         device.sendline('apt-get -o DPkg::Options::="--force-confnew" -y --force-yes install python-pip python-mysqldb')
         device.expect(device.prompt, timeout=60)
+
+def install_java(device):
+    '''Install java if not present.'''
+    device.sendline('\njavac -version')
+    try:
+        device.expect('javac 1.8', timeout=5)
+        device.expect(device.prompt)
+    except:
+        device.expect(device.prompt)
+        device.sendline('echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list')
+        device.expect(device.prompt)
+        device.sendline('echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list')
+        device.expect(device.prompt)
+        device.sendline('apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886')
+        device.expect(device.prompt)
+        device.sendline('apt-get update -y')
+        device.expect(device.prompt)
+        device.sendline('apt-get install oracle-java8-installer -y')
+        device.expect_exact("Do you accept the Oracle Binary Code license terms")
+        device.sendline('yes')
+        device.expect(device.prompt, timeout=60)
+        device.sendline('\njavac -version')
+        try:
+            device.expect('javac 1.8', timeout=5)
+            device.expect(device.prompt)
+        except:
+            device.sendline('sed -i \'s|JAVA_VERSION=8u144|JAVA_VERSION=8u152|\' /var/lib/dpkg/info/oracle-java8-installer.*')
+            device.sendline('sed -i \'s|PARTNER_URL=http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/|PARTNER_URL=http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/|\' /var/lib/dpkg/info/oracle-java8-installer.*')
+            device.sendline('sed -i \'s|SHA256SUM_TGZ="e8a341ce566f32c3d06f6d0f0eeea9a0f434f538d22af949ae58bc86f2eeaae4"|SHA256SUM_TGZ="218b3b340c3f6d05d940b817d0270dfe0cfd657a636bad074dcabe0c111961bf"|\' /var/lib/dpkg/info/oracle-java8-installer.*')
+            device.sendline('sed -i \'s|J_DIR=jdk1.8.0_144|J_DIR=jdk1.8.0_152|\' /var/lib/dpkg/info/oracle-java8-installer.*')
+            device.sendline('apt-get install oracle-java8-installer -y')
+            device.expect(device.prompt, timeout=60)
+
+def install_telnet_server(device):
+    '''Install telnet server if not present.'''
+    device.sendline('\nxinetd -version')
+    try:
+        device.expect('xinetd Version 2.3', timeout=5)
+        device.expect(device.prompt)
+    except:
+        device.expect(device.prompt)
+        device.sendline('apt-get install xinetd telnetd -y')
+        device.expect(device.prompt)
+        device.sendline('echo \"service telnet\" > /etc/xinetd.d/telnet')
+        device.sendline('echo "{" >> /etc/xinetd.d/telnet')
+        device.sendline('echo \"disable = no\" >> /etc/xinetd.d/telnet')
+        device.sendline('echo \"flags = REUSE\" >> /etc/xinetd.d/telnet')
+        device.sendline('echo \"socket_type = stream\" >> /etc/xinetd.d/telnet')
+        device.sendline('echo \"wait = no\" >> /etc/xinetd.d/telnet')
+        device.sendline('echo \"user = root\" >> /etc/xinetd.d/telnet')
+        device.sendline('echo \"server = /usr/sbin/in.telnetd\" >> /etc/xinetd.d/telnet')
+        device.sendline('echo \"log_on_failure += USERID\" >> /etc/xinetd.d/telnet')
+        device.sendline('echo \"}\" >> /etc/xinetd.d/telnet')
+        device.expect(device.prompt)
+        device.sendline('/etc/init.d/xinetd restart')
+        device.expect(device.prompt, timeout=60)
+
+def install_tcl(device):
+    '''Install tcl if not present.'''
+    device.sendline('\necho \'puts $tcl_patchLevel\' | tclsh')
+    try:
+        device.expect('8.6', timeout=5)
+        device.expect(device.prompt)
+    except:
+        device.expect(device.prompt)
+        device.sendline('apt-get install tcl -y')
+        device.expect(device.prompt, timeout=60)
+
+def install_telnet_client(device):
+    '''Install telnet client if not present.'''
+    device.sendline('\ndpkg -l | grep telnet')
+    try:
+        device.expect('The telnet client', timeout=5)
+        device.expect(device.prompt)
+    except:
+        device.expect(device.prompt)
+        device.sendline('apt-get install telnet -y')
+        device.expect(device.prompt, timeout=60)
+
+def install_expect(device):
+    '''Install expect if not present.'''
+    device.sendline('\nexpect -version')
+    try:
+        device.expect('expect version 5', timeout=5)
+        device.expect(device.prompt)
+    except:
+        device.expect(device.prompt)
+        device.sendline('apt-get install expect -y')
+        device.expect(device.prompt, timeout=60)
+
+def install_wget(device):
+    '''Install wget if not present.'''
+    device.sendline('\nwget --version')
+    try:
+        device.expect('GNU Wget 1', timeout=5)
+        device.expect(device.prompt)
+    except:
+        device.expect(device.prompt)
+        device.sendline('apt-get install wget -y')
+        device.expect(device.prompt, timeout=60)
+
+def install_ftp(device):
+    '''Install ftp if not present.'''
+    device.sendline('\ndpkg -l | grep ftp')
+    try:
+        device.expect('classical file transfer client', timeout=5)
+        device.expect(device.prompt)
+    except:
+        device.expect(device.prompt)
+        device.sendline('apt-get install ftp -y')
+        device.expect(device.prompt, timeout=60)
+
+def install_xampp(device):
+    '''Install xampp if not present.'''
+    device.sendline('\n/opt/lampp/lampp --help')
+    try:
+        device.expect('Usage: lampp', timeout=5)
+        device.expect(device.prompt)
+    except:
+        device.expect(device.prompt)
+        device.sendline('wget https://www.apachefriends.org/xampp-files/5.6.20/xampp-linux-x64-5.6.20-0-installer.run')
+        device.expect(device.prompt)
+        device.sendline('chmod +x xampp-linux-x64-5.6.20-0-installer.run')
+        device.expect(device.prompt)
+        device.sendline('./xampp-linux-x64-5.6.20-0-installer.run')
+        device.expect_exact("XAMPP Developer Files")
+        device.sendline('n')
+        device.expect_exact("Is the selection above correct?")
+        device.sendline('y')
+        device.expect_exact("Press [Enter] to continue")
+        device.sendline('')
+        device.expect_exact("Do you want to continue?")
+        device.sendline('y')
+        device.expect(device.prompt, timeout=120)
+        device.sendline('/opt/lampp/lampp restart')
+        device.expect(device.prompt)
+        device.sendline('touch /opt/lampp/htdocs/test.txt')
+        device.expect(device.prompt, timeout=120)
