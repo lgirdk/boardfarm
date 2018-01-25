54824cadaed3008e8dc0bea519b7622559237f61
Matthew McClintock <msm-oss@mcclintock.net>
2018-01-25 14:36:54 -0800

    devices: add support for flashing cougarpark
    This adds support for flashing one cougar park image at a time. It can
    flash an arm or atom image with the -k,kernel parameter
    
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>

--- devices/cougarpark.py
+++ devices/cougarpark.py
@@ -7,28 +7,61 @@
 
 import common
 import openwrt_router
+import pexpect
 
 class CougarPark(openwrt_router.OpenWrtRouter):
     '''
     Intel Cougar Park board
     '''
 
-    #wan_iface = "erouter0"
-    #lan_iface = "brlan0"
+    wan_iface = "erouter0"
+    lan_iface = "brlan0"
 
-    #uprompt = ["U-Boot>"]
-    #uboot_eth = "sms0"
-    #uboot_ddr_addr = "0x1000000"
-    #uboot_net_delay = 0
+    uprompt = ["Shell>"]
+    delaybetweenchar = 0.2
+    uboot_ddr_addr = "0x10000000"
+    uboot_eth = "eth0"
 
-    #def flash_uboot(self, uboot):
-    #    common.print_bold("\n===== Flashing bootloader (and u-boot) =====\n")
+    def wait_for_boot(self):
+        '''
+        Break into Shell.
+        '''
+        # Try to break into uboot
+        self.expect('Remaining timeout:',timeout=30)
+        self.send('\x1B')
+        self.expect('startup.nsh',timeout=30)
+        self.send('\x1B')
+        self.expect_exact('Shell>',timeout=30)
 
-    #def flash_rootfs(self, ROOTFS):
-    #    common.print_bold("\n===== Flashing rootfs =====\n")
+    def setup_uboot_network(self):
+        # line sep for UEFI
+        self.linesep = '\x0D'
+        # required delay for networking to work...
+        self.expect(pexpect.TIMEOUT, timeout=15)
+        self.sendline('ifconfig -c %s' % self.uboot_eth)
+        self.sendline('ifconfig -s %s dhcp' % self.uboot_eth)
+        self.expect_exact('Shell>',timeout=30)
+        self.sendline('ifconfig -l')
+        self.expect_exact('IP address: 192.168.0.')
+        self.expect_exact('Gateway: 192.168.0.1')
+        self.expect_exact('Shell>',timeout=30)
+        self.sendline('ping 192.168.0.1')
+        self.sendline('10 packets transmitted, 10 received, 0% packet loss, time 0ms')
+        self.expect_exact('Shell>',timeout=30)
 
-    #def flash_linux(self, KERNEL):
-    #    common.print_bold("\n===== Flashing linux =====\n")
+    def flash_linux(self, KERNEL):
+        print("\n===== Updating kernel and rootfs =====\n")
+        filename = self.prepare_file(KERNEL)
 
-    #def boot_linux(self, rootfs=None, bootargs=""):
-    #    common.print_bold("\n===== Booting linux for %s on %s =====" % (self.model, self.root_type))
+        self.sendline('tftp -p %s -d 192.168.0.1 %s' % (self.uboot_ddr_addr, filename))
+        self.expect_exact('TFTP  general status Success')
+        self.expect_exact('Shell>',timeout=30)
+
+        self.sendline('update -a A -s %s' % self.uboot_ddr_addr)
+        self.expect_exact('Congrats! Looks like everything went as planned! Your flash has been updated! Have a good day!')
+        self.expect_exact('Shell>',timeout=30)
+
+    def boot_linux(self, rootfs=None, bootargs=None):
+        common.print_bold("\n===== Booting linux for %s on %s =====" % (self.model, self.root_type))
+        self.sendline('bootkernel')
+        self.delaybetweenchar = None
