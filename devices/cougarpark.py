9f8ec52dbfe2bc4d64c70816848f52de9f2fff97
Matthew McClintock <msm-oss@mcclintock.net>
2018-01-31 23:40:30 -0800

    devices: port cougarpark class to use non-hard coded wan ip
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>

--- devices/cougarpark.py
+++ devices/cougarpark.py
@@ -8,6 +8,7 @@
 import common
 import openwrt_router
 import pexpect
+import ipaddress
 
 class CougarPark(openwrt_router.OpenWrtRouter):
     '''
@@ -16,6 +17,9 @@ class CougarPark(openwrt_router.OpenWrtRouter):
 
     wan_iface = "erouter0"
     lan_iface = "brlan0"
+
+    lan_network = ipaddress.IPv4Network(u"192.168.0.0/24")
+    lan_gateway = ipaddress.IPv4Address(u"192.168.0.1")
 
     uprompt = ["Shell>"]
     delaybetweenchar = 0.2
@@ -33,7 +37,8 @@ class CougarPark(openwrt_router.OpenWrtRouter):
         self.send('\x1B')
         self.expect_exact(self.uprompt, timeout=30)
 
-    def setup_uboot_network(self):
+    def setup_uboot_network(self, tftp_server):
+        self.tftp_server_int = tftp_server
         # line sep for UEFI
         self.linesep = '\x0D'
         # required delay for networking to work...
@@ -42,10 +47,11 @@ class CougarPark(openwrt_router.OpenWrtRouter):
         self.sendline('ifconfig -s %s dhcp' % self.uboot_eth)
         self.expect_exact(self.uprompt, timeout=30)
         self.sendline('ifconfig -l')
-        self.expect_exact('IP address: 192.168.0.')
-        self.expect_exact('Gateway: 192.168.0.1')
+        ip_c = str(tftp_server).split('.')
+        self.expect_exact('IP address: %s.%s.%s' % (ip_c[0], ip_c[1], ip_c[2]))
+        self.expect_exact('Gateway: %s' % tftp_server)
         self.expect_exact(self.uprompt, timeout=30)
-        self.sendline('ping 192.168.0.1')
+        self.sendline('ping %s' % tftp_server)
         self.sendline('10 packets transmitted, 10 received, 0% packet loss, time 0ms')
         self.expect_exact(self.uprompt, timeout=30)
 
@@ -53,12 +59,14 @@ class CougarPark(openwrt_router.OpenWrtRouter):
         print("\n===== Updating kernel and rootfs =====\n")
         filename = self.prepare_file(KERNEL)
 
-        self.sendline('tftp -p %s -d 192.168.0.1 %s' % (self.uboot_ddr_addr, filename))
+        self.sendline('tftp -p %s -d %s %s' % (self.uboot_ddr_addr, self.tftp_server_int, filename))
         self.expect_exact('TFTP  general status Success')
-        self.expect_exact(self.uprompt, timeout=30)
+        if 0 == self.expect_exact(['TFTP TFTP Read File status Time out'] + self.uprompt, timeout=60):
+            raise Exception("TFTP timed out")
 
         self.sendline('update -a A -s %s' % self.uboot_ddr_addr)
-        self.expect_exact('Congrats! Looks like everything went as planned! Your flash has been updated! Have a good day!')
+        if 0 == self.expect_exact(['UImage has wrong version magic', 'Congrats! Looks like everything went as planned! Your flash has been updated! Have a good day!']):
+            raise Exception("Image looks corrupt")
         self.expect_exact(self.uprompt, timeout=30)
 
     def boot_linux(self, rootfs=None, bootargs=None):
