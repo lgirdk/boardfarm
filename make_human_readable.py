cfbba7206a4f03840786ef0c1cb6ec0ac565d317
Matthew McClintock <msm-oss@mcclintock.net>
2018-02-06 19:50:23 -0800

    bft: use email template from overlay if it exists
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>

--- make_human_readable.py
+++ make_human_readable.py
@@ -27,8 +27,17 @@ def pick_template_filename():
     Decide which HTML file to use as template for results.
     This allows for different format for different audiences.
     '''
-    templates = {'basic': owrt_tests_dir+"/html/template_results_basic.html",
-                 'full': owrt_tests_dir+"/html/template_results.html"}
+
+    basic = owrt_tests_dir+"/html/template_results_basic.html"
+    full = owrt_tests_dir+"/html/template_results.html"
+    if 'BFT_OVERLAY' in os.environ:
+        if os.path.isfile(os.environ['BFT_OVERLAY'] + "/html/template_results_basic.html"):
+            basic = os.environ['BFT_OVERLAY'] + "/html/template_results_basic.html"
+        if os.path.isfile(os.environ['BFT_OVERLAY'] + "/html/template_results.html"):
+            full = os.environ['BFT_OVERLAY'] + "/html/template_results.html"
+
+    templates = {'basic': basic,
+                 'full': full}
     if os.environ.get('test_suite') == 'daily_au':
         return templates['basic']
     else:
