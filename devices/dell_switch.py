922eb437a3fe5fe8b4beb9b41ba4eab898baa9bb
Matthew McClintock <msm-oss@mcclintock.net>
2018-04-18 15:29:31 -0700

    devices: dell_switch: add some crude support for automating a dell switch
    You can load a basic example config like this:
    
    python ./devices/dell_switch.py 'telnet 172.19.17.180 6048'
    
    It will make private ports (via VLANs on 1-42 ) and trunk ports to those
    VLAN ports on 43-48
    
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>

---
+++ devices/dell_switch.py
@@ -0,0 +1,108 @@
+# Copyright (c) 2018
+#
+# All rights reserved.
+#
+# This file is distributed under the Clear BSD license.
+# The full text can be found in LICENSE in the root directory.
+#!/usr/bin/env python
+
+import pexpect
+import sys
+import base
+
+
+class DellSwitch(base.BaseDevice):
+    '''
+    Connects to and configures a Dell Switch
+    '''
+
+    prompt = ['console>', 'console#', 'console\(config.*\)#']
+
+    def __init__(self,
+                 conn_cmd,
+                 password=''):
+        pexpect.spawn.__init__(self, '/bin/bash', args=['-c', conn_cmd])
+        self.logfile_read = sys.stdout
+        self.password = password
+
+    def connect(self):
+        for i in range(10):
+            self.sendline('exit')
+            if 0 == self.expect([pexpect.TIMEOUT] + self.prompt, timeout=5):
+                self.sendline('enable')
+                if 0 == self.expect(['Password:'] + self.prompt):
+                    self.sendline(self.password)
+                    self.expect(self.prompt)
+                self.sendline('config')
+                self.expect(self.prompt)
+                return
+
+        raise Exception("Unable to get prompt on Dell switch")
+
+    def configure_basic_settings(self):
+        self.sendline('ip address dhcp')
+        self.expect(self.prompt)
+        self.sendline('ip address vlan 4093')
+        self.expect(self.prompt)
+        self.sendline('vlan database')
+        self.expect(self.prompt)
+        self.sendline('vlan 101-148,4093')
+        self.expect(self.prompt)
+        self.sendline('exit')
+        self.expect(self.prompt)
+
+    def configure_eth_private_port(self, port):
+        self.sendline('interface ethernet 1/g%s' % port)
+        self.expect(self.prompt)
+        self.sendline('spanning-tree disable')
+        self.expect(self.prompt)
+        self.sendline('switchport mode general')
+        self.expect(self.prompt)
+        self.sendline('switchport general pvid %s' % (100 + port))
+        self.expect(self.prompt)
+        self.sendline('switchport general ingress-filtering disable')
+        self.expect(self.prompt)
+        self.sendline('switchport forbidden vlan add 1,4093')
+        self.expect(self.prompt)
+        self.sendline('switchport general allowed vlan add %s' % (100 + port))
+        self.expect(self.prompt)
+        self.sendline('exit')
+        self.expect(self.prompt)
+
+    def configure_eth_trunk_port(self, port):
+        self.sendline('interface ethernet 1/g%s' % port)
+        self.expect(self.prompt)
+        self.sendline('switchport mode trunk')
+        self.expect(self.prompt)
+        self.sendline('switchport forbidden vlan add 1')
+        self.expect(self.prompt)
+        self.sendline('switchport trunk allowed vlan add 101-148,4093')
+        self.expect(self.prompt)
+        self.sendline('exit')
+        self.expect(self.prompt)
+
+    def save_running_to_startup_config(self):
+        self.sendline('exit')
+        self.expect(self.prompt)
+        self.sendline('copy running-config startup-config')
+        self.expect(self.prompt)
+        self.sendline('config')
+        self.expect(self.prompt)
+
+if __name__ == '__main__':
+    import time
+
+    dell_switch = DellSwitch(sys.argv[1])
+    dell_switch.connect()
+
+    dell_switch.configure_basic_settings()
+    for i in range(1, 42+1):
+        dell_switch.configure_eth_private_port(i)
+    for i in range(43, 48+1):
+        dell_switch.configure_eth_trunk_port(i)
+
+    print
+    print("Press Control-] to exit interact mode")
+    print("=====================================")
+    dell_switch.interact()
+    print
