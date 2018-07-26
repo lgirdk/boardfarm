9dd2ce4244c42d1a1095796529747ec440bd22e5
Nero_Chen <nero_chen@compalbn.com>
2018-07-26 16:21:55 +0800

    devices: windows_telnet: add function
    add get_ip ,ping ,set_dhcp ,set_fixip function
    
    Signed-off-by: Nero_Chen <nero_chen@compalbn.com>

--- devices/windows_telnet.py
+++ devices/windows_telnet.py
@@ -1,5 +1,5 @@
 
-import sys
+import sys, re
 import base
 import connection_decider
 
@@ -21,6 +21,7 @@ class WindowsTelnet(base.BaseDevice):
 
         self.connection = connection_decider.connection("local_cmd", device=self, conn_cmd=conn_cmd)
         self.connection.connect()
+        self.logfile_read = sys.stdout
         self.linesep = '\r'
 
         self.expect('login: ')
@@ -30,4 +31,40 @@ class WindowsTelnet(base.BaseDevice):
         self.expect(self.prompt)
 
         # Hide login prints, resume after that's done
-        self.logfile_read = sys.stdout
+
+    def get_ip(self, wifi_interface):
+
+        self.sendline('netsh interface ip show config '+wifi_interface)
+
+        self.expect("(.+)>",timeout=30)
+        Wifi_log = self.match.group(1)
+
+        match = re.search('IP Address:\s+([\d.]+)' , str(Wifi_log))
+        if match:
+            return match.group(1)
+        else:
+            return None
+
+    def ping(self, ping_ip ,source_ip=None):
+
+        if source_ip == None :
+            self.sendline('ping '+ping_ip)
+        else:
+            self.sendline('ping -S %s %s' % (source_ip , ping_ip))
+
+        self.expect("(.+)>",timeout=30)
+        Wifi_log = self.match.group(1)
+
+        match = re.search('Reply from .+: bytes=.+ TTL=' , str(Wifi_log))
+        if match:
+            return 'True'
+        else:
+            return 'False'
+
+    def set_dhcp(self , wifi_interface):
+        self.sendline('netsh interface ip set address '+wifi_interface+" dhcp")
+        self.expect(self.prompt)
+
+    def set_fixip(self , wifi_interface, fix_ip, fix_mark, fix_gateway):
+        self.sendline('netsh interface ip set address '+wifi_interface+" static "+fix_ip+" "+fix_mark+" "+fix_gateway+" 1")
+        self.expect(self.prompt)
