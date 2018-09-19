85f8b4067b112493c67fe2d5df3172bed527e736
Matthew McClintock <msm-oss@mcclintock.net>
2018-09-19 16:12:46 -0500

    devices: qemu_openwrt: add qemu device for openwrt
    This let's us boot up OpenWrt images:
    
    $ ./bft -b qemux86-openwrt -r ~/git/openwrt/bin/targets/x86/generic/openwrt-x86-generic-combined-ext4.vmdk
    
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>

---
+++ devices/qemu_openwrt.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2017
+#
+# All rights reserved.
+#
+# This file is distributed under the Clear BSD license.
+# The full text can be found in LICENSE in the root directory.
+
+import qemu
+import ipaddress
+
+class QemuOpenWrt(qemu.Qemu):
+    '''
+    Emulated QEMU board
+    '''
+    model = ("qemux86-openwrt")
+
+    wan_iface = "eth1"
+    lan_iface = "br-lan"
+
+    lan_network = ipaddress.IPv4Network(u"192.168.1.0/24")
+    lan_gateway = ipaddress.IPv4Address(u"192.168.1.1")
