d1a7958e1c0b16b7345e85a234d33b8a61d4aa8f
luke <luke_tseng@compalbn.com>
2018-11-19 16:39:20 +0800

    tests/lib/docsis.py: pr_add_docsis_md5_checK_function
    - md5sum_check(encode_cfg_path_from_tmp_dir)
        return True if the content of cfg is same as original file.
    - you can use as below to check file
        assert self.md5sum_check(cmcfg_path), 'md5sum_check() fail'
    
    Signed-off-by: luke <luke_tseng@compalbn.com>

--- tests/lib/docsis.py
+++ tests/lib/docsis.py
@@ -19,6 +19,8 @@ class docsis:
             return output file name(.txt)
         encode(output_type='cm_cfg')
             return output file name(.cfg or .bin)
+        md5sum_check(encode_cfg_path_from_tmp_dir)
+            return True if the content of cfg is same as original file.
     """
     def __init__(self, file_path):
         self.file_path=file_path
@@ -39,6 +41,7 @@ class docsis:
             cmcfg_path=os.path.join(self.dir_path, cmcfg_name)
             os.system("docsis -e %s /dev/null %s" % (self.file_path, cmcfg_path))
             assert os.path.exists(cmcfg_path)
+            assert self.md5sum_check(cmcfg_path), 'md5sum_check() fail'
 
             return  os.path.join(config.board['station'], cmcfg_name)
         elif '.txt' in self.file and output_type=='mta_cfg':
@@ -49,3 +52,30 @@ class docsis:
             assert os.path.exists(mtacfg_path)
 
             return  os.path.join(config.board['station'], mtacfg_name)
+
+    def md5sum_check(self, encode_cfg_path):
+        assert cmd_exists('md5sum')
+        assert cmd_exists('sed')
+        '''
+        Command by manual
+            ~$ docsis -na -d 9_EU_CBN_Dual-Stack_LG.cfg>txt
+            ~$ sed -i ":a;N;s/\t\/\* Pad \*\/\n//g;$!ba" txt
+            ~$ sed -e ":a;N;s/\t\/\* CmMic .* \*\/\n//g;$!ba" txt
+            ~$ sed -e ":a;N;s/\t\/\* CmtsMic .* \*\/\n//g;$!ba" txt
+            ~$ md5sum txt ../9_EU_CBN_Dual-Stack_LG.txt | awk '{print $1}'
+        '''
+        dir_path=os.path.split(encode_cfg_path)[0]
+
+        os.system("docsis -na -d %s>%s/txt" % (encode_cfg_path, dir_path))
+        os.system('sed -i \":a;N;s/\\t\/\* Pad \*\/\\n//g;$!ba" %s/txt' %dir_path)
+        os.system('sed -i \":a;N;s/\\t\/\* CmMic .* \*\/\\n//g;$!ba" %s/txt' %dir_path)
+        os.system('sed -i \":a;N;s/\\t\/\* CmtsMic .* \*\/\\n//g;$!ba" %s/txt' %dir_path)
+
+        os.system('sed -i \":a;N;s/\\t\/\* Pad \*\/\\n//g;$!ba" %s' %encode_cfg_path.replace('.cfg', '.txt'))
+        os.system('sed -i \":a;N;s/\\t\/\* CmMic .* \*\/\\n//g;$!ba" %s' %encode_cfg_path.replace('.cfg', '.txt'))
+        os.system('sed -i \":a;N;s/\\t\/\* CmtsMic .* \*\/\\n//g;$!ba" %s' %encode_cfg_path.replace('.cfg', '.txt'))
+        md5_req=os.popen("md5sum %s %s | awk \'{print $1}\'" %(encode_cfg_path.replace('.cfg', '.txt'), os.path.join(dir_path, 'txt'))).readlines()
+        if md5_req[0]==md5_req[1]:
+            return True
+        else:
+            return False
