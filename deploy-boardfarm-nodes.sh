9d0256575844384681e6e9c1321f6aaccd12c560
Prasada Reddy <preddy.contractor@libertyglobal.com>
2018-11-20 17:28:40 +0100

    tests: lib: randomMAC.py added
        added a simple function for the creation of a random MAC
        address, with the multicast bit NOT set (first octet, lsb).
        Updated deploy_boardfarm shell script to invoke this python
        module.
    
    Signed-off-by: Prasada Reddy <preddy.contractor@libertyglobal.com>

--- deploy-boardfarm-nodes.sh
+++ deploy-boardfarm-nodes.sh
@@ -7,20 +7,8 @@ OPTS=${4:-"both"} # both, odd, even, odd-dhcp, even-dhcp
 BRINT=br-bft
 BF_IMG=${BF_IMG:-"bft:node"}
 
-random_private_mac () {
-	python - <<END
-import random
-#
-def randomMAC():
-	mac = [ 0x00, 0x16, 0x3e,
-		random.randint(0x00, 0x7f),
-		random.randint(0x00, 0xff),
-		random.randint(0x00, 0xff) ]
-	return ':'.join(map(lambda x: "%02x" % x, mac))
-#
-print randomMAC()
-END
-}
+
+random_private_mac () { m=`python  boardfarm/tests/lib/randomMAC.py`; echo $m ;}
 
 local_route () {
 	# TODO: This is a problem if the router network matches the host network
