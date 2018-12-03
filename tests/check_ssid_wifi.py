d8c95f8f3a3d16357579a565c16bad491072eb51
Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
2018-12-03 19:46:23 +0530

    tests:check_ssid_wifi: Added the testcase for wifi connectivity
    Signed-off-by: Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>

--- tests/check_ssid_wifi.py
+++ tests/check_ssid_wifi.py
@@ -14,34 +14,79 @@ from lib.logging import logfile_assert_message
 
 class scan_ssid_wifi(rootfs_boot.RootFSBootTest):
     log_to_file = ""
+    ssid_name = None
+    pass_word = None
 
-    def WifiTest(self,ssid_name,pass_word):
+    def setupTest(self):
+        try:
+            self.ssid_name = self.config.board['common']['ssid_name']
+            self.pass_word = self.config.board['common']['pass_word']
+        except:
+            logfile_assert_message (self, False, "ssid_name and password must be specified in the .json 'common' section")
+
+    def runTest(self):
+
+        self.setupTest()
+
         board.expect(pexpect.TIMEOUT, timeout=50)
-
         """"Checking wifi connectivity"""
         wifi_name = ['wifi_2G','wifi_5G']
         for wifi_device in wifi_name:
             '''Scanning for SSID'''
             output = WifiScan(self).runTest()
             '''Matching the unique SSID'''
-            match = re.search(ssid_name,output)
+            match = re.search(self.ssid_name,output)
             logfile_assert_message(self, match!=None,'SSID value check in WLAN container')
 
             '''Checking for interface status'''
-            wlan.link_up()
+            link = wlan.link_up()
+            if match==None:
+                wlan.sendline("ip link set wlan up")
+                wlan.expect(prompt)
+                logfile_assert_message(self, True,'Setting Link up')
+            else:
+                logfile_assert_message(self, True,'Link is up')
 
             '''Generate WPA supplicant file and execute it'''
-            wlan.wpa_connect(ssid_name,pass_word)
+            conn_wpa = wlan.wpa_connect(self.ssid_name,self.pass_word)
+            logfile_assert_message(self, conn_wpa!=None,'WPA supplicant initiation')
 
             '''Check wlan connectivity'''
-            wlan.wlan_connect(ssid_name)
+            board.expect(pexpect.TIMEOUT, timeout=20)
+            conn_wlan = wlan.wlan_connect(self.ssid_name)
+            logfile_assert_message(self, conn_wlan!=None,'Connection establishment in WIFI')
+            wlan.sendline("dhclient -v wlan1")
+            wlan.expect(prompt,timeout=40)
 
             '''Ping gateway test'''
-            wlan.ping_gateway()
+            wlan.sendline("ifconfig wlan1")
+            wlan.expect(prompt)
+            if_config_res = wlan.before
+            match = re.search(r'inet ([0-9]+\.[0-9]+\.[0-9]+)\.([0-9]+).*',if_config_res)
+            logfile_assert_message(self, match!=None,'Ifconfig wlan IP fetch')
+            gateway_ip = match.group(1) + ".1"
+            ip = match.group(2)
+            value = range(2,255)
+            match = re.search(ip,str(value))
+            logfile_assert_message(self, match!=None,'Ifconfig wlan IP verify')
 
-            '''Ping hostname test'''
-            wlan.ping_hostname()
+            for host in {gateway_ip,"google.com"}:
+                wlan.sendline("ping -c 1 %s" % host)
+                wlan.expect(prompt)
+                ping_res = wlan.before
+                match = re.search('1 packets transmitted, 1 received, 0% packet loss' , ping_res)
+                logfile_assert_message(self, match!=None,'Ping status')
 
             '''Kill the wpa supplicant'''
             wlan.kill_supplicant()
+
+            self.cleanupTest()
+
+    def cleanupTest(self):
+        self.recover()
+        logfile_assert_message(self, True, "Nothing to do with cleanup")
+
+    def recover(self):
+        '''Kill the wpa supplicant'''
+        wlan.kill_supplicant()
 
