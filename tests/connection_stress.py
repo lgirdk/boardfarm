cb10ff51933c102f30e0578d2ee5f089e29c2b8a
Matthew McClintock <msm-oss@mcclintock.net>
2019-02-04 08:49:16 -0600

    tests: conn_stress: update ab test
    Collects stats, starts web server, uses non-hard coded IPs, more
    graceful recovery
    
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>

--- tests/connection_stress.py
+++ tests/connection_stress.py
@@ -9,37 +9,42 @@ import rootfs_boot
 import time
 from devices import board, wan, lan, wlan, prompt
 
+from lib.installers import install_lighttpd
+
 class Connection_Stress(rootfs_boot.RootFSBootTest):
     '''Measured CPU use while creating thousands of connections.'''
+
+    concurrency = 25
+    num_conn = 5000
+    reqs_per_sec = 0
+
     def runTest(self):
-        num_conn = 5000
+        install_lighttpd(wan)
+        wan.sendline('/etc/init.d/lighttpd start')
+        wan.expect(prompt)
         # Wan device: Create small file in web dir
         fname = 'small.txt'
         cmd = '\nhead -c 10000 /dev/urandom > /var/www/%s' % fname
         wan.sendline(cmd)
         wan.expect(prompt)
         # Lan Device: download small file a lot
-        concurrency = 25
-        url = 'http://192.168.0.1/%s' % fname
+        url = 'http://%s/%s' % (wan.gw, fname)
         # Start CPU monitor
-        board.sendline('\nmpstat -P ALL 10000 1')
+        board.collect_stats(stats=['mpstat'])
         # Lan Device: download small file a lot
-        lan.sendline('\nab -dn %s -c %s %s' % (num_conn, concurrency, url))
+        lan.sendline('\nab -dn %s -c %s %s' % (self.num_conn, self.concurrency, url))
         lan.expect('Benchmarking', timeout=5)
-        lan.expect('Requests per second:\s+(\d+)')
-        reqs_per_sec = int(lan.match.group(1))
+        if 0 != lan.expect(['Requests per second:\s+(\d+)', 'apr_socket_recv: Connection reset by peer'], timeout=120):
+            raise Exception("ab failed to run")
+        self.reqs_per_sec = int(lan.match.group(1))
         lan.expect(prompt)
-        # Stop CPU monitor
-        board.sendcontrol('c')
-        board.expect('Average:\s+all(\s+[0-9]+.[0-9]+){10}\r\n')
-        idle_cpu = float(board.match.group(1))
-        avg_cpu = 100 - float(idle_cpu)
-        board.expect(prompt)
-        msg = "ApacheBench measured %s connections/second, CPU use = %s%%." % (reqs_per_sec, avg_cpu)
-        self.result_message = msg
-        time.sleep(5) # Give router a few seconds to recover
+
+        self.recover()
+
     def recover(self):
-        board.sendcontrol('c')
-        board.expect(prompt)
         lan.sendcontrol('c')
-        time.sleep(2) # Give router a few seconds to recover
+        time.sleep(5) # Give router a few seconds to recover
+        board.parse_stats(dict_to_log=self.logged)
+        avg_cpu = self.logged['mpstat']
+        msg = "ApacheBench measured %s connections/second, CPU use = %s%%." % (self.reqs_per_sec, avg_cpu)
+        self.result_message = msg
