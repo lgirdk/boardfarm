7f90e2ff912d939f766bc07e3dc95bfdb246bd64
Matthew McClintock <msm-oss@mcclintock.net>
2019-02-06 17:58:43 -0600

    tests: iptables: save iptables log to file
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>

--- tests/iptables.py
+++ tests/iptables.py
@@ -1,16 +1,20 @@
 import rootfs_boot
 
+import os
+
 from devices import board
 
 class IPTablesDump(rootfs_boot.RootFSBootTest):
     '''Dumps all IPTables rules with stats'''
     def runTest(self):
         pp = board.get_pp_dev()
-        for tbl in ['filter', 'nat', 'mangle', 'raw', 'security']:
-            pp.sendline('iptables -n -t %s -L -v; echo DONE' % tbl)
-            pp.expect_exact('echo DONE')
-            pp.expect_exact('DONE')
-            pp.expect(pp.prompt)
+        with open(os.path.join(self.config.output_dir, 'iptables.log'), 'w') as ipt_log:
+            for tbl in ['filter', 'nat', 'mangle', 'raw', 'security']:
+                pp.sendline('iptables -n -t %s -L -v; echo DONE' % tbl)
+                pp.expect_exact('echo DONE')
+                pp.expect_exact('DONE')
+                ipt_log.write(pp.before)
+                pp.expect(pp.prompt)
 
 class IPTablesFlushMangle(rootfs_boot.RootFSBootTest):
     '''Flushes mangle table'''
