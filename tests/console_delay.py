551438eb2f1384a9feb0dabd2f1e49a88a3efa91
Matthew McClintock <msm-oss@mcclintock.net>
2019-02-11 12:59:27 -0600

    tests: console_delay: apply delay to all consoles
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>

--- tests/console_delay.py
+++ tests/console_delay.py
@@ -6,11 +6,18 @@ class DelayBetweenChar(rootfs_boot.RootFSBootTest):
     def runTest(self):
         board.delaybetweenchar = 0.1
 
+        for console in board.consoles:
+            console.delaybetweenchar = 0.1
+
 class DelayBetweenCharExtreme(rootfs_boot.RootFSBootTest):
     def runTest(self):
         board.delaybetweenchar = 1
+        for console in board.consoles:
+            console.delaybetweenchar = 1
 
 class NoDelayBetweenChar(rootfs_boot.RootFSBootTest):
     def runTest(self):
         board.delaybetweenchar = None
+        for console in board.consoles:
+            console.delaybetweenchar = None
 
