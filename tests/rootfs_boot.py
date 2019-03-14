ca4d8a675e54d41b206e9a5816f30374bb54af28
Matthew McClintock <msm-oss@mcclintock.net>
2019-03-14 18:01:19 -0500

    tests: rootfs_boot: fix logic for checking IP address
    We can't run the assert if we don't have an IP, we should just loop
    again
    
    Change-Id: I889f90887f853faf45aad764a56d1c0deb95a856
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>

--- tests/rootfs_boot.py
+++ tests/rootfs_boot.py
@@ -192,8 +192,10 @@ class RootFSBootTest(linux_boot.LinuxBootTest):
                     try:
                         ip = board.get_interface_ipaddr(board.wan_iface)
                     except:
-                        assert ipaddress.IPv4Address(ip.decode('utf-8')) in prov.cm_network, \
-                            "Board failed to obtain WAN IP address"
+                        continue
+
+                    assert ipaddress.IPv4Address(ip.decode('utf-8')) in prov.cm_network, \
+                        "Board failed to obtain WAN IP address"
 
                     ips += [ip]
 
@@ -201,17 +203,22 @@ class RootFSBootTest(linux_boot.LinuxBootTest):
                         try:
                             ip = board.get_interface_ipaddr(board.erouter_iface)
                         except:
-                            assert ipaddress.IPv4Address(ip.decode('utf-8')) in prov.open_network, \
-                                "Board failed to obtain erouter IP address"
+                            continue
+
+                        assert ipaddress.IPv4Address(ip.decode('utf-8')) in prov.open_network, \
+                            "Board failed to obtain erouter IP address"
                         ips += [ip]
                     if hasattr(board, 'mta_iface'):
                         try:
                             ip = board.get_interface_ipaddr(board.mta_iface)
                         except:
-                            assert ipaddress.IPv4Address(ip.decode('utf-8')) in prov.mta_network, \
-                                "Board failed to obtain MTA IP address"
+                            continue
+
+                        assert ipaddress.IPv4Address(ip.decode('utf-8')) in prov.mta_network, \
+                            "Board failed to obtain MTA IP address"
                         ips += [ip]
-                        break
+
+                    break
                 except:
                     if time.time() - start_time < time_for_provisioning:
                         raise
