ed020dc7394dc49fc63f11f592fc7326abf47870
Matthew McClintock <msm-oss@mcclintock.net>
2019-04-01 16:55:51 -0500

    tests: lib: docsis: return a more useful string for CM mode
    This will make parsing logic in tests easier to understand
    
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>
    Change-Id: Icf08e7c380a1cb66e521bfbd8374ac3b76fcc769

--- tests/lib/docsis.py
+++ tests/lib/docsis.py
@@ -161,11 +161,12 @@ class cm_cfg(object):
         '''function to check config mode in CM'''
         '''0-Disable/Bridge, 1-IPv4, 2-IPv6 (DSlite), 3-IPv4 and IPv6(Dual)'''
         modeset = ['0x010100', '0x010101', '0x010102', '0x010103']
+        modestr = ['bridge', 'ipv4', 'dslite', 'dual-stack']
         for mode in range(0, len(modeset)):
             tlv_check = "GenericTLV TlvCode 202 TlvLength 3 TlvValue "+modeset[mode]
             initmode_check = "InitializationMode "+str(mode)
             if (tlv_check in self.txt) or (initmode_check in self.txt):
-                return mode
+                return modestr[mode]
 
     cm_configmode = property(_cm_configmode)
 
