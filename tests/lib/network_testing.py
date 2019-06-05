57ea658d6c8e3f4ff39df493ced01c8641ce1414
Panimalar Palaniappan <ppalaniappan.contractor@libertyglobal.com>
2019-06-05 21:17:31 +0800

    tests: lib: network_testing: Modified function for sudo_sendline
    Modified function for sudo_sendline
    Added tcpdump capture without port
    
    Signed-off-by: Panimalar Palaniappan <ppalaniappan.contractor@libertyglobal.com>
    Change-Id: I7df4d074940d4a48e5ae5af048449d8662cb1469

--- tests/lib/network_testing.py
+++ tests/lib/network_testing.py
@@ -8,39 +8,42 @@
 
 import re
 
-def tcpdump_capture(device, ip_address, port, capture_file):
-    device.sendline("tcpdump -i %s -n \'portrange %s\' -w /tmp/%s &" %(ip_address, port, capture_file))
+def tcpdump_capture(device, interface, port=None, capture_file='pkt_capture.pcap'):
+    if port == None:
+        device.sudo_sendline("tcpdump -i %s -n -w %s &" %(interface, capture_file))
+    else:
+        device.sudo_sendline("tcpdump -i %s -n \'portrange %s\' -w %s &" %(interface, port, capture_file))
     device.expect(device.prompt)
     return device.before
 
 def kill_process(device, process="tcpdump"):
-    device.sendline("killall %s" %process)
+    device.sudo_sendline("killall %s" %process)
     device.expect(device.prompt)
     return device.before
 
 def tcpdump_read(device, capture_file):
-    device.sendline("tcpdump -n -r /tmp/%s" %(capture_file))
+    device.sudo_sendline("tcpdump -n -r %s" %(capture_file))
     device.expect(device.prompt)
     output = device.before
-    device.sendline("rm /tmp/%s" %(capture_file))
+    device.sudo_sendline("rm %s" %(capture_file))
     device.expect(device.prompt)
     return output
 
 def nmap_cli(device, ip_address, port, protocol=None, retry="0"):
     if protocol == "tcp":
-        device.sendline("nmap -sS %s -p %s -Pn -r -max-retries %s" %(ip_address,port,retry))
+        device.sudo_sendline("nmap -sS %s -p %s -Pn -r -max-retries %s" %(ip_address,port,retry))
     elif protocol == "udp":
-        device.sendline("nmap -sU %s -p %s -Pn -r -max-retries %s" %(ip_address,port,retry))
+        device.sudo_sendline("nmap -sU %s -p %s -Pn -r -max-retries %s" %(ip_address,port,retry))
     else:
-        device.sendline("nmap -sS -sU %s -p %s -Pn -r -max-retries %s" %(ip_address,port,retry))
+        device.sudo_sendline("nmap -sS -sU %s -p %s -Pn -r -max-retries %s" %(ip_address,port,retry))
     device.expect(device.prompt,timeout=200)
     return device.before
 
 def ping(device, ping_ip, ping_interface=None, count=4):
     if ping_interface == None:
-        device.sendline("ping -c %s %s"%(count,ping_ip))
+        device.sudo_sendline("ping -c %s %s"%(count,ping_ip))
     else:
-        device.sendline("ping -I %s -c %s %s"%(ping_interface,count,ping_ip))
+        device.sudo_sendline("ping -I %s -c %s %s"%(ping_interface,count,ping_ip))
     device.expect(device.prompt, timeout=50)
     match = re.search("%s packets transmitted, %s received, 0%% packet loss" % (count, count), device.before)
     if match:
