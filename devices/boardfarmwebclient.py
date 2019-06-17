b1ba9e4fffc3392bc9e093d011673396fd01dd4f
Mike Anderson <mbanderson@uwalumni.com>
2019-06-17 14:55:37 -0500

    Add class for interacting with a boardfarm server
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>
    Change-Id: Ia1127c281ce9119ee8b7d92dbd72f1aede672437

---
+++ devices/boardfarmwebclient.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2015
+#
+# All rights reserved.
+#
+# This file is distributed under the Clear BSD license.
+# The full text can be found in LICENSE in the root directory.
+#!/usr/bin/env python
+
+import re
+
+import requests
+from simplejson.errors import JSONDecodeError
+
+class BoardfarmWebClient(object):
+    '''
+    Handles interacting with a boardfarm server. For checking out
+    stations, etc.
+    '''
+
+    def __init__(self, config_url):
+        self.config_url = config_url
+        self.server_url = None
+        self.server_version = None
+        # Test if this is a smart boardfarm server
+        try:
+            self.server_url = re.search('http.*/api', self.config_url).group(0)
+            r = requests.get(self.server_url)
+            data = r.json()
+            self.server_version = data.get('version', None)
+            print("Using %s as boardfarm server, version %s" % (self.server_url, self.server_version))
+        except JSONDecodeError:
+            print("Error: %s does not contain valid JSON." % self.config_url)
+            raise
+        except Exception as e:
+            print("Using '%s' like a plain config file." % self.config_url)
+
+    def checkout(self, name):
+        if not self.server_version:
+            return
+        try:
+            url = self.server_url + "/checkout"
+            requests.post(url, json={"name": name})
+            print("Notified boardfarm server of checkout of %s" % name)
+        except Exception as e:
+            print(e)
+            print("Failed to notify boardfarm server of checkout")
+
+    def checkin(self, name):
+        if not self.server_version:
+            return
+        try:
+            url = self.server_url + "/checkin"
+            requests.post(url, json={"name": name})
+            print("Notified boardfarm server of checkin of %s" % name)
+        except Exception as e:
+            print(e)
+            print("Failed to notify boardfarm server of checkin")
+
+if __name__ == '__main__':
+    bf_config = "http://boardfarm.myexamplesite.com/api/bf_config"
+    bfweb = BoardfarmWebClient(bf_config)
+    bfweb.checkout("mv1-1-1")
+    bfweb.checkin("mv1-1-1")
