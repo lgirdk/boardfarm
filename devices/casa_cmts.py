e440bdd39df2033ef518e56afa6d5403f3918ffe
Michele Gualco <mgualco.contractor@libertyglobal.com>
2019-06-28 08:48:54 +0200

    devices: added base_cmts and arris_cmts
    first stub for premilinary test checks
    check_online: returns true is cm is operational (the status string otherwise)
    clear_offline
    clear_cm_reset
    get_mtaip
    get_cmip
    get_cmipv6
    get_cm_mac_domain
    check_PartialService
    DUT_chnl_lock
    
    Signed-off-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    Change-Id: Iade7607b488c721ebd13e1a3ebdc1b17a40807dd

--- devices/casa_cmts.py
+++ devices/casa_cmts.py
@@ -12,8 +12,9 @@ import base
 import re
 import connection_decider
 from lib.regexlib import ValidIpv6AddressRegex, ValidIpv4AddressRegex, AllValidIpv6AddressesRegex
+import base_cmts
 
-class CasaCMTS(base.BaseDevice):
+class CasaCMTS(base_cmts.BaseCmts):
     '''
     Connects to and configures a CASA CMTS
     '''
@@ -146,6 +147,8 @@ class CasaCMTS(base.BaseDevice):
         return output
 
     def get_mtaip(self, cmmac, mtamac):
+        if ':' in mtamac:
+            mtamac = self.get_cm_mac_cmts_format(mtamac)
         self.sendline('show cable modem %s cpe' % cmmac)
         self.expect('([\d\.]+)\s+dhcp\s+' + mtamac)
         result = self.match.group(1)
@@ -478,17 +481,6 @@ class CasaCMTS(base.BaseDevice):
             return False
         else:
             return True
-
-    # Function:   get_cm_mac_cmts_format(mac)
-    # Parameters: mac        (mac address XX:XX:XX:XX:XX:XX)
-    # returns:    the cm_mac in cmts format xxxx.xxxx.xxxx (lowercase)
-    def get_cm_mac_cmts_format(self, mac):
-        if mac == None:
-            return None
-        # the mac cmts syntax format example is 3843.7d80.0ac0
-        tmp = mac.replace(':', '')
-        mac_cmts_format = tmp[:4]+"."+tmp[4:8]+"."+tmp[8:]
-        return mac_cmts_format.lower()
 
     def check_docsis_mac_ip_provisioning_mode(self, index):
         self.sendline('show interface docsis-mac %s' % index)
