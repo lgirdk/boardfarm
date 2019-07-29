b2879d9dc176d6bcb0ad18c15d907e8547a0e8eb
Mike Anderson <mbanderson@uwalumni.com>
2019-07-29 13:08:25 -0500

    First commit of boardfarm as a module
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>
    Change-Id: Ie30d895d22d2518016a99732acdc0c52c0f4e537

---
+++ boardfarm/zephyr/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2019
+#
+# All rights reserved.
+#
+# This file is distributed under the Clear BSD license.
+# The full text can be found in LICENSE in the root directory.
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='boardfarm',
+      version='1.0.0',
+      description='Automated testing of network devices',
+      author='Matt McClintock & Mike Anderson, and others',
+      url='https://github.com/lgirdk/boardfarm',
+      packages=find_packages(),
+      package_data={'': ['*.txt','*.json','*.cfg','*.md']},
+      include_package_data=True,
+      entry_points = {
+        'console_scripts': ['bft=boardfarm.bft:main'],
+      }
+     )
