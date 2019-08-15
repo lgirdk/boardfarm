0b7720ec35e0c2e74f19bc47211834f9bdf35fb7
Mike Anderson <mbanderson@uwalumni.com>
2019-08-15 14:48:43 -0500

    If boardfarm server gives error, print the msg and quit
    Print error message from the server if it gives us one. For
    example, if the server version is incompatible with our
    version of bft.
    
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>
    Change-Id: Ib978d9138709c7d7c69e2e9bf3d2dfdba4b1a9ac

--- boardfarm/dbclients/boardfarmwebclient.py
+++ boardfarm/dbclients/boardfarmwebclient.py
@@ -16,6 +16,9 @@ import time
 
 import requests
 
+class ServerError(Exception):
+    pass
+
 class BoardfarmWebClient(object):
     '''
     Handles interacting with a boardfarm server. For checking out
@@ -29,6 +32,8 @@ class BoardfarmWebClient(object):
         self.server_url = None
         self.server_version = None
         self.checked_out = None
+        self.bf_config_str = None
+        self.bf_config = None
         # If config isn't on a server, do nothing
         if not config_url.startswith('http'):
             return
@@ -38,6 +43,16 @@ class BoardfarmWebClient(object):
                                          os.environ.get('USER', None),
                              'build_url': os.environ.get('BUILD_URL', None)
                             }
+        try:
+            res = requests.get(self.config_url, headers=self.headers)
+            self.bf_config_str = res.text
+            self.bf_config = res.json()
+            res.raise_for_status()
+        except Exception as e:
+            if self.bf_config:
+                raise ServerError(self.bf_config.get('message', ''))
+            else:
+                raise e
         try:
             # See if this is a boardfarm server by checking the root /api path
             self.server_url = re.search('http.*/api', self.config_url).group(0)
