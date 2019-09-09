42dc0436278c5ba2ee0e23bfed1b088531e429e2
Mike Anderson <mbanderson@uwalumni.com>
2019-09-09 10:19:59 -0500

    Boardfarm: add list of supported_devices
    Just a thing for convience for developers or test writers.
    Also basically allows the code to self-document.
    
    Example use:
    
    >>> import boardfarm
    >>> bf = boardfarm.Boardfarm("")
    >>> bf.supported_device_names
    ['AxirosACS', 'CougarPark', 'DebianBox', 'DebianISCProvisioner', 'DebianWifi',
    'DockerFactory', 'QcomAkroniteRouterNAND', 'QcomAkroniteRouterNOR',
    'QcomDakotaRouterNAND', 'QcomDakotaRouterNOR', 'QcomMipsRouter', 'Qemu',
    'QemuOpenWrt', 'RPI', 'WRT3200ACM', 'WindowsTelnet', 'WindowsWSL']
    
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>
    Change-Id: Ib5ba2d2ba0d62af04b4ceb7acebe5dd419eacf7d
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/369
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Matthew McClintock <matthew@meldnetworks.com>

--- boardfarm/Boardfarm.py
+++ boardfarm/Boardfarm.py
@@ -6,6 +6,8 @@
 # This file is distributed under the Clear BSD license.
 # The full text can be found in LICENSE in the root directory.
 
+import devices
+
 from dbclients import boardfarmwebclient
 
 
@@ -13,6 +15,7 @@ class Boardfarm(object):
     
     def __init__(self, url, debug=False):
         self.server = boardfarmwebclient.BoardfarmWebClient(url, debug=debug)
+        self.supported_devices = self._supported_devices()
 
     def list_tests(self):
         pass
@@ -20,3 +23,10 @@ class Boardfarm(object):
     def list_stations(self):
         for name in self.server.bf_config:
             print("%s" % name)
+
+    def _supported_devices(self):
+        '''Create list of supported device names that can be used in tests.'''
+        devs = []
+        for f in devices.device_mappings:
+            devs += [n.__name__ for n in devices.device_mappings[f]]
+        return sorted(devs)
