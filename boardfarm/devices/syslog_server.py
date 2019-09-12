0a3b55d3d440f791de5772bcd224d70fbdd7ba4f
Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
2019-09-12 18:34:52 +0530

    devices: syslog_server: Moving the syslog server to generic repo
    Made changes  in the class according to the profile
    
    Signed-off-by: Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
    Change-Id: I9969c0bb7bf62f283e1201bbb00b0b68122cb5f3
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/406
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>
    Reviewed-by: Matthew McClintock <matthew@meldnetworks.com>

---
+++ boardfarm/devices/syslog_server.py
@@ -0,0 +1,74 @@
+import pexpect
+import re
+import time
+import datetime
+
+class SyslogServer(object):
+    '''
+    Linux based syslog server
+    '''
+
+    model = ('syslog')
+    profile = {}
+
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+        self.syslog_ip = self.kwargs['ipaddr']
+        self.syslog_path = self.kwargs.get('syslog_path', '/var/log/BF/')
+
+    def __str__(self):
+        return "SyslogServer"
+
+    def remove_syslog_via_ip(self, ip):
+        '''
+        Remove dut syslog on server
+
+        This method is to remove the syslog from the server for the given IP
+
+        Parameters: (string)IP address of the DUT
+        '''
+
+        command="rm %s/log_%s" % (self.syslog_path, ip)
+        self.sendline(command)
+        self.expect_exact(command)
+        self.expect(self.prompt)
+
+    def get_syslog_via_ip(self, ip, n=10):
+        '''
+        Getting the syslog
+
+        This method is to get the syslog from the server
+
+        Parameters: (string)Ip address of the DUT
+                    (int) Number of lines to be returned
+
+        Returns: (string)Syslog messages
+        '''
+
+        command="tail -n %s %slog_%s" % (n, self.syslog_path, ip)
+        req=self.check_output(command)
+        return req
+
+    def check_syslog_time(self, ip, check_string):
+        '''
+        Check for syslog time
+
+        This method is to get the syslog message time in server
+        and the time in DUT for a particular check string
+
+        Parameters: (string)IP address of the DUT
+                    (string):Chcek string or message for which the time to be fetched
+
+        Returns: (string) Time of the syslog message
+        '''
+        log = self.get_syslog_via_ip(ip)
+        match = re.search(".*\s(\d+\:\d+\:\d+).*(%s).*" %check_string, log)
+        time_log_msg = match.group(1)
+
+        # Check syslog time from server
+        x = time.strptime(time_log_msg,'%H:%M:%S')
+        time_log_msg = datetime.timedelta(hours=x.tm_hour,minutes=x.tm_min,seconds=x.tm_sec).total_seconds()
+
+        return time_log_msg
