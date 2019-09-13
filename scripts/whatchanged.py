3b7fe330b3584505d00c8a6aa24ce1b8573ebef3
Mike Anderson <mbanderson@uwalumni.com>
2019-09-13 10:41:28 -0500

    scripts/whatchanged: Output directly and indirectly changed tests
    Child classes of changed tests are now included in the final result.
    
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>
    Change-Id: Ie876d20de9ee61e0abf27ba295f6daf5a148912c
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/453
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Matthew McClintock <matthew@meldnetworks.com>

--- scripts/whatchanged.py
+++ scripts/whatchanged.py
@@ -11,7 +11,7 @@ def get_all_classes_from_code(directories, debug=False):
     Uses 'grep' to find all files of type '.py' in the given directories.
     Then parses those files to return a dict where:
          * keys = class names
-         * values = parent class name
+         * values = list with "parent class name" and "grandparent class name"
     '''
     if debug:
         print("Searching for classes in:")
@@ -27,25 +27,26 @@ def get_all_classes_from_code(directories, debug=False):
             if debug:
                 print("Warning: No tests found in %s" % d)
     raw_text = "".join(raw_text)
-    all_classes = dict(re.findall('class\s(\w+)\(([\w\.]+)\):', raw_text))
+    # Create a list of tuples (classname, parent_classname)
+    result = re.findall('class\s(\w+)\(([\w\.]+)\):', raw_text)
+    #print(result)
+    # Convert that list into a Python dict such that
+    #    {"classname1": [parent_classname1,],
+    #     "classname2": [parent_classname2,], ... etc}
+    # Because we will add parents to that list.
+    all_classes = dict([(x[0], [x[1],]) for x in result])
+    # Add grandparent class
+    for name in all_classes:
+        parent = all_classes[name][0]
+        if parent not in all_classes:
+            continue
+        grandparent = all_classes[parent][0]
+        all_classes[name].append(grandparent)
     if debug:
         print("Found %s python classes." % len(all_classes))
-        #print("All classes and their parent found in code:")
-        #print("\n".join(["%s -> %s" % (k,v) for k,v in sorted(all_classes.iteritems())]))
+        #for name in sorted(all_classes):
+        #    print("%30s: %s" % (name, ", ".join(all_classes[name])))
     return(all_classes)
-
-def replace_parent_with_grandparent(data, no_modify, debug=False):
-    '''
-    Given a dictionary where all keys are class names, and values are parent classes,
-    return a dictionary with parent class names replaced with grandparent class names.
-    '''
-    new_dict = {}
-    for key in data:
-        if data[key] in no_modify:
-            new_dict[key] = data[key]
-            continue
-        new_dict[key] = data.get(data[key], data[key])
-    return new_dict
 
 def changed_classes(directories, start, end, debug=False):
     '''
@@ -65,8 +66,9 @@ def changed_classes(directories, start, end, debug=False):
             if debug:
                 print("Warning: git diff command failed in %s" % d)
     if debug:
-        print("\nAll changed classes from %s to %s:" % (start, end))
-        print("  " + "\n  ".join(result.keys()))
+        print("\nAll directly changed classes from %s to %s:" % (start, end))
+        for name in sorted(result):
+            print("  %s : %s" % (name, result[name]))
     return result
 
 def get_features(directories, start, end, debug=False):
@@ -111,23 +113,35 @@ if __name__ == '__main__':
             d = os.path.join(d, "boardfarm")
         test_code_loc.append(os.path.join(d, "tests", "*.py"))
     git_loc = [os.path.join(x, ".git") for x in args.directories]
-    valid_test_types = ['rootfs_boot.RootFSBootTest']
+    valid_test_types = 'rootfs_boot.RootFSBootTest'
 
+    # Get a dictionary of the form
+    #     {"classname": ["parent_classname", "grandparent_classname"], ... }
     all_classes = get_all_classes_from_code(test_code_loc, debug=args.debug)
-    all_classes = replace_parent_with_grandparent(all_classes,
-                                                  no_modify=valid_test_types,
-                                                  debug=args.debug)
+
+    # Find names of all *directly* changed classes
     all_changed_classes = changed_classes(git_loc,
                                           args.start,
                                           args.end,
                                           debug=args.debug)
+    # Add names of *indirectly* changed classes (child classes of changed classes)
+    indirectly_changed_classes = {}
+    for name, parents in all_classes.iteritems():
+        if parents[0] in all_changed_classes.keys():
+            indirectly_changed_classes[name] = all_classes[name]
+    if args.debug:
+        print("\nAll indirectly changed classes:")
+        print("  " + "\n  ".join(indirectly_changed_classes))
+    all_changed_classes.update(indirectly_changed_classes)
 
     features = get_features(git_loc, args.start, args.end, debug=args.debug)
 
     # Print all valid tests with a '-e ' in front of them for bft
     final_result = []
-    for name in all_changed_classes:
-        parent_type = all_classes.get(name, None)
-        if parent_type in valid_test_types:
+    for name in sorted(all_changed_classes):
+        parents = all_classes.get(name, [])
+        if valid_test_types in parents:
             final_result.append(name)
+    if args.debug:
+        print("\nFinal output including directly and indirectly changed tests:")
     print(" -e ".join([''] + final_result) + " -q ".join([''] + features))
