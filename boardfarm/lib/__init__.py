73d0236059b4202a6f75cc14f68c249aa0becbb1
Mike Anderson <mbanderson@uwalumni.com>
2019-09-24 14:03:31 -0500

    lib: Create find_subdirs() to use for finding devices and tests
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>
    Change-Id: I69456e3a5afb6d95a60d5392993c72e376fcfdc9

--- boardfarm/lib/__init__.py
+++ boardfarm/lib/__init__.py
@@ -4,8 +4,12 @@
 #
 # This file is distributed under the Clear BSD license.
 # The full text can be found in LICENSE in the root directory.
+
+import glob
+import os
+
 import unittest2
-import common
+
 
 def expectedFailureIf(test):
     def wrap(func):
@@ -18,3 +22,22 @@ def expectedFailureIf(test):
             return func(self)
         return wrapped
     return wrap
+
+def find_subdirs(directories, name):
+    '''
+    Given a list of directories, find subdirectories with the given name.
+    This is useful for finding devices and tests in boardfarm overlays.
+
+    Note: This will only look in the base dir and then one subdirectory deep.
+    '''
+    result = []
+    for d in directories:
+        tmp = glob.glob(os.path.join(d, name)) + \
+              glob.glob(os.path.join(d, '*', name))
+        result += [os.path.join(d, x) for x in tmp]
+        if len(tmp) > 1:
+            # By design there shouldn't be more than one "devices" or "tests" directory in
+            # a given boardfarm project
+            print("WARNING: Multiple directories of the name %s found in %s." % (name, d))
+            print("All will be used to find python classes")
+    return result
