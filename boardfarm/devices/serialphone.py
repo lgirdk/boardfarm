ccf9780b68000cc1dc38aa46e418c192cc0254a4
Nathuriga Sevvel <nsevvel.contractor@libertyglobal.com>
2019-10-01 14:44:17 +0530

    devices: serialphone: serialphone profile is added
    Signed-off-by: Nathuriga Sevvel <nsevvel.contractor@libertyglobal.com>
    Change-Id: I25b17ff72e59ca3d5f292c771c646aecbbc2a825
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/781
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Matthew McClintock <matthew@meldnetworks.com>

---
+++ boardfarm/devices/serialphone.py
@@ -0,0 +1,31 @@
+class SerialPhone(object):
+    '''
+    Fax modem
+    '''
+
+    model = ('serialmodem')
+    profile = {}
+
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+        self.line = self.kwargs.get("line")
+
+    def __str__(self):
+        return "serialmodem %s"% self.line
+
+    def phone_config(self):
+        '''
+        to configure system link/soft link
+        '''
+        self.sendline("ln -s /dev/tty%s  /root/line-%s" % (self.line,self.line))
+        self.expect(["File exists"] + self.prompt)
+    
+    def phone_unconfig(self):
+        '''
+        to remove the system link
+        '''
+        self.sendline("rm  /root/line-%s" % self.line)
+        self.expect(self.prompt)
+
