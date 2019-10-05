194856a602d43931e6360f8f49eacf5b4fc3d7a7
luke <luke_tseng@compalbn.com>
2019-10-05 17:40:04 +0800

    boardfarm: lib: wifi_snmp.py: modify apply_changes function for wifi_snmp
    - modify apply_changes step and all call function line.
    - boardfarm-cbn need to modify all *.py file.
        - https://cicd.lgirdk.com/gerrit/c/cbn/boardfarm-cbn/+/856
        - https://cicd.lgirdk.com/gerrit/c/cbn/boardfarm-cbn/+/892
    
    Change-Id: I0a24c8ba9a670841b276086b8d6982ed964b421f
    Signed-off-by: luke <luke_tseng@compalbn.com>
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/854
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm/lib/wifi_snmp.py
+++ boardfarm/lib/wifi_snmp.py
@@ -16,7 +16,7 @@ class wifi_snmp(wifi_stub):
         index = self.mib_value["mib_index"][wifi_mode]
         mib_out=snmp_mib_set(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Wifi_enable"],index,"i","1")
         if self.apply_changes_no_delay:
-            self.apply_changes(wifi_mode)
+            self.apply_changes()
             mib_out=snmp_mib_get(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Wifi_enable"],index)
         assert mib_out=="1", "Mib query return value for enable wifi: %s" % mib_out
 
@@ -24,7 +24,7 @@ class wifi_snmp(wifi_stub):
         index = self.mib_value["mib_index"][wifi_mode]
         mib_out=snmp_mib_set(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["SSID_set"],index,"s",ssid_name)
         if self.apply_changes_no_delay:
-            self.apply_changes(wifi_mode)
+            self.apply_changes()
             mib_out=snmp_mib_get(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["SSID_set"],index)
         assert mib_out==ssid_name, "Mib query return value for setting the SSID: %s" % mib_out
 
@@ -33,7 +33,7 @@ class wifi_snmp(wifi_stub):
         security_mode = self.mib_value["security_mode"][security]
         mib_out=snmp_mib_set(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Security_mode"],index,"i",security_mode)
         if self.apply_changes_no_delay:
-            self.apply_changes(wifi_mode)
+            self.apply_changes()
             mib_out=snmp_mib_get(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Security_mode"],index)
         assert mib_out==security_mode, "Mib query return value for setting the security mode: %s" % mib_out
 
@@ -41,14 +41,14 @@ class wifi_snmp(wifi_stub):
         index = self.mib_value["mib_index"][wifi_mode]
         mib_out=snmp_mib_set(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Password_set"],index,"s",password)
         if self.apply_changes_no_delay:
-            self.apply_changes(wifi_mode)
+            self.apply_changes()
             mib_out=snmp_mib_get(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Password_set"],index)
         assert mib_out==password, "Mib query return value for setting the password: %s" % mib_out
 
     def enable_channel_utilization(self, wifi_mode):
         mib_out=snmp_mib_set(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Channel_Util"],"0","i","2")
         if self.apply_changes_no_delay:
-            self.apply_changes(wifi_mode)
+            self.apply_changes()
             mib_out=snmp_mib_get(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Channel_Util"],"0")
         assert mib_out=="2", "Mib query return value for setting the channel utilisation: %s" % mib_out
 
@@ -57,7 +57,7 @@ class wifi_snmp(wifi_stub):
         operating_mode = self.mib_value["operating_mode"][operating_mode]
         mib_out=snmp_mib_set(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Operating_mode"],index,"i",operating_mode)
         if self.apply_changes_no_delay:
-            self.apply_changes(wifi_mode)
+            self.apply_changes()
             mib_out=snmp_mib_get(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Operating_mode"],index)
         assert mib_out==operating_mode, "Mib query return value for setting the operating mode: %s" % mib_out
 
@@ -66,7 +66,7 @@ class wifi_snmp(wifi_stub):
         bandwidth = self.mib_value["bandwidth"][bandwidth]
         mib_out=snmp_mib_set(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Bandwidth"],index,"i",bandwidth)
         if self.apply_changes_no_delay:
-            self.apply_changes(wifi_mode)
+            self.apply_changes()
             mib_out=snmp_mib_get(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Bandwidth"],index)
         assert mib_out==bandwidth, "Mib query return value for setting the bandwidth: %s" % mib_out
 
@@ -74,7 +74,7 @@ class wifi_snmp(wifi_stub):
         index = self.mib_value["mib_index"][wifi_mode]
         mib_out=snmp_mib_set(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Channel_mode"],index,"u",channel_number)
         if self.apply_changes_no_delay:
-            self.apply_changes(wifi_mode)
+            self.apply_changes()
             mib_out=snmp_mib_get(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Channel_mode"],index)
         assert mib_out==channel_number, "Mib query return value for setting the channel number: %s" % mib_out
 
@@ -86,24 +86,28 @@ class wifi_snmp(wifi_stub):
         index = self.mib_value["mib_index"][wifi_mode]
         mib_out=snmp_mib_set(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Broadcast"],index,"i",broadcast_value)
         if self.apply_changes_no_delay:
-            self.apply_changes(wifi_mode)
+            self.apply_changes()
             mib_out=snmp_mib_get(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Broadcast"],index)
         assert mib_out==broadcast_value, "Mib query return value for setting the broadcast for SSID: %s" % mib_out
 
-    def apply_changes(self, wifi_mode):
+    def apply_changes(self):
         """
         Name: apply_changes
         Purpose: Chekck DUT wireless is enable after wifiMgmtApplySettings setup
         Input: None
         Output: True or False
         """
-        index = self.mib_value["mib_index"][wifi_mode]
-        snmp_mib_set(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Wifi_Apply_setting"],"0","i","1")
+        snmp_mib_set(self.device, self.parser, self.iface_ip, self.mib_value["mib_name"]["Wifi_Apply_setting"], '0', 'i', '1')
         for i in range(4):
             self.board.expect(pexpect.TIMEOUT, timeout=20)
             try:
-                mib_out=snmp_mib_get(self.device,self.parser,self.iface_ip,self.mib_value["mib_name"]["Current_channel"],index)
-                if mib_out=='0':
+                mib_out=snmp_mib_set(self.device, self.parser, self.iface_ip, self.mib_value["mib_name"]["Wifi_diag_command"], '0', 's', '"echo 1"')
+                mib_out=snmp_mib_get(self.device, self.parser, self.iface_ip, self.mib_value["mib_name"]["Wifi_diag_result"], '0')
+                if mib_out != '1':
+                    continue
+                wifi_2G_channel=snmp_mib_get(self.device, self.parser, self.iface_ip, self.mib_value["mib_name"]["Current_channel"], '32')
+                wifi_5G_channel=snmp_mib_get(self.device, self.parser, self.iface_ip, self.mib_value["mib_name"]["Current_channel"], '92')
+                if wifi_2G_channel != '0' and wifi_5G_channel != '0':
                     return True
                     break
             except:
