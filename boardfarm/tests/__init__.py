6fa9b77a3dbc93d61c39671e2c2859c9bbe3bbc1
Mike Anderson <mbanderson@uwalumni.com>
2019-10-17 13:39:16 -0500

    tests: Don't import tests unless 'init' is called
    Soon we will allow importing of things from other tests. This change
    will make it so that not every import will re-trigger code to
    import all the other tests.
    
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>
    Change-Id: I0bc28e3cc33badc3539a2f52a803136e3ccfeb64
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/1048
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Matthew McClintock <matthew@meldnetworks.com>

--- boardfarm/tests/__init__.py
+++ boardfarm/tests/__init__.py
@@ -14,40 +14,49 @@ import traceback
 
 from boardfarm.lib import find_subdirs
 
-test_files = glob.glob(os.path.dirname(__file__) + "/*.py")
-boardfarm_overlays = os.environ.get('BFT_OVERLAY')
-if boardfarm_overlays:
-    # Insert 'tests' directories from the overlays
-    dirs = boardfarm_overlays.split(" ")
-    for x in find_subdirs(dirs, "tests"):
-        sys.path.insert(0, x)
-        test_files += glob.glob(os.path.join(x, '*.py'))
-    # Insert this local tests directory too
-    sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
-
-test_mappings = {}
-for x in sorted([os.path.basename(f)[:-3] for f in test_files if not "__" in f]):
-    if x == "tests":
-        raise Exception("INVALID test file name found, tests.py will cause namespace issues, please rename")
-    try:
-        test_file = None
-        exec("import %s as test_file" % x)
-        assert test_file is not None
-        test_mappings[test_file] = []
-        for obj in dir(test_file):
-            ref = getattr(test_file, obj)
-            if inspect.isclass(ref) and issubclass(ref, unittest2.TestCase):
-                test_mappings[test_file].append(ref)
-                exec("from %s import %s" % (x, obj))
-    except Exception as e:
-        if 'BFT_DEBUG' in os.environ:
-            traceback.print_exc()
-            print("Warning: could not import from file %s.py" % x)
-        else:
-            print("Warning: could not import from file %s.py. Run with BFT_DEBUG=y for more details" % x)
+available_tests = {}
 
 def init(config):
+    test_files = glob.glob(os.path.dirname(__file__) + "/*.py")
+    boardfarm_overlays = os.environ.get('BFT_OVERLAY')
+    if boardfarm_overlays:
+        # Insert 'tests' directories from the overlays
+        dirs = boardfarm_overlays.split(" ")
+        for x in find_subdirs(dirs, "tests"):
+            sys.path.insert(0, x)
+            test_files += glob.glob(os.path.join(x, '*.py'))
+        # Insert this local tests directory too
+        sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+
+    test_mappings = {}
+    for x in sorted([os.path.basename(f)[:-3] for f in test_files if not "__" in f]):
+        if x == "tests":
+            raise Exception("INVALID test file name found, tests.py will cause namespace issues, please rename")
+        try:
+            test_file = None
+            exec("import %s as test_file" % x)
+            assert test_file is not None
+            test_mappings[test_file] = []
+            for obj in dir(test_file):
+                ref = getattr(test_file, obj)
+                if inspect.isclass(ref) and issubclass(ref, unittest2.TestCase):
+                    test_mappings[test_file].append(ref)
+                    exec("from %s import %s" % (x, obj))
+        except Exception as e:
+            if 'BFT_DEBUG' in os.environ:
+                traceback.print_exc()
+                print("Warning: could not import from file %s.py" % x)
+            else:
+                print("Warning: could not import from file %s.py. Run with BFT_DEBUG=y for more details" % x)
+
+    # Build dictionary where
+    #   key = test name
+    #   value = reference to test class
+    for key in test_mappings:
+        for item in test_mappings[key]:
+            available_tests[item.__name__] = item
+
     for test_file, tests in test_mappings.iteritems():
         for test in tests:
             if not hasattr(test, "parse"):
