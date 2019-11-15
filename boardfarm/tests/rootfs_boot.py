2eab3874d3195a34ffbfee16f7e618ab9f88888f
Mike Anderson <mbanderson@uwalumni.com>
2019-11-15 11:37:48 -0600

    tests/rootfs_boot: Fix import to support Python2 and 3
    Signed-off-by: Mike Anderson <mbanderson@uwalumni.com>
    Change-Id: I4933364773f01db0798c37c3265c06dd1dd4447d
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/1502
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Mike Anderson <mike@meldnetworks.com>

--- boardfarm/tests/rootfs_boot.py
+++ boardfarm/tests/rootfs_boot.py
@@ -6,7 +6,7 @@
 # The full text can be found in LICENSE in the root directory.
 
 import time
-import linux_boot
+from . import linux_boot
 from boardfarm import lib
 
 from boardfarm.devices import board, wan, lan, prompt
