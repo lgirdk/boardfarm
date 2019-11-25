29174ae5ed713dada6fa03a6774a60761ae68ea2
Matthew McClintock <msm-oss@mcclintock.net>
2019-11-25 13:04:46 -0600

    devices: cdrouter: pop ipv6 config
    Change-Id: I106f2f925e78ea9aa6b63d98cb53a88ded5441fc
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/1617
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Matthew McClintock <matthew@meldnetworks.com>

--- boardfarm/devices/cdrouter_bft.py
+++ boardfarm/devices/cdrouter_bft.py
@@ -6,5 +6,7 @@ class CDrouterDevice(object):
         self.wan_iface = kwargs.pop('wan_iface')
         self.lan_iface = kwargs.pop('lan_iface')
         self.wanispip = kwargs.pop('wanispip')
+        self.wanispip_v6 = kwargs.pop('wanispip_v6')
         self.wanispgateway = kwargs.pop('wanispgateway')
+        self.wanispgateway_v6 = kwargs.pop('wanispgateway_v6')
         self.ipv4hopcount = kwargs.pop('ipv4hopcount')
