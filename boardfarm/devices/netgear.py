66d5c8a6dbf3de733a723d2662258c3c8b90b17a
Raju Shanigarapu <rshanigarapu.contractor@libertyglobal.com>
2019-12-09 16:18:18 +0100

    devices/netgear.py Added docstrings.
    Added docstring in the file.
    
    Signed-off-by: Raju Shanigarapu <rshanigarapu.contractor@libertyglobal.com>
    Change-Id: Ifc16535568a46b0f65722b921f3bcd37de0c1033
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/1870
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/devices/netgear.py
+++ boardfarm/devices/netgear.py
@@ -14,15 +14,24 @@ from boardfarm.lib.bft_pexpect_helper import bft_pexpect_helper
 prompt = "\(M4100-50G\) "
 
 class NetgearM4100(linux.LinuxDevice):
-    '''
-    A netgear switch allows for changing connections by modifying
-    VLANs on ports.
-    '''
+    """A netgear switch allows for changing connections by modifying VLANs on ports extends LinuxDevice.
+    """
 
     def __init__(self,
                  conn_cmd,
                  username='admin',
                  password='bigfoot1'):
+        """Constructor used to initialize variables in NetgearM4100
+
+        :param self: self object
+        :type self: object
+        :param conn_cmd: conn_cmd to connect to device
+        :type conn_cmd: string
+        :param username: username to be for connection, defaults to "admin"
+        :type username: string
+        :param password: password to connect to device, defaults to "bigfoot1"
+        :type password: string
+        """
         bft_pexpect_helper.spawn.__init__(self, '/bin/bash', args=['-c', conn_cmd])
         self.logfile_read = sys.stdout
         self.username = username
@@ -31,6 +40,11 @@ class NetgearM4100(linux.LinuxDevice):
         self.connect()
 
     def connect(self):
+        """connects to the device
+
+        :param self: self object
+        :type self: object
+        """
         self.sendline("\n")
         i = self.expect(["User:", prompt], timeout=20)
         if i == 0:
@@ -40,6 +54,11 @@ class NetgearM4100(linux.LinuxDevice):
             self.expect(prompt)
 
     def disconnect(self):
+        """disconnects to the device
+
+        :param self: self object
+        :type self: object
+        """
         # Leave config mode
         self.sendline("exit")
         self.expect(prompt)
@@ -52,6 +71,15 @@ class NetgearM4100(linux.LinuxDevice):
         self.close()
 
     def change_port_vlan(self, port, vlan):
+        """This method changes the vlan associated to a port
+
+        :param self: self object
+        :type self: object
+        :param port: port for which vlan to be changed
+        :type port: int
+        :param vlan: vlan to be associated to the port
+        :type vlan: string
+        """
         # Enter privileged mode
         self.sendline("enable")
         i = self.expect([prompt, "Password:"])
@@ -80,13 +108,21 @@ class NetgearM4100(linux.LinuxDevice):
         self.expect(prompt)
 
     def setup_standard_vlans(self, min_port=1, max_port=49):
-        '''
+        """This method sets up the standard vlans
         Create enough VLANs, then put ports on VLANS such that:
         port 1 & 2 are on VLAN 3
         port 3 & 4 are on VLAN 4
         etc...
         Also remove all ports from VLAN 1 (default setting).
-        '''
+
+
+        :param self: self object
+        :type self: object
+        :param min_port: the start port to be associated
+        :type min_port: int
+        :param max_port: the end port to be assocauted
+        :type max_port: int
+        """
         # Enter privileged mode
         self.sendline("enable")
         i = self.expect([prompt, "Password:"])
@@ -128,10 +164,11 @@ class NetgearM4100(linux.LinuxDevice):
             pvid += 1
 
     def print_vlans(self):
-        '''
-        Query each port on switch to see connected mac addresses.
-        Print connection table in the end.
-        '''
+        """This method  Query each port on switch to see connected mac addresses and Print connection table in the end.
+
+        :param self: self object
+        :type self: object
+        """
         vlan_macs = {}
         # Enter privileged mode
         self.sendline("enable")
