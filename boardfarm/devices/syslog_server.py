77ee07c9ab86206d5fd4ff5787e81b6dffad3a6b
Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
2019-12-09 13:54:07 +0100

    devices: syslog_server: Added docstring for syslog server methods
    Signed-off-by: Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
    Change-Id: I0963da4f87ceebaa9d77f445b590990068ed9745
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/1863
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Panimalar Palaniappan <ppalaniappan.contractor@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/devices/syslog_server.py
+++ boardfarm/devices/syslog_server.py
@@ -1,17 +1,19 @@
-
+""" Syslog server method
+"""
 import re
 import time
 import datetime
 
 class SyslogServer(object):
-    '''
-    Linux based syslog server
-    '''
+    """Linux based syslog server
+    """
 
     model = ('syslog')
     profile = {}
 
     def __init__(self, *args, **kwargs):
+        """ Constructor method
+        """
         self.args = args
         self.kwargs = kwargs
 
@@ -19,16 +21,19 @@ class SyslogServer(object):
         self.syslog_path = self.kwargs.get('syslog_path', '/var/log/BF/')
 
     def __str__(self):
+        """Return string format
+
+        :return: SyslogServer
+        :rtype: string
+        """
         return "SyslogServer"
 
     def remove_syslog_via_ip(self, ip):
-        '''
-        Remove dut syslog on server
+        """To remove the syslog from the server for the given IP
 
-        This method is to remove the syslog from the server for the given IP
-
-        Parameters: (string)IP address of the DUT
-        '''
+        :param ip: IP address of the DUT
+        :type ip: string
+        """
 
         command = "rm %s/log_%s" % (self.syslog_path, ip)
         self.sendline(command)
@@ -36,33 +41,31 @@ class SyslogServer(object):
         self.expect(self.prompt)
 
     def get_syslog_via_ip(self, ip, n=10):
-        '''
+        """To get the syslog from the server
         Getting the syslog
-
-        This method is to get the syslog from the server
-
-        Parameters: (string)Ip address of the DUT
-                    (int) Number of lines to be returned
-
-        Returns: (string)Syslog messages
-        '''
+        :param ip: Ip address of the DUT
+        :type ip: string
+        :param n: Number of lines to be returned, defaults to 10
+        :type n: Integer, optional
+        :return: Syslog messages
+        :rtype: string
+        """
 
         command = "tail -n %s %slog_%s" % (n, self.syslog_path, ip)
         req = self.check_output(command)
         return req
 
     def check_syslog_time(self, ip, check_string):
-        '''
-        Check for syslog time
-
-        This method is to get the syslog message time in server
+        """This method is to get the syslog message time in server
         and the time in DUT for a particular check string
 
-        Parameters: (string)IP address of the DUT
-                    (string):Chcek string or message for which the time to be fetched
-
-        Returns: (string) Time of the syslog message
-        '''
+        :param IP: IP address of the DUT
+        :type IP: string
+        :param check_string: Chcek string or message for which the time to be fetched
+        :type check_string: string
+        :return: Time of the syslog message
+        :rtype: string
+        """
         log = self.get_syslog_via_ip(ip)
         match = re.search(".*\s(\d+\:\d+\:\d+).*(%s).*" % check_string, log)
         time_log_msg = match.group(1)
