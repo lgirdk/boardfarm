7e38183a4a3ae6e255a4612964b7c9cc258b2a5b
Raju Shanigarapu <rshanigarapu.contractor@libertyglobal.com>
2019-12-09 14:15:00 +0100

    devices/ser2net_connection.py Added docstring
    Added docstring in the methods __init__, connect and close.
    
    Signed-off-by: Raju Shanigarapu <rshanigarapu.contractor@libertyglobal.com>
    Change-Id: I92b07549610c21baddc15b40918b4c2fe599b00c
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/1865
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>

--- boardfarm/devices/ser2net_connection.py
+++ boardfarm/devices/ser2net_connection.py
@@ -2,11 +2,32 @@ import pexpect
 from boardfarm.lib.bft_pexpect_helper import bft_pexpect_helper
 
 class Ser2NetConnection():
+    """The ser2net daemon allows telnet and tcp sessions to be established with a unit's serial ports.
+    If a board is connected serially to a server running ser2net daemon, this class can be used
+    to connect to the board.
+    """
     def __init__(self, device=None, conn_cmd=None, **kwargs):
+        """This method initializes the class instance to open a pexpect session.
+
+        :param device: device to connect, defaults to None
+        :type device: object
+        :param conn_cmd: conn_cmd to connect to device, defaults to None
+        :type conn_cmd: string
+        :param **kwargs: args to be used
+        :type **kwargs: dict
+        """
         self.device = device
         self.conn_cmd = conn_cmd
 
     def connect(self):
+        """Connects to the board/station using telnet.
+
+        This method spawn a pexpect session with telnet command.
+        The telnet port must be as per the ser2net configuration file in order to connect to
+        serial ports of the board.
+
+        :raises: Exception Board is in use (connection refused). / Password required and not supported
+        """
         bft_pexpect_helper.spawn.__init__(self.device,
                                command='/bin/bash',
                                args=['-c', self.conn_cmd])
@@ -19,4 +40,6 @@ class Ser2NetConnection():
             raise Exception("Password required and not supported")
 
     def close(self):
+        """Closes the connection
+        """
         self.device.sendline("~.")
