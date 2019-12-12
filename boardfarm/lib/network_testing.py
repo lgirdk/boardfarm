63feb53b8f55cb4c74a474c541611eb494f74c50
PREM KUMAR K N <prekumar.contractor@libertyglobal.com>
2019-12-12 15:50:53 +0100

    lib: network_testing: Added doc string for all methods
    Change-Id: I57b2bcd85125f57679541183a3886347f49fbb1d
    Signed-off-by: PREM KUMAR K N <prekumar.contractor@libertyglobal.com>
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/1991
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/lib/network_testing.py
+++ boardfarm/lib/network_testing.py
@@ -11,6 +11,21 @@ import ipaddress
 import six
 
 def tcpdump_capture(device, interface, port=None, capture_file='pkt_capture.pcap'):
+    """Capture network traffic using tcpdump.
+    Note: This function will keep capturing until you Kill tcpdump.
+    The kill_process method can be used to kill the process.
+
+    :param device: lan or wan
+    :type device: Object
+    :param interface: interface on which the packets to be captured (eg: eth0)
+    :type interface: String
+    :param port: Port number to capture. Can be a single port or range of ports (for https: 443 or 443-433)
+    :type port: String
+    :param capture_file: Filename to create in which packets shall be stored. Defaults to 'pkt_capture.pcap'
+    :type capture_file: String, Optional
+    :return: Console ouput of tcpdump sendline command.
+    :rtype: string
+    """
 
     if port == None:
         device.sudo_sendline("tcpdump -i %s -n -w %s &" % (interface, capture_file))
@@ -20,6 +35,15 @@ def tcpdump_capture(device, interface, port=None, capture_file='pkt_capture.pcap
     return device.before
 
 def kill_process(device, process="tcpdump"):
+    """Kill any active process
+
+    :param device: lan or wan
+    :type device: Object
+    :param process: process to kill, defaults to tcpdump
+    :type process: String, Optional
+    :return: Console ouput of sync sendline command after kill process
+    :rtype: string
+    """
     device.sudo_sendline("killall %s" % process)
     device.expect(device.prompt)
     device.sudo_sendline("sync")
@@ -27,14 +51,19 @@ def kill_process(device, process="tcpdump"):
     return device.before
 
 def tcpdump_read(device, capture_file, protocol='', opts=''):
-    '''Read the tcpdump packets
-    Parameters:
-       device: Device where the captured file is located
-       capture_file : Name of the filename where the packets captured via tcpdump_capture
-       opts: can be more than one parameter but it should be joined with "and" eg: ('host '+dest_ip+' and port '+port)
-    Returns:
-       Returns the output of filter
-    '''
+    """Read the tcpdump packets and deletes the capture file after read
+
+    :param device: lan or wan
+    :type device: Object
+    :param capture_file: Filename in which the packets were captured
+    :type capture_file: String
+    :param protocol: protocol to filter. Defaults to ''
+    :type protocol: String, Optional
+    :param opts: can be more than one parameter but it should be joined with "and" eg: ('host '+dest_ip+' and port '+port). Defaults to ''
+    :type opts: String, Optional
+    :return: Output of tcpdump read command.
+    :rtype: string
+    """
     if opts:
         protocol = protocol+' and '+opts
     device.sudo_sendline("tcpdump -n -r %s %s" % (capture_file, protocol))
@@ -46,27 +75,31 @@ def tcpdump_read(device, capture_file, protocol='', opts=''):
 
 
 def sip_read(device, capture_file):
+    """Read and filter SIP packets from the captured file.
+    The Session Initiation Protocol is a signaling protocol used for initiating, maintaining, and
+    terminating real-time sessions that include voice, video and messaging applications
+
+    :param device: lan or wan
+    :type device: Object
+    :param capture_file: Filename in which the packets were captured
+    :type capture_file: String
+    :return: Output of tshark read command.
+    :rtype: string
     """
-    To filter SIP packets from the captured file.
-    Parameters:
-        device (obj): Device where the captured file is located
-        capture_file : return value of tcpdump_capture fn
-    Returns:
-        output_sip (str): Filtered SIP packets
-    """
+
     device.sudo_sendline("tshark -r %s -Y sip" % (capture_file))
     device.expect(device.prompt)
     output_sip = device.before
     return output_sip
 
 def rtp_read_verify(device, capture_file):
-    """
-    To filter RTP packets from the captured file and verify.
-    Parameters:
-        device (obj): Device where the captured file is located
-        capture_file : return value of tcpdump_capture fn
-    Returns:
-        None
+    """To filter RTP packets from the captured file and verify. Delete the capture file after verify.
+    Real-time Transport Protocol is for delivering audio and video over IP networks.
+
+    :param device: lan or wan
+    :type device: Object
+    :param capture_file: Filename in which the packets were captured
+    :type capture_file: String
     """
     device.sudo_sendline("tshark -r %s -Y rtp > rtp.txt" % (capture_file))
     device.expect_prompt()
@@ -77,31 +110,40 @@ def rtp_read_verify(device, capture_file):
     device.expect_prompt()
 
 def basic_call_verify(output_sip, ip_src):
-    """
-    To verify basic call flow with sip messages.
-    Parameters:
-        output_sip (str): return value of sip_read
-        ip_src (str): IP of device which initiates the call
-    Returns:
-        None
+    """To verify basic call flow with sip messages.
+
+    :param output_sip: return value of sip_read function
+    :type output_sip: String
+    :param ip_src: IP of device which initiates the call
+    :type ip_src: String
     """
     import re
     sip_msg = re.search(".*" + ip_src + ".*INVITE.*?" + ip_src + "\s+SIP.*100\s+Trying.*?" + ip_src + "\s+SIP.*180\s+Ringing.*?" + ip_src + "\s+SIP\/SDP.*200\s+OK.*?" + ip_src + ".*ACK.*?" + ip_src + ".*BYE.*?" + ip_src + "\s+SIP.*200\s+OK\s+\|", output_sip, re.DOTALL)
     assert sip_msg is not None, "SIP call failed"
 
 def nmap_cli(device, ip_address, port, protocol = None, retry = 0, timing = '', optional = ''):
-    """
-    To run port scanning on the specified target.
+    """To run port scanning on the specified target.Port scan is a method for determining which ports on a interface are open.
     This method is used to perform port scanning on the specified port range of the target ip specified from the device specified.
-    Parameters: (object)device
-                (string)target_ip
-                (string)port rane to be scanned
-                (string)protocol (tcp/ucp/both)
-                (int)max_retries
-                (string)timing (-T[1-4])
-                (int)min_rate
-    Returns: (string) output of namp command.
+
+    :param device: device on which nmap command to run
+    :type device: object
+    :param ip_address: target ip address
+    :type ip_address: string
+    :param port: port range to be scanned
+    :type port: string
+    :param protocol: protocol (tcp/ucp/both), default to None
+    :type protocol: String, Optional
+    :param retry: maximum number of retries, defaults to 0
+    :type retry: integer, Optional
+    :param timing: Timing templates(-T[1-4]), defaults to ''
+                   Each template will have different timings for different actions
+    :type timing: String, Optional
+    :param optional: Other options like minimum rate limit(packets/sec), max rate limit(packets/sec)
+    :type optional: String
+    :return: Output of namp command.
+    :rtype: string
     """
+
     if not protocol:
         protocol = "both"
     ipv6 = '-6' if 'IPv6Address' == type(ipaddress.ip_address(six.text_type(ip_address))).__name__ else ''
@@ -116,9 +158,21 @@ def nmap_cli(device, ip_address, port, protocol = None, retry = 0, timing = '', 
     return nmap_output
 
 def ssh_service_verify(device, dest_device, ip, opts="", ssh_key="-oKexAlgorithms=+diffie-hellman-group1-sha1"):
-    """
-    This function assumes that the server does not know the identity of the client!!!!!
-    I.e. no passwordless login
+    """This function will try to verify if SSH service is running on a target device.
+    If the ssh connection expects key exchange, then ssh_key provided is used to add the target device to known hosts.
+    If connection is accepted and SSH password of target device is provided for login.
+
+    :param device: client device from which the SSH session is initaited
+    :type device: object
+    :param dest_device: target device to connect
+    :type dest_device: string
+    :param ip: target device ip address
+    :type ip: string
+    :param opts: SSH options if any, default to ""
+    :type opts: String, Optional
+    :param ssh_key: SSH key for authentication, defaults to "-oKexAlgorithms=+diffie-hellman-group1-sha1"
+    :type ssh_key: String, Optional
+    :raises Exception: Exception thrown on SSH connection fail
     """
     device.sendline("ssh %s@%s" % (dest_device.username, ip))
     try:
@@ -141,6 +195,18 @@ def ssh_service_verify(device, dest_device, ip, opts="", ssh_key="-oKexAlgorithm
         raise Exception("Failed to connect SSH to :%s" % device.before)
 
 def telnet_service_verify(device, dest_device, ip, opts=""):
+    """Verify telent service connection
+
+    :param device: client device from which the telent session is initaited
+    :type device: object
+    :param dest_device: target device to connect
+    :type dest_device: string
+    :param ip: target device ip address
+    :type ip: string
+    :param opts: telent options if any, default to ""
+    :type opts: String, Optional
+    :raises Exception: Exception thrown on telnet connection fail
+    """
     device.sendline("telnet%s %s" % (opts, ip))
     try:
         device.expect(["Username:"] + ["login:"], timeout=60)
