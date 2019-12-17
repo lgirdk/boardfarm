72e7014001c8f162ccb3003c3f042f1371d23afa
Raju Shanigarapu <rshanigarapu.contractor@libertyglobal.com>
2019-12-17 18:28:01 +0100

    devices/local_cmd.py Added docstrings.
    Added docstring in the methods.
    
    Signed-off-by: Raju Shanigarapu <rshanigarapu.contractor@libertyglobal.com>
    Change-Id: I1731ed83ffc07c99022842d919b73ea098dc976a
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/2068
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>

--- boardfarm/devices/local_cmd.py
+++ boardfarm/devices/local_cmd.py
@@ -2,14 +2,31 @@ import pexpect
 from boardfarm.lib.bft_pexpect_helper import bft_pexpect_helper
 
 class LocalCmd():
-    '''
-    Set connection_type to local_cmd, ignores all output for now
-    '''
+    """This class is meant to be used to connect to a device
+    using a custom Linux command instead of telnet/SSH.
+
+    Sets connection_type to local_cmd, ignores all output for now
+    """
+
     def __init__(self, device=None, conn_cmd=None, **kwargs):
+        """Initializes instance of LocalCmd class
+
+        :param device: the device on which the command is to be executed, defaults to None
+        :type device: object
+        :param conn_cmd: the command to be used to connect to the device, defaults to None
+        :type conn_cmd: string
+        :param **kwargs: extra args to be used if any
+        :type **kwargs: dict
+        """
         self.device = device
         self.conn_cmd = conn_cmd
 
     def connect(self):
+        """This method is used to connect to the device
+           It spawns a pexpect session for the device using the local cmd.
+
+        :raises: Exception Board is in use (connection refused).
+        """
         try:
             bft_pexpect_helper.spawn.__init__(self.device,
                                command='/bin/bash',
@@ -19,4 +36,6 @@ class LocalCmd():
             raise Exception("Board is in use (connection refused).")
 
     def close(self):
+        """closes the pexpect session to the device
+        """
         self.device.sendcontrol('c')
