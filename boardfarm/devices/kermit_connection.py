9d393113cc83e495e10c8a9b4e8997ef79dfa0fd
Raju Shanigarapu <rshanigarapu.contractor@libertyglobal.com>
2019-12-18 10:16:56 +0100

    devices/kermit_connection.py Added docstrings.
    Added docstring to the methods in the file.
    
    Signed-off-by: Raju Shanigarapu <rshanigarapu.contractor@libertyglobal.com>
    Change-Id: I332101f9f5e4ff9dafd06c2f32383580543e36e5
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/2092
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>

--- boardfarm/devices/kermit_connection.py
+++ boardfarm/devices/kermit_connection.py
@@ -2,8 +2,7 @@ import pexpect
 from boardfarm.lib.bft_pexpect_helper import bft_pexpect_helper
 
 class KermitConnection():
-    """
-    Wrapper for the kermit command
+    """Wrapper for the kermit command
     kermit can be used as an alternative to telnet. On some
     platform telnet can hog the cpu to 100% for no apparent
     reason. kermit seems to be more stable, but to work properly
@@ -11,11 +10,26 @@ class KermitConnection():
     """
     prompt = "C-Kermit>"
 
-    def __init__(self, device=None, conn_cmd=None, **kwargs):
+    def __init__(self, device = None, conn_cmd = None, **kwargs):
+        """This method initializes the variables used for a kermit connection.
+
+        :param device: the device on which the command is to be executed, defaults to None
+        :type device: object
+        :param conn_cmd: the command to be used to connect to the device, defaults to None
+        :type conn_cmd: string
+        :param **kwargs: extra args to be used if any
+        :type **kwargs: dict
+        """
         self.device = device
         self.conn_cmd = conn_cmd
 
     def connect(self):
+        """This method initializes a pexpect session with kermit command as argument.
+        This will result in a telnet connection to the device.
+        Note: This function only works on password-less devices
+
+        :raises: Exception Board is in use (connection refused).
+        """
         try:
             bft_pexpect_helper.spawn.__init__(self.device,
                                    command='/bin/bash',
@@ -37,6 +51,8 @@ class KermitConnection():
             raise Exception("Board is in use (connection refused).")
 
     def close(self):
+        """Closes the pexpect session to the device
+        """
         self.device.sendcontrol('\\')
         self.device.sendline('c')
         self.device.expect(self.prompt)
