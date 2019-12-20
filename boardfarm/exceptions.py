9526a2bb9ffce0f2441bab40bcaaf6677d6b03ab
Raju Shanigarapu <rshanigarapu.contractor@libertyglobal.com>
2019-12-20 10:59:10 +0100

    boardfarm/exceptions.py Enhanced docstring.
    Formated the docstrings.
    
    Signed-off-by: Raju Shanigarapu <rshanigarapu.contractor@libertyglobal.com>
    Change-Id: I57c3378f2d45155e6ec54341466a8a97db1f8a42
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/2065
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/exceptions.py
+++ boardfarm/exceptions.py
@@ -1,13 +1,12 @@
 import pexpect
 
 class BftBaseException(Exception):
-    '''
-    Base exception all Bft exceptions inherit from
-    '''
+    """Base exception all Bft exceptions inherit from
+    """
 
 class TestError(BftBaseException):
-    """
-    Raise this if a TestStep verification fails
+    """Raise this if a TestStep verification fails.
+    Each test step in boardfarm.orchestration verifies the output of its execution and throws a TestError exception on False"
     """
     pass
 
@@ -18,26 +17,25 @@ class TestImportError(Exception):
     pass
 
 class BftNotSupportedDevice(BftBaseException):
+    """Raise this if a device is blocked but not used.
+    """
     pass
 
 class SkipTest(BftBaseException):
-    '''
-    Raise this to skip running a test.
-    '''
+    """Raise this to skip running a test.
+    """
     pass
 
 class BootFail(BftBaseException):
-    '''
-    Raise this if the board fails to boot.
+    """Raise this if the board fails to boot.
     This exception is special because if it
     occurs then most likely no other test
     can successfully run.
-    '''
+    """
     pass
 
 class CodeError(BftBaseException):
     """Raise this if an code assert fails
-
     This exception is only meant for custom assert
     clause used inside libraries.
     Not to be used with TestStep verification.
@@ -45,18 +43,16 @@ class CodeError(BftBaseException):
     pass
 
 class PexpectErrorTimeout(pexpect.TIMEOUT, BftBaseException):
-    """
-    Raise this if pexpect times out
+    """Raise this if pexpect times out
     """
     pass
 
 class ConfigKeyError(BftBaseException):
-    '''
-    Invalid use of key in config object
-    '''
+    """Invalid use of key in config object
+    """
     pass
 
 class DeviceDoesNotExistError(BftBaseException):
-    '''
-    Device does not exist
-    '''
+    """Device does not exist
+    """
+    pass
