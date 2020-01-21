abdd8a5771081ba5cd7a54570370e89667ffd9ab
Nathuriga Sevvel <nsevvel.contractor@libertyglobal.com>
2020-01-21 20:35:08 +0530

    lib: voice: added voice helper common functions
    These fuctions already exist in voice_helper in bft lgi.Adding these here to use it in DockerBoot.
    add_dns_auth_record(), rm_dns_auth_record(), voice_devices_configure()
    
    Signed-off-by: Nathuriga Sevvel <nsevvel.contractor@libertyglobal.com>
    Change-Id: I41c06bb5bb6510b6ed225a067fabb5628c9123c4
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/2452
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

---
+++ boardfarm/lib/voice.py
@@ -0,0 +1,55 @@
+from nested_lookup import nested_lookup
+
+def add_dns_auth_record(dns,sipserver_name):
+    '''
+    To add a record and srv to the dns server
+
+    Parameters:
+    dns(object): device where the dns server is installed
+    sipserver_name(string): name of the sipserver
+    '''
+    sip_domain = sipserver_name+".boardfarm.com"
+    #removing the auth record lines if present
+    rm_dns_auth_record(dns)
+    dns.sendline('cat >> /etc/dnsmasq.conf << EOF')
+    dns.sendline('auth-zone=%s' % sip_domain)
+    dns.sendline('auth-soa=12345678,admin.%s' % sip_domain)
+    dns.sendline('srv-host=_sip._tcp,%s,5060,20,10' % sip_domain)
+    dns.sendline('srv-host=_sip._tcp,%s,5060,20,10' % sip_domain)
+    dns.sendline('mx-host=%s' % sip_domain)
+    dns.sendline('EOF')
+    dns.expect(dns.prompt)
+    dns.sendline('/etc/init.d/dnsmasq restart')
+    dns.expect(dns.prompt)
+
+def rm_dns_auth_record(dns):
+    '''
+    To remove A record and srv to the dns server
+
+    Parameters:
+    dns(object): device where the dns server is installed
+    '''
+    dns.sendline('sed \'/auth-zone\=/,/mx-host\=/d\' /etc/dnsmasq.conf > /etc/tmpfile.txt')
+    dns.expect(dns.prompt)
+    dns.sendline('mv /etc/tmpfile.txt /etc/dnsmasq.conf')
+    dns.expect(dns.prompt)
+    dns.sendline('/etc/init.d/dnsmasq restart')
+    dns.expect(dns.prompt)
+
+def voice_devices_configure(voice_devices_list, sip_server):
+    '''
+    Initialize the Voice test setup
+
+    Parameters:
+    voice_devices_list(list of obj): list of voice devices
+    sip_server(obj): sipserver device
+    '''
+    try:
+        for voice_device_name in voice_devices_list:
+            if hasattr(voice_device_name, "profile"):
+                boot_list = nested_lookup("on_boot",voice_device_name.profile.get(voice_device_name.name, {}))
+                for profile_boot in boot_list:
+                    profile_boot()
+    except Exception as e:
+        sip_server.kill_asterisk()
+        raise Exception("Unable to initialize Voice devices, failed due to the error : ", e)
