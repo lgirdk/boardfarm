bd2231d1de4439e7ad164964b252608939018d41
Matthew McClintock <msm-oss@mcclintock.net>
2020-02-07 17:05:22 +0000

    automation/manifest-based-boardfarm-run.groovy: add message why we are killing
    Change-Id: I97d27b2c651f2b75af55877780d1356a77b744b8
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/3029
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Matthew McClintock <matthew@meldnetworks.com>

--- automation/manifest-based-boardfarm-run.groovy
+++ automation/manifest-based-boardfarm-run.groovy
@@ -323,6 +323,7 @@ pipeline {
             for PID in $(ps -eo pid,command -u ${USER} | grep -v grep | tail -n+2 | awk '{print $1}' | grep -v ${PID_SELF} | grep -v ${PPID}); do
                 echo "Checking pid ${PID}"
                 if xargs -0 -L1 -a /proc/${PID}/environ 2>/dev/null | grep "BUILD_ID=${BUILD_ID}$"; then
+                    echo "Matched BUILD_ID=${BUILD_ID}$"
                     echo "Killing $(ps -p ${PID} | tail -1 | awk '{print $1}')"
                     sed -z 's/$/ /' /proc/$PID/cmdline; echo
                     kill ${PID}
