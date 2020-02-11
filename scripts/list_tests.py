428bcbcd2f81eb2c865d1d473ebfc3454baa36fa
Mike Anderson <mike@meldnetworks.com>
2020-02-11 17:33:50 +0000

    scripts: Create list_tests.py to list tests in any testsuite
    Signed-off-by: Mike Anderson <mike@meldnetworks.com>
    Change-Id: I357c4a3ead2f4ebbb016af6c4af45cce04725c1b
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/3081
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>

---
+++ scripts/list_tests.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import argparse
+import re
+
+from boardfarm import testsuites
+
+
+def get_num(name):
+    '''
+    If a test case name contains a number, just return the number.
+    Useful for sorting.
+    '''
+    result = re.search(r'\d+', name)
+    if result:
+        return int(result.group(0))
+    else:
+        return 0
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Display test names in testsuite')
+    parser.add_argument('testsuite', type=str, help='name of a test suite')
+    args = parser.parse_args()
+    names = testsuites.list_tests[args.testsuite]
+    print("\n".join(sorted(names, key=get_num)))
