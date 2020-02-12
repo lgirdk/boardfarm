64914b7f14cbe158408aabdefdcc98a594f0d660
Mike Anderson <mike@meldnetworks.com>
2020-02-12 14:59:54 +0000

    tests/uname: Use self.dev.board
    Signed-off-by: Mike Anderson <mike@meldnetworks.com>
    Change-Id: Ifa132dfc126a8d7876de7a639c31898a798c7a5e
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/3102
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>

--- boardfarm/tests/uname.py
+++ boardfarm/tests/uname.py
@@ -6,26 +6,26 @@
 # The full text can be found in LICENSE in the root directory.
 
 from boardfarm.tests import rootfs_boot
-from boardfarm.devices import board
-from boardfarm.devices import prompt
+
 
 class Uname(rootfs_boot.RootFSBootTest):
     '''Checked board system information.'''
     def runTest(self):
+        board = self.dev.board
         board.sendline('\nuname -a')
         board.expect('uname -a', timeout=6)
-        board.expect(prompt)
+        board.expect(board.prompt)
         info = board.before.replace('\r', '').replace('\n', '')
         self.result_message = info
         board.sendline('uname -m')
         board.expect('uname -m')
-        board.expect(prompt)
+        board.expect(board.prompt)
         self.logged['machine'] = board.before.replace('\r', '').replace('\n', '')
         board.sendline('uname -r')
         board.expect('uname -r')
-        board.expect(prompt)
+        board.expect(board.prompt)
         self.logged['release'] = board.before.replace('\r', '').replace('\n', '')
         board.sendline('uname -s')
         board.expect('uname -s')
-        board.expect(prompt)
+        board.expect(board.prompt)
         self.logged['kernel'] = board.before.replace('\r', '').replace('\n', '')
