1c50739e739593653f589527a894eaed4414512c
Mike Anderson <mike@meldnetworks.com>
2020-03-09 17:24:40 +0000

    In sh scripts, tabs to spaces
    Signed-off-by: Mike Anderson <mike@meldnetworks.com>
    Change-Id: I66406dc81ba6a75918bfa76e964cd30c2a57c377
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/3726
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>

--- scripts/parse_commit_msg.sh
+++ scripts/parse_commit_msg.sh
@@ -1,32 +1,32 @@
 #!/bin/bash -xe
 
 for change_id in $(repo forall -r ^$GERRIT_PROJECT$ -c  'git show --format=%b -s HEAD' | grep '^Depends-on: ' | awk '{print $2}'); do
-	raw=$(ssh $auto_user@$GERRIT_HOST -p $GERRIT_PORT gerrit query --format JSON --patch-sets $change_id | \
-		jq -r 'if .patchSets then .patchSets[-1].ref,.project else empty end')
+    raw=$(ssh $auto_user@$GERRIT_HOST -p $GERRIT_PORT gerrit query --format JSON --patch-sets $change_id | \
+        jq -r 'if .patchSets then .patchSets[-1].ref,.project else empty end')
 
-	if [ "$raw" = "" ]; then
-		exit 1
-	fi
+    if [ "$raw" = "" ]; then
+        exit 1
+    fi
 
-	read -r ref proj <<<$(echo $raw)
+    read -r ref proj <<<$(echo $raw)
 
-	echo "Picking $ref from $proj"
-	repo forall -r ^$proj\$ -c 'pwd && git fetch gerrit '$ref' && git rebase FETCH_HEAD && git rebase m/master'
+    echo "Picking $ref from $proj"
+    repo forall -r ^$proj\$ -c 'pwd && git fetch gerrit '$ref' && git rebase FETCH_HEAD && git rebase m/master'
 done
 
 rm -f .env
 touch .env
 
 if repo forall -r ^$GERRIT_PROJECT$ -c 'git show --format=%b -s HEAD' | grep ^Environment:; then
-	echo export BFT_ARGS=$(realpath $(repo forall -r ^$GERRIT_PROJECT$ -c 'git show --format=%b -s HEAD' | grep ^Environment: | awk '{print $2}')) >> .env
-	. ./.env
-	if [ ! -f "$BFT_ARGS" ]; then
-		exit 1
-	fi
+    echo export BFT_ARGS=$(realpath $(repo forall -r ^$GERRIT_PROJECT$ -c 'git show --format=%b -s HEAD' | grep ^Environment: | awk '{print $2}')) >> .env
+    . ./.env
+    if [ ! -f "$BFT_ARGS" ]; then
+        exit 1
+    fi
 fi
 
 if repo forall -r ^$GERRIT_PROJECT$ -c 'git show --format=%b -s HEAD' | grep Boards:; then
-	echo export boards=\"$(repo forall -r ^$GERRIT_PROJECT$ -c 'git show --format=%b -s HEAD' | grep Boards: | sed 's/.*Boards: //g')\" >> .env
+    echo export boards=\"$(repo forall -r ^$GERRIT_PROJECT$ -c 'git show --format=%b -s HEAD' | grep Boards: | sed 's/.*Boards: //g')\" >> .env
 else
-	export boards=$board
+    export boards=$board
 fi
--- scripts/whatchanged.sh
+++ scripts/whatchanged.sh
@@ -1,26 +1,26 @@
 new_tests () {
-	foo=
+    foo=
 
-	if [ -d "tests" ]; then
-		dir="tests"
-	else
-		dir="boardfarm/tests"
-	fi
+    if [ -d "tests" ]; then
+        dir="tests"
+    else
+        dir="boardfarm/tests"
+    fi
 
-	for t in `git diff $1...HEAD -U0 $dir | grep -v '^-' | grep -v __class__ | sed -n 's/.*class\s\+\(.*\)(.*/\1/p' | grep -v super\(`; do
-		foo="$foo $t $(git grep "^class.*($t)" | grep -v ^devices | sed -n 's/.*py:class \([^(]*\)(.*):/\1/p')"
-	done
+    for t in `git diff $1...HEAD -U0 $dir | grep -v '^-' | grep -v __class__ | sed -n 's/.*class\s\+\(.*\)(.*/\1/p' | grep -v super\(`; do
+        foo="$foo $t $(git grep "^class.*($t)" | grep -v ^devices | sed -n 's/.*py:class \([^(]*\)(.*):/\1/p')"
+    done
 
-	for t in $(echo $foo | xargs -n1 | sort -u | xargs); do
-		echo -n "-e $t "
-	done
+    for t in $(echo $foo | xargs -n1 | sort -u | xargs); do
+        echo -n "-e $t "
+    done
 }
 
 features () {
-	feature=$(git log $1...HEAD | sed -n 's/\s*Features\?:\s*\(.*\)$/\1/p' | tr '\n' ' ')
-	if [ -n "$feature" ]; then
-		echo -n "-q $feature"
-	fi
+    feature=$(git log $1...HEAD | sed -n 's/\s*Features\?:\s*\(.*\)$/\1/p' | tr '\n' ' ')
+    if [ -n "$feature" ]; then
+        echo -n "-q $feature"
+    fi
 }
 
 result="$(new_tests $1)$(features $1)"
