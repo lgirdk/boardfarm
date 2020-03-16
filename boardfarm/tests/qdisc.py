2094c4b1895716dc3d14841f09792d6d4658089c
Matthew McClintock <msm-oss@mcclintock.net>
2020-03-16 20:18:11 +0000

    tests: convert qdisc.py to use self.dev
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>
    Change-Id: I6e8635174a3da70859f265c167f4b5ac11ff7805
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/3980
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Mike Anderson <mike@meldnetworks.com>

--- boardfarm/tests/qdisc.py
+++ boardfarm/tests/qdisc.py
@@ -5,13 +5,14 @@
 # This file is distributed under the Clear BSD license.
 # The full text can be found in LICENSE in the root directory.
 
-from boardfarm.devices import board
 from boardfarm.tests import rootfs_boot
 
 
 class DelQdisc(rootfs_boot.RootFSBootTest):
     '''Tries to remove qdisc root node'''
     def runTest(self):
+        board = self.dev.board
+
         board.sendline('tc qdisc del dev eth0 root')
         i = board.expect(['RTNETLINK answers: No such file or directory'] +
                          board.prompt)
