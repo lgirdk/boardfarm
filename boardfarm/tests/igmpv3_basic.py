5a8894a06b987b79cadf48e83d4ad2668ef0b6d2
Matthew McClintock <msm-oss@mcclintock.net>
2020-03-16 20:17:35 +0000

    tests: convert igmpv3_basic.py to use self.dev
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>
    Change-Id: Ife9e34524980ffb5e3e577168e6ecb2df6c8c251
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/3960
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Mike Anderson <mike@meldnetworks.com>

--- boardfarm/tests/igmpv3_basic.py
+++ boardfarm/tests/igmpv3_basic.py
@@ -5,13 +5,15 @@
 # This file is distributed under the Clear BSD license.
 # The full text can be found in LICENSE in the root directory.
 
-from boardfarm.devices import board, prompt
+from boardfarm.devices import prompt
 from boardfarm.tests import rootfs_boot
 
 
 class IGMPv3_Running(rootfs_boot.RootFSBootTest):
     '''IGMP Proxy daemon mcproxy is up and running.'''
     def runTest(self):
+        board = self.dev.board
+
         board.sendline('\nps | grep mcproxy')
         board.expect('/usr/sbin/mcproxy -f /etc/mcproxy.conf', timeout=5)
         board.expect(prompt)
@@ -20,6 +22,8 @@ class IGMPv3_Running(rootfs_boot.RootFSBootTest):
 class IGMPv3_Config(rootfs_boot.RootFSBootTest):
     '''IGMP Proxy daemon mcproxy config is set correctly.'''
     def runTest(self):
+        board = self.dev.board
+
         board.sendline('\nuci show mcproxy')
         board.expect_exact('mcproxy.config=mcproxy', timeout=5)
         board.expect_exact('mcproxy.config.protocol=IGMPv3', timeout=5)
@@ -35,6 +39,8 @@ class IGMPv3_Config(rootfs_boot.RootFSBootTest):
 class IGMPv3_StopStart(rootfs_boot.RootFSBootTest):
     '''IGMP Proxy daemon mcproxy can be stopped and started without rebooting.'''
     def runTest(self):
+        board = self.dev.board
+
         board.sendline('\n/etc/init.d/mcproxy stop')
         board.expect(prompt)
         board.sendline('ps | grep mcproxy')
