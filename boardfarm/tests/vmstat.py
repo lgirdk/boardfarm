465b25291b4a16d77753959ea65695365f3c8c9a
Matthew McClintock <msm-oss@mcclintock.net>
2020-03-16 18:49:05 +0000

    tests: convert vmstat.py to use self.dev
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>
    Change-Id: I5c68fe2de85980759721b36c24473a042b7726d2
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/3920
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Mike Anderson <mike@meldnetworks.com>

--- boardfarm/tests/vmstat.py
+++ boardfarm/tests/vmstat.py
@@ -5,13 +5,15 @@
 # This file is distributed under the Clear BSD license.
 # The full text can be found in LICENSE in the root directory.
 
-from boardfarm.devices import board, prompt
+from boardfarm.devices import prompt
 from boardfarm.tests import rootfs_boot
 
 
 class ProcVmstat(rootfs_boot.RootFSBootTest):
     '''Check /proc/vmstat stats.'''
     def runTest(self):
+        board = self.dev.board
+
         self.logged.update(board.get_proc_vmstat())
 
         # Display extra info
