e364c7964006e2fa7522e8cfdd110a70014415d1
Matthew McClintock <msm-oss@mcclintock.net>
2020-03-16 20:18:32 +0000

    tests: convert wifi.py to use self.dev
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>
    Change-Id: I7416f4fd0b8890efe39660728ef4a061f4c6db76
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/3992
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Mike Anderson <mike@meldnetworks.com>

--- boardfarm/tests/wifi.py
+++ boardfarm/tests/wifi.py
@@ -1,9 +1,10 @@
-from boardfarm.devices import wlan
 from boardfarm.tests import rootfs_boot
 
 
 class WifiScan(rootfs_boot.RootFSBootTest):
     '''Simple test to run a wifi scan'''
     def runTest(self):
+        wlan = self.dev.wlan
+
         scan_output = wlan.scan()
         return scan_output
