d53c68d4265f1c986b45494d7685676c26cb3a80
Matthew McClintock <msm-oss@mcclintock.net>
2020-03-16 20:17:51 +0000

    tests: convert netperf_rfc2544.py to use self.dev
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>
    Change-Id: I72619356f24a082a034ebf78296c7bc294b82ae7
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/3969
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Mike Anderson <mike@meldnetworks.com>

--- boardfarm/tests/netperf_rfc2544.py
+++ boardfarm/tests/netperf_rfc2544.py
@@ -5,13 +5,16 @@
 # This file is distributed under the Clear BSD license.
 # The full text can be found in LICENSE in the root directory.
 
-from boardfarm.devices import board, lan, prompt
+from boardfarm.devices import prompt
 from boardfarm.tests import rootfs_boot
 
 
 class NetperfRFC2544(rootfs_boot.RootFSBootTest):
     '''Single test to simulate RFC2544'''
     def runTest(self):
+        board = self.dev.board
+        lan = self.dev.lan
+
         for sz in ["74", "128", "256", "512", "1024", "1280", "1518"]:
             print("running %s UDP test" % sz)
             lan.sendline(
