a61b0c188b12e435477a004dd30449fa684a4775
Matthew McClintock <msm-oss@mcclintock.net>
2020-03-16 20:18:25 +0000

    tests: convert ubus.py to use self.dev
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>
    Change-Id: I9071e8ea51c652e6e985e4ab9d4f5c498a4ba7cf
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/3988
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Mike Anderson <mike@meldnetworks.com>

--- boardfarm/tests/ubus.py
+++ boardfarm/tests/ubus.py
@@ -8,11 +8,11 @@
 import json
 import time
 
-from boardfarm.devices import board, lan, prompt
+from boardfarm.devices import prompt
 from boardfarm.tests import rootfs_boot
 
 
-def ubus_call_raw(payload, ipaddr="192.168.1.1"):
+def ubus_call_raw(payload, lan, ipaddr="192.168.1.1"):
     curl_cmd = "curl -d '%s' http://%s/ubus" % (json.dumps(payload), ipaddr)
     lan.sendline(curl_cmd)
     lan.expect("\r\n")
@@ -21,17 +21,17 @@ def ubus_call_raw(payload, ipaddr="192.168.1.1"):
     return json.loads(lan.before)
 
 
-def ubus_call(session_id, ubus_object, ubus_func, params):
+def ubus_call(session_id, ubus_object, ubus_func, params, lan):
     j = {
         "jsonrpc": "2.0",
         "id": 1,
         "method": "call",
         "params": [session_id, ubus_object, ubus_func, params]
     }
-    return ubus_call_raw(j)
+    return ubus_call_raw(j, lan)
 
 
-def ubus_check_error(reply, assert_on_err=True):
+def ubus_check_error(reply, lan, assert_on_err=True):
     if "error" in reply:
         print("Got error in reply")
         print(reply)
@@ -39,7 +39,7 @@ def ubus_check_error(reply, assert_on_err=True):
         assert (not assert_on_err)
 
 
-def ubus_login_raw(username="root", password="password"):
+def ubus_login_raw(lan, username="root", password="password"):
     json = {
         "jsonrpc":
         "2.0",
@@ -54,18 +54,18 @@ def ubus_login_raw(username="root", password="password"):
             }
         ]
     }
-    return ubus_call_raw(json)
+    return ubus_call_raw(json, lan)
 
 
-def ubus_login_session(username="root", password="password"):
-    reply = ubus_login_raw(username, password)
+def ubus_login_session(lan, username="root", password="password"):
+    reply = ubus_login_raw(lan, username, password)
 
-    ubus_check_error(reply)
+    ubus_check_error(reply, lan)
 
     return reply['result'][1]['ubus_rpc_session']
 
 
-def ubus_network_restart(session_id):
+def ubus_network_restart(session_id, lan):
     json = {
         "jsonrpc": "2.0",
         "id": 1,
@@ -73,11 +73,11 @@ def ubus_network_restart(session_id):
         "params": [session_id, "network", "restart", {}]
     }
 
-    reply = ubus_call_raw(json)
-    ubus_check_error(reply)
+    reply = ubus_call_raw(json, lan)
+    ubus_check_error(reply, lan)
 
 
-def ubus_system_reboot(session_id):
+def ubus_system_reboot(session_id, lan):
     json = {
         "jsonrpc": "2.0",
         "id": 1,
@@ -85,21 +85,22 @@ def ubus_system_reboot(session_id):
         "params": [session_id, "foo", "bar", {}]
     }
 
-    reply = ubus_call_raw(json)
-    ubus_check_error(reply)
+    reply = ubus_call_raw(json, lan)
+    ubus_check_error(reply, lan)
 
 
 class UBusTestNetworkRestart(rootfs_boot.RootFSBootTest):
     '''Various UBus tests'''
     def runTest(self):
+        lan = self.dev.lan
 
         for i in range(1000):
             print(
                 "\nRunning iteration of ubus json-rpc network restart nubmer %s\n"
                 % i)
-            session_id = ubus_login_session()
+            session_id = ubus_login_session(lan)
             print("\nLogged in with sessionid = %s\n" % session_id)
-            ubus_network_restart(session_id)
+            ubus_network_restart(session_id, lan)
             # wait some amount of time, we can get a new session id before restart
             # really starts
             time.sleep(5)
@@ -108,13 +109,15 @@ class UBusTestNetworkRestart(rootfs_boot.RootFSBootTest):
 class UBusTestSystemReboot(rootfs_boot.RootFSBootTest):
     '''Various UBus tests'''
     def runTest(self):
+        board = self.dev.board
+        lan = self.dev.lan
 
         for i in range(1000):
             print(
                 "\nRunning iteration of ubus json-rpc system reboot nubmer %s\n"
                 % i)
-            session_id = ubus_login_session()
+            session_id = ubus_login_session(lan)
             print("\nLogged in with sessionid = %s\n" % session_id)
 
-            ubus_system_reboot(session_id)
+            ubus_system_reboot(session_id, lan)
             board.wait_for_linux()
