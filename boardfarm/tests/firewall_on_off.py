5400021a64b871b9740df2b252fb7a883f91dd88
Matthew McClintock <msm-oss@mcclintock.net>
2020-03-16 20:17:31 +0000

    tests: convert firewall_on_off.py to use self.dev
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>
    Change-Id: I2125acdf8cc656002cac4bb759ef6729566499e4
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/3958
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Mike Anderson <mike@meldnetworks.com>

--- boardfarm/tests/firewall_on_off.py
+++ boardfarm/tests/firewall_on_off.py
@@ -5,13 +5,15 @@
 # This file is distributed under the Clear BSD license.
 # The full text can be found in LICENSE in the root directory.
 
-from boardfarm.devices import board, prompt
+from boardfarm.devices import prompt
 from boardfarm.tests import rootfs_boot
 
 
 class FirewallOFF(rootfs_boot.RootFSBootTest):
     '''Turned router firewall off.'''
     def runTest(self):
+        board = self.dev.board
+
         board.sendline('\nuci set firewall.@defaults[0].forward=ACCEPT')
         board.expect('uci set firewall')
         board.expect(prompt)
@@ -29,6 +31,8 @@ class FirewallOFF(rootfs_boot.RootFSBootTest):
 class FirewallON(rootfs_boot.RootFSBootTest):
     '''Turned router firewall on.'''
     def runTest(self):
+        board = self.dev.board
+
         board.sendline('\nuci set firewall.@defaults[0].forward=REJECT')
         board.expect('uci set firewall')
         board.expect(prompt)
