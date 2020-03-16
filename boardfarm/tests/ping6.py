0e3c33fde34f97e3b2a808c6fcc4efcc82517773
Matthew McClintock <msm-oss@mcclintock.net>
2020-03-16 20:18:09 +0000

    tests: convert ping6.py to use self.dev
    Signed-off-by: Matthew McClintock <msm-oss@mcclintock.net>
    Change-Id: Idbe2f411dda771cfc80648cb923b4fd057f3ce12
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/3979
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Mike Anderson <mike@meldnetworks.com>

--- boardfarm/tests/ping6.py
+++ boardfarm/tests/ping6.py
@@ -5,13 +5,15 @@
 # This file is distributed under the Clear BSD license.
 # The full text can be found in LICENSE in the root directory.
 
-from boardfarm.devices import lan, prompt
+from boardfarm.devices import prompt
 from boardfarm.tests import rootfs_boot
 
 
 class LanDevPing6Router(rootfs_boot.RootFSBootTest):
     '''Device on LAN can ping6 router.'''
     def runTest(self):
+        lan = self.dev.lan
+
         lan.sendline('\nping6 -c 20 4aaa::1')
         lan.expect('PING ')
         lan.expect(' ([0-9]+) (packets )?received')
@@ -23,6 +25,8 @@ class LanDevPing6Router(rootfs_boot.RootFSBootTest):
 class LanDevPing6WanDev(rootfs_boot.RootFSBootTest):
     '''Device on LAN can ping6 through router.'''
     def runTest(self):
+        lan = self.dev.lan
+
         # Make Lan-device ping Wan-Device
         lan.sendline('\nping6 -c 20 5aaa::6')
         lan.expect('PING ')
