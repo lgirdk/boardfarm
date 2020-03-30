022667ab7554573c71623338fc7897a615fbf08e
Mike Anderson <mike@meldnetworks.com>
2020-03-30 17:44:34 +0000

    Run pyflakes in Python3
    Signed-off-by: Mike Anderson <mike@meldnetworks.com>
    Change-Id: Ia0b6beae53009c75d538fdf5d104666a88170c53
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/4261
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Prem Kumar K N <prekumar.contractor@libertyglobal.com>
    (cherry picked from commit eb150a1cfcc096d20b9915c9badf67a9410ba605)
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/3928
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- automation/manifest-based-boardfarm-run.groovy
+++ automation/manifest-based-boardfarm-run.groovy
@@ -112,7 +112,7 @@ def run_lint () {
     # Check for non-ascii characters
     grep --color='auto' -P -n "[^\\x00-\\x7F]" ${files_changed} | awk '{print $1" contains non-ascii characters."}' >> errors.txt
     # Check pyflakes errors
-    python2 -m pyflakes ${files_changed} > flakes.txt 2>&1
+    python3 -m pyflakes ${files_changed} > flakes.txt 2>&1
     cat flakes.txt | grep -v 'devices\\.' | grep -v 'No such file' >> errors.txt
     # Check print errors
     grep -n -E '^\\s+print\\s' ${files_changed} | awk '{print $1" print should be function: print()"}' >> errors.txt
