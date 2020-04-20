a589de024b98c725c463be63d1d042c2429e205e
Nathuriga Sevvel <nsevvel.contractor@libertyglobal.com>
2020-04-20 10:25:44 +0200

    unittests:test_axiros_acs: added tests for dateTime and SPV positive scenario
    Signed-off-by: Nathuriga Sevvel <nsevvel.contractor@libertyglobal.com>
    Change-Id: I14ae4a536a3b9c8a8536c418a92f2ec3e39b3a15
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/4699
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- unittests/boardfarm/devices/test_axiros_acs.py
+++ unittests/boardfarm/devices/test_axiros_acs.py
@@ -56,9 +56,47 @@ content_5 = b'<?xml version="1.0" encoding="UTF-8"?>\n<SOAP-ENV:Envelope\n  \n  
 #507 and fault code
 response_5 = Response(content_5, text_5)
 
+text_6 = '<?xml version="1.0" encoding="UTF-8"?>\n<SOAP-ENV:Envelope\n  \n  xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"\n  xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"\n  xmlns:xsd3="http://www.w3.org/2001/XMLSchema"\n  xmlns:xsi3="http://www.w3.org/2001/XMLSchema-instance"\n>\n<SOAP-ENV:Body >\n<ns1:GetParameterValuesResponse xmlns:ns1="urn:AxessInterface">\n<Result>\n<code xsi3:type="xsd3:int">200</code>\n<details SOAP-ENC:arrayType="ns1:KeyValueStruct[1]" xsi3:type="SOAP-ENC:Array">\n<item>\n<key xsi3:type="xsd3:string">Device.DeviceInfo.FirstUseDate</key>\n<value SOAP-ENC:arrayType="xsd3:ur-type[6]" xsi3:type="SOAP-ENC:Array">\n<item xsi3:type="xsd3:integer">2020</item>\n<item xsi3:type="xsd3:integer">4</item>\n<item xsi3:type="xsd3:integer">19</item>\n<item xsi3:type="xsd3:integer">19</item>\n<item xsi3:type="xsd3:integer">56</item>\n<item xsi3:type="xsd3:double">18.0</item>\n</value>\n</item>\n</details>\n<message xsi3:type="xsd3:string">OK</message>\n<ticketid xsi3:type="xsd3:int">208900</ticketid>\n</Result>\n</ns1:GetParameterValuesResponse>\n</SOAP-ENV:Body>\n</SOAP-ENV:Envelope>\n'
+
+content_6 = b'<?xml version="1.0" encoding="UTF-8"?>\n<SOAP-ENV:Envelope\n  \n  xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"\n  xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"\n  xmlns:xsd3="http://www.w3.org/2001/XMLSchema"\n  xmlns:xsi3="http://www.w3.org/2001/XMLSchema-instance"\n>\n<SOAP-ENV:Body >\n<ns1:GetParameterValuesResponse xmlns:ns1="urn:AxessInterface">\n<Result>\n<code xsi3:type="xsd3:int">200</code>\n<details SOAP-ENC:arrayType="ns1:KeyValueStruct[1]" xsi3:type="SOAP-ENC:Array">\n<item>\n<key xsi3:type="xsd3:string">Device.DeviceInfo.FirstUseDate</key>\n<value SOAP-ENC:arrayType="xsd3:ur-type[6]" xsi3:type="SOAP-ENC:Array">\n<item xsi3:type="xsd3:integer">2020</item>\n<item xsi3:type="xsd3:integer">4</item>\n<item xsi3:type="xsd3:integer">19</item>\n<item xsi3:type="xsd3:integer">19</item>\n<item xsi3:type="xsd3:integer">56</item>\n<item xsi3:type="xsd3:double">18.0</item>\n</value>\n</item>\n</details>\n<message xsi3:type="xsd3:string">OK</message>\n<ticketid xsi3:type="xsd3:int">208900</ticketid>\n</Result>\n</ns1:GetParameterValuesResponse>\n</SOAP-ENV:Body>\n</SOAP-ENV:Envelope>\n'
+
+#to verify dateTime
+response_6 = Response(content_6, text_6)
+
+out_6 = [{
+    'key': 'Device.DeviceInfo.FirstUseDate',
+    'type': 'dateTime',
+    'value': '2020-04-19T19:56:18'
+}]
+
+text_7 = '<?xml version="1.0" encoding="UTF-8"?>\n<SOAP-ENV:Envelope\n  \n  xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"\n  xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"\n  xmlns:xsd3="http://www.w3.org/2001/XMLSchema"\n  xmlns:xsi3="http://www.w3.org/2001/XMLSchema-instance"\n>\n<SOAP-ENV:Body >\n<ns1:GetParameterValuesResponse xmlns:ns1="urn:AxessInterface">\n<Result>\n<code xsi3:type="xsd3:int">200</code>\n<details SOAP-ENC:arrayType="ns1:KeyValueStruct[1]" xsi3:type="SOAP-ENC:Array">\n<item>\n<key xsi3:type="xsd3:string">Device.DeviceInfo.FirstUseDate</key>\n<value SOAP-ENC:arrayType="xsd3:ur-type[6]" xsi3:type="SOAP-ENC:Array">\n<item xsi3:type="xsd3:integer">1</item>\n<item xsi3:type="xsd3:integer">1</item>\n<item xsi3:type="xsd3:integer">1</item>\n<item xsi3:type="xsd3:integer">0</item>\n<item xsi3:type="xsd3:integer">0</item>\n<item xsi3:type="xsd3:double">0.0</item>\n</value>\n</item>\n</details>\n<message xsi3:type="xsd3:string">OK</message>\n<ticketid xsi3:type="xsd3:int">208900</ticketid>\n</Result>\n</ns1:GetParameterValuesResponse>\n</SOAP-ENV:Body>\n</SOAP-ENV:Envelope>\n'
+
+content_7 = b'<?xml version="1.0" encoding="UTF-8"?>\n<SOAP-ENV:Envelope\n  \n  xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"\n  xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"\n  xmlns:xsd3="http://www.w3.org/2001/XMLSchema"\n  xmlns:xsi3="http://www.w3.org/2001/XMLSchema-instance"\n>\n<SOAP-ENV:Body >\n<ns1:GetParameterValuesResponse xmlns:ns1="urn:AxessInterface">\n<Result>\n<code xsi3:type="xsd3:int">200</code>\n<details SOAP-ENC:arrayType="ns1:KeyValueStruct[1]" xsi3:type="SOAP-ENC:Array">\n<item>\n<key xsi3:type="xsd3:string">Device.DeviceInfo.FirstUseDate</key>\n<value SOAP-ENC:arrayType="xsd3:ur-type[6]" xsi3:type="SOAP-ENC:Array">\n<item xsi3:type="xsd3:integer">1</item>\n<item xsi3:type="xsd3:integer">1</item>\n<item xsi3:type="xsd3:integer">1</item>\n<item xsi3:type="xsd3:integer">0</item>\n<item xsi3:type="xsd3:integer">0</item>\n<item xsi3:type="xsd3:double">0.0</item>\n</value>\n</item>\n</details>\n<message xsi3:type="xsd3:string">OK</message>\n<ticketid xsi3:type="xsd3:int">208900</ticketid>\n</Result>\n</ns1:GetParameterValuesResponse>\n</SOAP-ENV:Body>\n</SOAP-ENV:Envelope>\n'
+
+#to verify dateTime 0001-01-01T00:00:00
+response_7 = Response(content_7, text_7)
+
+out_7 = [{
+    'key': 'Device.DeviceInfo.FirstUseDate',
+    'type': 'dateTime',
+    'value': '1-01-01T00:00:00'
+}]
+
+text_8 = '<?xml version="1.0" encoding="UTF-8"?>\n<SOAP-ENV:Envelope\n  \n  xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"\n  xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"\n  xmlns:xsd3="http://www.w3.org/2001/XMLSchema"\n  xmlns:xsi3="http://www.w3.org/2001/XMLSchema-instance"\n>\n<SOAP-ENV:Body >\n<ns1:SetParameterValuesResponse xmlns:ns1="urn:AxessInterface">\n<Result>\n<code xsi3:type="xsd3:int">200</code>\n<details SOAP-ENC:arrayType="ns1:KeyValueStruct[1]" xsi3:type="SOAP-ENC:Array">\n<item>\n<key xsi3:type="xsd3:string">Status</key>\n<value xsi3:type="xsd3:string">0</value>\n</item>\n</details>\n<message xsi3:type="xsd3:string">OK</message>\n<ticketid xsi3:type="xsd3:int">209094</ticketid>\n</Result>\n</ns1:SetParameterValuesResponse>\n</SOAP-ENV:Body>\n</SOAP-ENV:Envelope>\n'
+
+content_8 = b'<?xml version="1.0" encoding="UTF-8"?>\n<SOAP-ENV:Envelope\n  \n  xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/"\n  xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/"\n  xmlns:xsd3="http://www.w3.org/2001/XMLSchema"\n  xmlns:xsi3="http://www.w3.org/2001/XMLSchema-instance"\n>\n<SOAP-ENV:Body >\n<ns1:SetParameterValuesResponse xmlns:ns1="urn:AxessInterface">\n<Result>\n<code xsi3:type="xsd3:int">200</code>\n<details SOAP-ENC:arrayType="ns1:KeyValueStruct[1]" xsi3:type="SOAP-ENC:Array">\n<item>\n<key xsi3:type="xsd3:string">Status</key>\n<value xsi3:type="xsd3:string">0</value>\n</item>\n</details>\n<message xsi3:type="xsd3:string">OK</message>\n<ticketid xsi3:type="xsd3:int">209094</ticketid>\n</Result>\n</ns1:SetParameterValuesResponse>\n</SOAP-ENV:Body>\n</SOAP-ENV:Envelope>\n'
+
+#spv positive scenario
+response_8 = Response(content_8, text_8)
+
+out_8 = [{'key': 'Status', 'type': 'string', 'value': '0'}]
+
 
 @pytest.mark.parametrize("test_parse_soap_response, expected_result", [
     (response_1, out_1),
+    (response_6, out_6),
+    (response_7, out_7),
+    (response_8, out_8),
 ])
 def test_parse_soap_response(test_parse_soap_response, expected_result):
     assert expected_result == AxirosACS._parse_soap_response(
