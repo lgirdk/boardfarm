2d63916b3b0c035112ae764e7b301688fe210b96
PREM KUMAR K N <prekumar.contractor@libertyglobal.com>
2020-05-07 19:57:39 +0200

    boardfarm: lib: ConfigHelper: Added SchemaValidator class
     - Added function to validate json files against schemas
    
    Signed-off-by: PREM KUMAR K N <prekumar.contractor@libertyglobal.com>
    Change-Id: I2b6744358287aeef9e177d1fb73dbaaa68ea7a5a
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5330
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/lib/ConfigHelper.py
+++ boardfarm/lib/ConfigHelper.py
@@ -1,12 +1,13 @@
 import traceback
+from json import load
 
+import jsonschema
 from boardfarm.exceptions import ConfigKeyError
 
 
 class ConfigHelper(dict):
     '''
     Accessing the board (station) configuration will soon go through this class.
-
     Getters and Setters will be here, rather than accessing the dict directly
     so that some control can be maintained.
     '''
@@ -42,3 +43,32 @@ class ConfigHelper(dict):
 
     def get_station(self):
         return dict.__getitem__(self, 'station')
+
+
+class SchemaValidator(object):
+    ''' Validates the json files against the schema provided '''
+    def __init__(self, schemapath, schemaname):
+
+        with open(schemapath + schemaname, encoding='utf-8') as f:
+            self.schema_file = load(f)
+
+        self.resolver = jsonschema.RefResolver(base_uri='file://' +
+                                               schemapath + '/',
+                                               referrer=self.schema_file)
+
+    def validate_json_schema(self, jsonpath, jsonname):
+        with open(jsonpath + jsonname, encoding='utf-8') as f:
+            json_entry = load(f)
+        try:
+            jsonschema.validate(json_entry,
+                                self.schema_file,
+                                resolver=self.resolver,
+                                format_checker=jsonschema.FormatChecker())
+            print("ok -", jsonname)
+        except jsonschema.exceptions.ValidationError as error:
+            print("not ok -", jsonname)
+            print("Error: " + error.message + " in " + str(error.path))
+
+    def validate_json_schema_dict(self, dict):
+        # place holder to validate json dictionary
+        pass
