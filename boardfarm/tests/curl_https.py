d9ab5b0fd1489eb6860b8193c796ecbd18f9a5a7
sdutta.contractor <sdutta.contractor@libertyglobal.com>
2020-05-18 09:33:11 +0000

    tests: curl_https: Resolved flake8 issue
    Signed-off-by: sdutta.contractor <sdutta.contractor@libertyglobal.com>
    Change-Id: I42b82138a5de1d801f2bdc5f822304d8afc2e527
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5682
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/curl_https.py
+++ boardfarm/tests/curl_https.py
@@ -3,61 +3,71 @@ from boardfarm.tests import rootfs_boot
 
 
 class CurlSSLGood(rootfs_boot.RootFSBootTest):
-    '''Curl can access https and verify signature.'''
+    """Curl can access https and verify signature."""
     def runTest(self):
+        """Start test to curl to access https and verify signature."""
         board = self.dev.board
 
-        board.sendline('\n')
+        board.sendline("\n")
         board.expect(prompt)
-        board.sendline('opkg install ca-certificates')
+        board.sendline("opkg install ca-certificates")
         board.expect(prompt)
         checks = [
-            'https://sha256.badssl.com/',
-            'https://1000-sans.badssl.com/',
-            'https://mozilla-modern.badssl.com/',
-            'https://dh2048.badssl.com/',
-            'https://hsts.badssl.com/',
-            'https://upgrade.badssl.com/',
-            'https://preloaded-hsts.badssl.com/',
+            "https://sha256.badssl.com/",
+            "https://1000-sans.badssl.com/",
+            "https://mozilla-modern.badssl.com/",
+            "https://dh2048.badssl.com/",
+            "https://hsts.badssl.com/",
+            "https://upgrade.badssl.com/",
+            "https://preloaded-hsts.badssl.com/",
         ]
         for check in checks:
-            board.sendline('curl ' + check)
-            board.expect('<!DOCTYPE html>')
+            board.sendline("curl " + check)
+            board.expect("<!DOCTYPE html>")
             board.expect(prompt)
-            print('\n\nCurl downloaded ' + check + ' as expected\n')
+            print("\n\nCurl downloaded " + check + " as expected\n")
 
 
 class CurlSSLBad(rootfs_boot.RootFSBootTest):
-    '''Curl can't access https with bad signature.'''
+    """Curl can't access https with bad signature."""
     def runTest(self):
+        """Start test to curl can't access https with bad signature."""
         board = self.dev.board
 
-        board.sendline('\n')
+        board.sendline("\n")
         board.expect(prompt)
-        board.sendline('opkg install ca-certificates')
+        board.sendline("opkg install ca-certificates")
         board.expect(prompt)
         checks = [
-            ('https://expired.badssl.com/', 'certificate has expired'),
-            ('https://wrong.host.badssl.com/',
-             'no alternative certificate subject name matches target host name'
-             ),
-            ('https://subdomain.preloaded-hsts.badssl.com/',
-             'no alternative certificate subject name matches target host name'
-             ),
-            ('https://self-signed.badssl.com/',
-             'unable to get local issuer certificate'),
-            ('https://superfish.badssl.com/',
-             'unable to get local issuer certificate'),
-            ('https://edellroot.badssl.com/',
-             'unable to get local issuer certificate'),
-            ('https://dsdtestprovider.badssl.com/',
-             'unable to get local issuer certificate'),
-            ('https://incomplete-chain.badssl.com/',
-             'unable to get local issuer certificate'),
+            ("https://expired.badssl.com/", "certificate has expired"),
+            (
+                "https://wrong.host.badssl.com/",
+                "no alternative certificate subject name matches target host name",
+            ),
+            (
+                "https://subdomain.preloaded-hsts.badssl.com/",
+                "no alternative certificate subject name matches target host name",
+            ),
+            (
+                "https://self-signed.badssl.com/",
+                "unable to get local issuer certificate",
+            ),
+            ("https://superfish.badssl.com/",
+             "unable to get local issuer certificate"),
+            ("https://edellroot.badssl.com/",
+             "unable to get local issuer certificate"),
+            (
+                "https://dsdtestprovider.badssl.com/",
+                "unable to get local issuer certificate",
+            ),
+            (
+                "https://incomplete-chain.badssl.com/",
+                "unable to get local issuer certificate",
+            ),
         ]
         for check in checks:
-            board.sendline('curl ' + check[0])
+            board.sendline("curl " + check[0])
             board.expect(check[1])
             board.expect(prompt)
-            print('\n\nCurl refused to download ' + check[0] +
-                  ' as expected\n')
+            print("\n\nCurl refused to download " + check[0] +
+                  " as expected\n")
