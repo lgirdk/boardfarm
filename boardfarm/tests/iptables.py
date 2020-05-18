0443f8c6dcb3aa083ae8dc7cffb3d9e7e6e80f83
sdutta.contractor <sdutta.contractor@libertyglobal.com>
2020-05-18 09:21:54 +0000

    tests: iptables: Resolved flake8 issue
    Signed-off-by: sdutta.contractor <sdutta.contractor@libertyglobal.com>
    Change-Id: I705834773fa823e07393b27bd7d1af13cae91697
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5680
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/iptables.py
+++ boardfarm/tests/iptables.py
@@ -4,36 +4,39 @@ from boardfarm.tests import rootfs_boot
 
 
 class IPTablesDump(rootfs_boot.RootFSBootTest):
-    '''Dumps all IPTables rules with stats'''
+    """Dumps all IPTables rules with stats."""
     def runTest(self):
+        """Start Dumping all IPTables rules with stats."""
         board = self.dev.board
 
         pp = board.get_pp_dev()
-        with open(os.path.join(self.config.output_dir, 'iptables.log'),
-                  'w') as ipt_log:
-            for tbl in ['filter', 'nat', 'mangle', 'raw', 'security']:
-                pp.sendline('iptables -n -t %s -L -v; echo DONE' % tbl)
-                pp.expect_exact('echo DONE')
-                pp.expect_exact('DONE')
+        with open(os.path.join(self.config.output_dir, "iptables.log"),
+                  "w") as ipt_log:
+            for tbl in ["filter", "nat", "mangle", "raw", "security"]:
+                pp.sendline("iptables -n -t %s -L -v; echo DONE" % tbl)
+                pp.expect_exact("echo DONE")
+                pp.expect_exact("DONE")
                 ipt_log.write(pp.before)
                 pp.expect(pp.prompt)
 
 
 class IPTablesFlushMangle(rootfs_boot.RootFSBootTest):
-    '''Flushes mangle table'''
+    """Flushes mangle table."""
     def runTest(self):
+        """Start flushing mangle table."""
         board = self.dev.board
 
         pp = board.get_pp_dev()
-        pp.sendline('iptables -t mangle -F; iptables -t mangle -X')
+        pp.sendline("iptables -t mangle -F; iptables -t mangle -X")
         pp.expect(pp.prompt)
 
 
 class IPTablesResetCounters(rootfs_boot.RootFSBootTest):
-    '''Reset iptables counters'''
+    """Reset iptables counters."""
     def runTest(self):
+        """Start reseting iptables counters."""
         board = self.dev.board
 
         pp = board.get_pp_dev()
-        pp.sendline('iptables -Z')
+        pp.sendline("iptables -Z")
         pp.expect(pp.prompt)
