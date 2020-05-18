f2677c96de182f073860d3a8ac56e497825b8d32
sdutta.contractor <sdutta.contractor@libertyglobal.com>
2020-05-18 05:46:30 +0000

    tests: netperf_rfc2544: Resolved Flake8 issue
    Signed-off-by: sdutta.contractor <sdutta.contractor@libertyglobal.com>
    Change-Id: Ib63985a964900b473afe614931dae1ee41d1772f
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5667
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/netperf_rfc2544.py
+++ boardfarm/tests/netperf_rfc2544.py
@@ -10,18 +10,19 @@ from boardfarm.tests import rootfs_boot
 
 
 class NetperfRFC2544(rootfs_boot.RootFSBootTest):
-    '''Single test to simulate RFC2544'''
+    """Single test to simulate RFC2544."""
     def runTest(self):
+        """Single test to simulate RFC2544."""
         board = self.dev.board
         lan = self.dev.lan
 
         for sz in ["74", "128", "256", "512", "1024", "1280", "1518"]:
             print("running %s UDP test" % sz)
             lan.sendline(
-                'netperf -H 192.168.0.1 -t UDP_STREAM -l 60 -- -m %s' % sz)
+                "netperf -H 192.168.0.1 -t UDP_STREAM -l 60 -- -m %s" % sz)
             lan.expect_exact(
-                'netperf -H 192.168.0.1 -t UDP_STREAM -l 60 -- -m %s' % sz)
-            lan.expect('UDP UNIDIRECTIONAL')
+                "netperf -H 192.168.0.1 -t UDP_STREAM -l 60 -- -m %s" % sz)
+            lan.expect("UDP UNIDIRECTIONAL")
             lan.expect(prompt, timeout=90)
             board.sendline()
             board.expect(prompt)
