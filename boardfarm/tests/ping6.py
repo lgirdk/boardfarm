1aab74220c0dfce39d1a1da4cae4c2e5bd4298ab
sdutta.contractor <sdutta.contractor@libertyglobal.com>
2020-05-18 10:35:05 +0000

    tests: ping6: Resolved flake8
    Signed-off-by: sdutta.contractor <sdutta.contractor@libertyglobal.com>
    Change-Id: Ieda055605fd7eabf6f7e5b5bbff98a0f440ca719
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5685
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/ping6.py
+++ boardfarm/tests/ping6.py
@@ -10,27 +10,29 @@ from boardfarm.tests import rootfs_boot
 
 
 class LanDevPing6Router(rootfs_boot.RootFSBootTest):
-    '''Device on LAN can ping6 router.'''
+    """Device on LAN can ping6 router."""
     def runTest(self):
+        """Start running test on LAN Device to ping6 router."""
         lan = self.dev.lan
 
-        lan.sendline('\nping6 -c 20 4aaa::1')
-        lan.expect('PING ')
-        lan.expect(' ([0-9]+) (packets )?received')
+        lan.sendline("\nping6 -c 20 4aaa::1")
+        lan.expect("PING ")
+        lan.expect(" ([0-9]+) (packets )?received")
         n = int(lan.match.group(1))
         lan.expect(prompt)
         assert n > 0
 
 
 class LanDevPing6WanDev(rootfs_boot.RootFSBootTest):
-    '''Device on LAN can ping6 through router.'''
+    """Device on LAN can ping6 through router."""
     def runTest(self):
+        """Start running test to make Lan device ping Wan-device."""
         lan = self.dev.lan
 
         # Make Lan-device ping Wan-Device
-        lan.sendline('\nping6 -c 20 5aaa::6')
-        lan.expect('PING ')
-        lan.expect(' ([0-9]+) (packets )?received')
+        lan.sendline("\nping6 -c 20 5aaa::6")
+        lan.expect("PING ")
+        lan.expect(" ([0-9]+) (packets )?received")
         n = int(lan.match.group(1))
         lan.expect(prompt)
         assert n > 0
