7e9c5856a42883d3a975c304e4f006749e28e09a
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-18 09:34:23 +0000

    boardfarm: plugins: fixing flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: I8b6b3b7bde1175fad5b22e798ac8ae7b95991394
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5666
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/plugins.py
+++ boardfarm/plugins.py
@@ -1,15 +1,17 @@
+"""module: boardfarm.plugins."""
 import importlib
 import pkgutil
 
 
 def find_plugins():
-    '''
-    Returns a dictionary of all boardfarm plugins.
+    """
+    Return a dictionary of all boardfarm plugins.
+
     key = name of plugin (string)
     value = imported module
-    '''
+    """
     result = {}
     for finder, name, ispkg in pkgutil.iter_modules():
-        if name.startswith('boardfarm_'):
+        if name.startswith("boardfarm_"):
             result[name] = importlib.import_module(name)
     return result
