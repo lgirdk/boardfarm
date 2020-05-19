72ed2626b8886cb694aa87a579d433162f8c59ae
sdutta.contractor <sdutta.contractor@libertyglobal.com>
2020-05-19 07:21:46 +0000

    tests: uci_wireless: Resolved flake8 issue
    Signed-off-by: sdutta.contractor <sdutta.contractor@libertyglobal.com>
    Change-Id: I966fb78f94f7eb8ba22d5e6515334396fc358080
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5711
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/uci_wireless.py
+++ boardfarm/tests/uci_wireless.py
@@ -13,20 +13,21 @@ from boardfarm.tests import rootfs_boot
 
 
 class UciShowWireless(rootfs_boot.RootFSBootTest):
-    '''UCI lists wifi interfaces.'''
+    """UCI lists wifi interfaces."""
     def runTest(self):
+        """Start test to list wifi interfaces by UCI."""
         board = self.dev.board
 
         wlan_iface = wifi_interface(board)
         if wlan_iface is None:
             self.skipTest("No wifi interfaces detected, skipping..")
 
-        board.sendline('\nuci show wireless')
-        board.expect('uci show wireless')
+        board.sendline("\nuci show wireless")
+        board.expect("uci show wireless")
         board.expect(prompt)
-        wifi_interfaces = re.findall('wireless.*=wifi-device', board.before)
+        wifi_interfaces = re.findall("wireless.*=wifi-device", board.before)
         self.result_message = "UCI shows %s wifi interface(s)." % (
             len(wifi_interfaces))
-        self.logged['num_ifaces'] = len(wifi_interfaces)
+        self.logged["num_ifaces"] = len(wifi_interfaces)
         # Require that at least one wifi interface is present
         assert len(wifi_interfaces) > 0
