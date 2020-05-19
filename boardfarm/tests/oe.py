42783da690f87aaa9482bbc02ab5e7eebfbcd5d7
sdutta.contractor <sdutta.contractor@libertyglobal.com>
2020-05-19 05:26:39 +0000

    tests: oe.py:Resolved  Flake8 issues
    Signed-off-by: sdutta.contractor <sdutta.contractor@libertyglobal.com>
    Change-Id: Iab756cbb97d7e3eba206bc240a67c7c89cf435ee
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5706
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/oe.py
+++ boardfarm/tests/oe.py
@@ -1,7 +1,7 @@
-# Copyright (c) 2015
-#
+# Copyright (c) 2015.
+
 # All rights reserved.
-#
+
 # This file is distributed under the Clear BSD license.
 # The full text can be found in LICENSE in the root directory.
 
@@ -10,15 +10,16 @@ from boardfarm.tests import rootfs_boot
 
 
 class OEVersion(rootfs_boot.RootFSBootTest):
-    '''Record OE version'''
+    """Record OE version."""
     def runTest(self):
+        """Start test to record OE version."""
         board = self.dev.board
 
-        board.sendline('cat /etc/os-release')
+        board.sendline("cat /etc/os-release")
         # PRETTY_NAME=RDK (A Yocto Project 1.6 based Distro) 2.0 (krogoth)
         if 0 == board.expect([
                 "cat: can't open '/etc/os-release': No such file or directory",
-                'PRETTY_NAME=([^\s]*) \(A Yocto Project (?:[^\s]*?)\s?based Distro\) ([^\s]*) \(([^\)]*)\)'
+                r"PRETTY_NAME=([^\s]*) \(A Yocto Project (?:[^\s]*?)\s?based Distro\) ([^\s]*) \(([^\)]*)\)",
         ]):
             self.skipTest("Skipping, not not an OE based distro")
 
@@ -44,9 +45,10 @@ class OEVersion(rootfs_boot.RootFSBootTest):
         print("oe-version-string = %s" % oe_version_string)
         print("#########################################")
 
-        self.result_message="BSP = %s, BSP version = %s, OE version = %s, OE version string = %s" % \
-                (bsp_type, bsp_version, oe_version, oe_version_string)
-        self.logged['bsp-type'] = bsp_type
-        self.logged['oe-version'] = oe_version
-        self.logged['bsp-version'] = bsp_version
-        self.logged['oe-version-string'] = oe_version_string
+        self.result_message = (
+            "BSP = %s, BSP version = %s, OE version = %s, OE version string = %s"
+            % (bsp_type, bsp_version, oe_version, oe_version_string))
+        self.logged["bsp-type"] = bsp_type
+        self.logged["oe-version"] = oe_version
+        self.logged["bsp-version"] = bsp_version
+        self.logged["oe-version-string"] = oe_version_string
