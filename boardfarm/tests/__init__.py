9e994eb92d77664ba4924007d2aa56cb9ce605a5
sdutta.contractor <sdutta.contractor@libertyglobal.com>
2020-05-19 05:12:04 +0000

    tests: __init__.py: Resolved flake8 issues
    Signed-off-by: sdutta.contractor <sdutta.contractor@libertyglobal.com>
    Change-Id: Idc1c2b761aa95b2e56d801fb80427b6713ad6206
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5705
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/__init__.py
+++ boardfarm/tests/__init__.py
@@ -18,11 +18,10 @@ available_tests = {}
 
 
 def init(config=None):
-    '''
-    Dynamically find all test classes accross all boardfarm projects.
-    This creates a dictionary of "test names" to "python object of test class".
-    '''
+    """Dynamically find all test classes accross all boardfarm projects.
 
+    This creates a dictionary of "test names" to "python object of test class".
+    """
     # This will be a dictionary where:
     #   key = filename (without '.py')
     # value = list of classes
@@ -32,19 +31,19 @@ def init(config=None):
     all_boardfarm_modules = dict(
         boardfarm.plugins
     )  # use dict() to create a copy instead of a reference
-    all_boardfarm_modules['boardfarm'] = importlib.import_module('boardfarm')
+    all_boardfarm_modules["boardfarm"] = importlib.import_module("boardfarm")
 
     # Loop over all modules to import their tests
     for modname in all_boardfarm_modules:
         # Find all python files in 'tests' directories
         location = os.path.join(
-            os.path.dirname(all_boardfarm_modules[modname].__file__), 'tests')
-        file_names = glob.glob(os.path.join(location, '*.py'))
+            os.path.dirname(all_boardfarm_modules[modname].__file__), "tests")
+        file_names = glob.glob(os.path.join(location, "*.py"))
         file_names = [
-            os.path.basename(x)[:-3] for x in file_names if not "__" in x
+            os.path.basename(x)[:-3] for x in file_names if "__" not in x
         ]
         for fname in sorted(file_names):
-            tmp = '%s.tests.%s' % (modname, fname)
+            tmp = "%s.tests.%s" % (modname, fname)
             try:
                 module = importlib.import_module(tmp)
             except Exception:
@@ -52,12 +51,12 @@ def init(config=None):
                 raise TestImportError(
                     "Error: Could not import from test file %s.py" % fname)
             if fname in test_mappings:
-                print('WARNING: Two test files have the same name, %s.py' %
+                print("WARNING: Two test files have the same name, %s.py" %
                       fname)
             test_mappings[fname] = []
             for thing_name in dir(module):
                 thing = getattr(module, thing_name)
-                if inspect.isclass(thing) and hasattr(thing, 'run'):
+                if inspect.isclass(thing) and hasattr(thing, "run"):
                     test_mappings[fname].append(thing)
 
     # Loop over all test classes in all test files, and
@@ -72,7 +71,7 @@ def init(config=None):
                     for new_test in new_tests:
                         available_tests[new_test.__name__] = new_test
             except Exception:
-                if 'BFT_DEBUG' in os.environ:
+                if "BFT_DEBUG" in os.environ:
                     traceback.print_exc()
                 print("Warning: Failed to run parse function in %s" %
                       inspect.getsourcefile(test))
