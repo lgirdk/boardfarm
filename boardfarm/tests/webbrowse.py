62a86c7b30465a7d50ce4d772f1681a659047e74
sdutta.contractor <sdutta.contractor@libertyglobal.com>
2020-05-19 06:23:46 +0000

    tests: webbrowse: Resolved flake8 issue
    Signed-off-by: sdutta.contractor <sdutta.contractor@libertyglobal.com>
    Change-Id: I7898a836c20cc5ec517314baccb6620345f74706
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5708
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/webbrowse.py
+++ boardfarm/tests/webbrowse.py
@@ -13,53 +13,53 @@ from boardfarm.tests import rootfs_boot
 
 
 class RandomWebBrowse(rootfs_boot.RootFSBootTest):
-    '''Created light web traffic.'''
+    """Created light web traffic."""
     def runTest(self):
+        """Run Test to Create light web traffic."""
         board = self.dev.board
         lan = self.dev.lan
 
         installers.apt_install(lan, "wget")
 
         urls = [
-            'www.amazon.com',
-            'www.apple.com',
-            'www.baidu.com',
-            'www.bing.com',
-            'www.cnn.com',
-            'www.ebay.com',
-            'www.facebook.com',
-            'www.google.com',
-            'www.imdb.com',
-            'www.imgur.com',
-            'www.instagram.com',
-            'www.linkedin.com',
-            'www.microsoft.com',
-            'www.nbcnews.com',
-            'www.netflix.com',
-            'www.pinterest.com',
-            'www.reddit.com',
-            'www.twitter.com',
-            'www.wikipedia.org',
-            'www.yahoo.com',
+            "www.amazon.com",
+            "www.apple.com",
+            "www.baidu.com",
+            "www.bing.com",
+            "www.cnn.com",
+            "www.ebay.com",
+            "www.facebook.com",
+            "www.google.com",
+            "www.imdb.com",
+            "www.imgur.com",
+            "www.instagram.com",
+            "www.linkedin.com",
+            "www.microsoft.com",
+            "www.nbcnews.com",
+            "www.netflix.com",
+            "www.pinterest.com",
+            "www.reddit.com",
+            "www.twitter.com",
+            "www.wikipedia.org",
+            "www.yahoo.com",
         ]
         random.shuffle(urls)
-        user = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0'
-        cmd = "wget -Hp http://%(url)s " + \
-              "-e robots=off " + \
-              "-P /tmp/webbrowse-test " + \
-              "-T 10 " + \
-              "--header='Accept: text/html' " + \
-              "-U '%(user)s' " + \
-              "2>&1 | tail -1"
+        user = (
+            "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:39.0) Gecko/20100101 Firefox/39.0"
+        )
+        cmd = ("wget -Hp http://%(url)s " + "-e robots=off " +
+               "-P /tmp/webbrowse-test " + "-T 10 " +
+               "--header='Accept: text/html' " + "-U '%(user)s' " +
+               "2>&1 | tail -1")
         for url in urls:
             lan.sendline("mkdir -p /tmp/webbrowse-test")
             print("\n%s" % url)
-            tmp = cmd % {'url': url, 'user': user}
+            tmp = cmd % {"url": url, "user": user}
             lan.sendline(tmp)
             try:
-                lan.expect('Downloaded:', timeout=20)
+                lan.expect("Downloaded:", timeout=20)
             except Exception:
-                lan.sendcontrol('c')
+                lan.sendcontrol("c")
             lan.expect(prompt)
             lan.sendline("rm -rf /tmp/webbrowse-test")
             lan.expect(prompt)
