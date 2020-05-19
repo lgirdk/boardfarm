22d7cc3dcde4b306285d73822bd3ab310d2a644e
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-19 06:37:24 +0000

    Boardfarm:Boardfarm.py fixed flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: I40b5b634352e05e9f293d90457afa8f258ec03d2
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5709
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/Boardfarm.py
+++ boardfarm/Boardfarm.py
@@ -15,50 +15,39 @@ from .dbclients import boardfarmwebclient
 
 
 class Boardfarm(object):
-    '''
-    This class makes it easy to interact with the boardfarm server, and to run tests.
-    '''
+    """This class makes it easy to interact with the boardfarm server, and to run tests."""
     def __init__(self, bfconfig_url, debug=False):
-        '''
-        Parameters:
-        bfconfig_url: location of boardfarm config file.
+        """Parameters: bfconfig_url: location of boardfarm config file.
+
         debug: if True, there will be much more verbose output.
-        '''
+        """
         self.bfconfig_url = bfconfig_url
         self.server = boardfarmwebclient.BoardfarmWebClient(
             bfconfig_url, bf_version=boardfarm.__version__, debug=debug)
         self.debug = debug
 
     def list_tests(self):
-        '''
-        Search for all available tests and return a dict.
-        '''
-
+        """Search for all available tests and return a dict."""
         from boardfarm import tests, config
         tests.init(config)
         return tests.available_tests
 
     def list_stations(self):
-        '''
-        Query boardfarm server for stations and return a dict.
-        '''
+        """Query boardfarm server for stations and return a dict."""
         for name in self.server.bf_config:
             print("%s" % name)
 
     def _supported_devices(self):
-        '''
-        Create list of supported device names that can be used in tests.
-        '''
+        """Create list of supported device names that can be used in tests."""
         from boardfarm import devices
+
         devs = []
         for f in devices.device_mappings:
             devs += [n.__name__ for n in devices.device_mappings[f]]
         return sorted(devs)
 
     def run_bft(self, board_type, testsuite):
-        '''
-        Runs the boardfarm tests using the 'bft' command-line tool.
-        '''
+        """Run the boardfarm tests using the 'bft' command-line tool."""
         output_dir = os.path.join(os.getcwd(), "results")
         print("Trying to run bft ...")
         cmd = ""
