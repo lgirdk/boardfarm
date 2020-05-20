e3f9b29abc654da998a384c1db99d91e5bf0c826
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-20 10:42:26 +0000

    tests:hping3: Fixed flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: If1667c817b01de28b5c73d3ee7aa549609f627c9
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5878
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/hping3.py
+++ boardfarm/tests/hping3.py
@@ -5,7 +5,7 @@ from boardfarm.tests import rootfs_boot
 
 
 class hping3_basic_udp(rootfs_boot.RootFSBootTest):
-    '''Floods hping3, creating lots of firewall entries in router'''
+    """Floods hping3, creating lots of firewall entries in router."""
 
     conn_rate = "u2000"
     conns = 20000
@@ -17,15 +17,15 @@ class hping3_basic_udp(rootfs_boot.RootFSBootTest):
 
         install_hping3(lan)
         wan_ip = wan.get_interface_ipaddr(wan.iface_dut)
-        wan.sendline('nc -lvu %s 565' % wan_ip)
-        wan.expect_exact('nc -lvu %s 565' % wan_ip)
+        wan.sendline("nc -lvu %s 565" % wan_ip)
+        wan.expect_exact("nc -lvu %s 565" % wan_ip)
 
-        board.collect_stats(stats=['mpstat'])
+        board.collect_stats(stats=["mpstat"])
 
         # dest ip and port are fixed, random src port, fixed src ip, 100 us between
-        lan.sendline('hping3 -2 -c %s -d 120 -S -w 64 -p 445 -i %s %s' %
+        lan.sendline("hping3 -2 -c %s -d 120 -S -w 64 -p 445 -i %s %s" %
                      (self.conns, self.conn_rate, wan_ip))
-        lan.expect('HPING')
+        lan.expect("HPING")
 
         self.max_conns = 0
         for _ in range(10):
@@ -43,26 +43,28 @@ class hping3_basic_udp(rootfs_boot.RootFSBootTest):
         wan = self.dev.wan
         lan = self.dev.lan
 
-        lan.sendcontrol('c')
+        lan.sendcontrol("c")
         lan.expect(prompt)
-        lan.sendline('pkill -9 -f hping3')
-        lan.expect_exact('pkill -9 -f hping3')
+        lan.sendline("pkill -9 -f hping3")
+        lan.expect_exact("pkill -9 -f hping3")
         lan.expect(prompt)
 
-        wan.sendcontrol('c')
+        wan.sendcontrol("c")
         wan.expect(prompt)
-        wan.sendline('pkill -9 -f nc ')
-        wan.expect_exact('pkill -9 -f nc')
+        wan.sendline("pkill -9 -f nc ")
+        wan.expect_exact("pkill -9 -f nc")
         wan.expect(prompt)
 
         board.parse_stats(dict_to_log=self.logged)
 
-        args = (self.conn_rate, self.max_conns, self.logged['mpstat'])
-        self.result_message = "hping3 udp firewall test, conn_rate = %s, max_conns = %s, cpu usage = %.2f" % args
+        args = (self.conn_rate, self.max_conns, self.logged["mpstat"])
+        self.result_message = (
+            "hping3 udp firewall test, conn_rate = %s, max_conns = %s, cpu usage = %.2f"
+            % args)
 
 
 class hping3_basic_udp_long(hping3_basic_udp):
-    '''Floods hping3, creating lots of firewall entries in router'''
+    """Floods hping3, creating lots of firewall entries in router."""
 
     conn_rate = "u2000"
     conns = "60000"
