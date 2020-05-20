678bb4883f3db306aaca0156ccf916614441f5e5
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-20 11:07:26 +0000

    tests: ping: Fixed flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: Iba30307ce44ea6ebf3219c6bf5a241981701a6af
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5882
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/ping.py
+++ boardfarm/tests/ping.py
@@ -10,89 +10,89 @@ from boardfarm.tests import rootfs_boot
 
 
 class RouterPingWanDev(rootfs_boot.RootFSBootTest):
-    '''Router can ping device through WAN interface.'''
+    """Router can ping device through WAN interface."""
     def runTest(self):
         board = self.dev.board
         wan = self.dev.wan
         if not wan:
-            msg = 'No WAN Device defined, skipping ping WAN test.'
+            msg = "No WAN Device defined, skipping ping WAN test."
             lib.common.test_msg(msg)
             self.skipTest(msg)
-        board.sendline('\nping -c5 %s' % wan.gw)
-        board.expect('5 (packets )?received', timeout=15)
+        board.sendline("\nping -c5 %s" % wan.gw)
+        board.expect("5 (packets )?received", timeout=15)
         board.expect(board.prompt)
 
     def recover(self):
-        self.dev.board.sendcontrol('c')
+        self.dev.board.sendcontrol("c")
 
 
 class RouterPingInternet(rootfs_boot.RootFSBootTest):
-    '''Router can ping internet address by IP.'''
+    """Router can ping internet address by IP."""
     def runTest(self):
         board = self.dev.board
-        board.sendline('\nping -c2 8.8.8.8')
-        board.expect('2 (packets )?received', timeout=15)
+        board.sendline("\nping -c2 8.8.8.8")
+        board.expect("2 (packets )?received", timeout=15)
         board.expect(board.prompt)
 
 
 class RouterPingInternetName(rootfs_boot.RootFSBootTest):
-    '''Router can ping internet address by name.'''
+    """Router can ping internet address by name."""
     def runTest(self):
         board = self.dev.board
-        board.sendline('\nping -c2 www.google.com')
-        board.expect('2 (packets )?received', timeout=15)
+        board.sendline("\nping -c2 www.google.com")
+        board.expect("2 (packets )?received", timeout=15)
         board.expect(board.prompt)
 
 
 class LanDevPingRouter(rootfs_boot.RootFSBootTest):
-    '''Device on LAN can ping router.'''
+    """Device on LAN can ping router."""
     def runTest(self):
         board = self.dev.board
         lan = self.dev.lan
         if not lan:
-            msg = 'No LAN Device defined, skipping ping test from LAN.'
+            msg = "No LAN Device defined, skipping ping test from LAN."
             lib.common.test_msg(msg)
             self.skipTest(msg)
         router_ip = board.get_interface_ipaddr(board.lan_iface)
-        lan.sendline('\nping -i 0.2 -c 5 %s' % router_ip)
-        lan.expect('PING ')
-        lan.expect('5 (packets )?received', timeout=15)
+        lan.sendline("\nping -i 0.2 -c 5 %s" % router_ip)
+        lan.expect("PING ")
+        lan.expect("5 (packets )?received", timeout=15)
         lan.expect(lan.prompt)
 
 
 class LanDevPingWanDev(rootfs_boot.RootFSBootTest):
-    '''Device on LAN can ping through router.'''
+    """Device on LAN can ping through router."""
     def runTest(self):
         lan = self.dev.lan
         wan = self.dev.wan
         if not lan:
-            msg = 'No LAN Device defined, skipping ping test from LAN.'
+            msg = "No LAN Device defined, skipping ping test from LAN."
             lib.common.test_msg(msg)
             self.skipTest(msg)
         if not wan:
-            msg = 'No WAN Device defined, skipping ping WAN test.'
+            msg = "No WAN Device defined, skipping ping WAN test."
             lib.common.test_msg(msg)
             self.skipTest(msg)
-        lan.sendline('\nping -i 0.2 -c 5 %s' % wan.gw)
-        lan.expect('PING ')
-        lan.expect('5 (packets )?received', timeout=15)
+        lan.sendline("\nping -i 0.2 -c 5 %s" % wan.gw)
+        lan.expect("PING ")
+        lan.expect("5 (packets )?received", timeout=15)
         lan.expect(lan.prompt)
 
     def recover(self):
-        self.dev.lan.sendcontrol('c')
+        self.dev.lan.sendcontrol("c")
 
 
 class LanDevPingInternet(rootfs_boot.RootFSBootTest):
-    '''Device on LAN can ping through router to internet.'''
+    """Device on LAN can ping through router to internet."""
     def runTest(self):
         lan = self.dev.lan
         if not lan:
-            msg = 'No LAN Device defined, skipping ping test from LAN.'
+            msg = "No LAN Device defined, skipping ping test from LAN."
             lib.common.test_msg(msg)
             self.skipTest(msg)
-        lan.sendline('\nping -c2 8.8.8.8')
-        lan.expect('2 (packets )?received', timeout=10)
+        lan.sendline("\nping -c2 8.8.8.8")
+        lan.expect("2 (packets )?received", timeout=10)
         lan.expect(lan.prompt)
 
     def recover(self):
-        self.dev.lan.sendcontrol('c')
+        self.dev.lan.sendcontrol("c")
