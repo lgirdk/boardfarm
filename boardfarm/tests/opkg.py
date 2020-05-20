98ed87fd27daacb4f3a8f6167dcbc6aa2950cf21
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-20 11:46:39 +0000

    tests:opkg.py : Fixed flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: Ic19520e542c309507104639fd65256bb81b92a8a
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5884
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/opkg.py
+++ boardfarm/tests/opkg.py
@@ -10,40 +10,41 @@ from boardfarm.tests import rootfs_boot
 
 
 class OpkgList(rootfs_boot.RootFSBootTest):
-    '''Opkg list shows installed packages.'''
+    """Opkg list shows installed packages."""
     def runTest(self):
         board = self.dev.board
 
-        board.sendline('\nopkg list-installed | wc -l')
-        board.expect('opkg list')
-        board.expect(r'(\d+)\r\n')
+        board.sendline("\nopkg list-installed | wc -l")
+        board.expect("opkg list")
+        board.expect(r"(\d+)\r\n")
         num_pkgs = int(board.match.group(1))
         board.expect(prompt)
-        board.sendline('opkg list-installed')
+        board.sendline("opkg list-installed")
         board.expect(prompt)
-        self.result_message = '%s OpenWrt packages are installed.' % num_pkgs
-        self.logged['num_installed'] = num_pkgs
+        self.result_message = "%s OpenWrt packages are installed." % num_pkgs
+        self.logged["num_installed"] = num_pkgs
 
 
 class CheckQosScripts(rootfs_boot.RootFSBootTest):
-    '''Package "qos-scripts" is not installed.'''
+    """Package "qos-scripts" is not installed."""
     def runTest(self):
         board = self.dev.board
 
-        board.sendline('\nopkg list | grep qos-scripts')
+        board.sendline("\nopkg list | grep qos-scripts")
         try:
-            board.expect('qos-scripts - ', timeout=4)
-        except:
+            board.expect("qos-scripts - ", timeout=4)
+        except Exception as error:
+            print(error)
             return  # pass if not installed
         assert False  # fail if installed
 
 
 class OpkgUpdate(rootfs_boot.RootFSBootTest):
-    '''Opkg is able to update list of packages.'''
+    """Opkg is able to update list of packages."""
     def runTest(self):
         board = self.dev.board
 
         board.sendline('\nopkg update && echo "All package lists updated"')
-        board.expect('Updated list of available packages')
-        board.expect('All package lists updated')
+        board.expect("Updated list of available packages")
+        board.expect("All package lists updated")
         board.expect(prompt)
