669f7daf97a03e14e77f59a2a7266896f0af9318
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-20 11:02:12 +0000

    tests:connection_stress.py: Fixed flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: If27dfb336595c3b8593932dbf14dfa586fbfab51
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5880
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/connection_stress.py
+++ boardfarm/tests/connection_stress.py
@@ -13,7 +13,7 @@ from boardfarm.tests import rootfs_boot
 
 
 class Connection_Stress(rootfs_boot.RootFSBootTest):
-    '''Measured CPU use while creating thousands of connections.'''
+    """Measured CPU use while creating thousands of connections."""
 
     concurrency = 25
     num_conn = 5000
@@ -27,28 +27,30 @@ class Connection_Stress(rootfs_boot.RootFSBootTest):
         lan = self.dev.lan
 
         install_lighttpd(wan)
-        wan.sendline('/etc/init.d/lighttpd start')
+        wan.sendline("/etc/init.d/lighttpd start")
         wan.expect(prompt)
         # Wan device: Create small file in web dir
-        fname = 'small.txt'
-        cmd = '\nhead -c 10000 /dev/urandom > /var/www/%s' % fname
+        fname = "small.txt"
+        cmd = "\nhead -c 10000 /dev/urandom > /var/www/%s" % fname
         wan.sendline(cmd)
         wan.expect(prompt)
         # Lan Device: download small file a lot
         # TODO: this is actually a 404 for lighthttpd config issues?
-        url = 'http://%s/%s' % (wan.gw, fname)
+        url = "http://%s/%s" % (wan.gw, fname)
         # Start CPU monitor
-        board.collect_stats(stats=['mpstat'])
+        board.collect_stats(stats=["mpstat"])
         # Lan Device: download small file a lot
-        lan.sendline('\nab -dn %s -c %s %s' %
+        lan.sendline("\nab -dn %s -c %s %s" %
                      (self.num_conn, self.concurrency, url))
-        lan.expect('Benchmarking')
+        lan.expect("Benchmarking")
         timeout = 0.05 * self.num_conn
-        if 0 != lan.expect([
-                r'Requests per second:\s+(\d+)',
-                'apr_socket_recv: Connection reset by peer'
-        ],
-                           timeout=timeout):
+        if 0 != lan.expect(
+            [
+                r"Requests per second:\s+(\d+)",
+                "apr_socket_recv: Connection reset by peer",
+            ],
+                timeout=timeout,
+        ):
             raise Exception("ab failed to run")
         self.reqs_per_sec = int(lan.match.group(1))
         lan.expect(prompt)
@@ -59,24 +61,26 @@ class Connection_Stress(rootfs_boot.RootFSBootTest):
         board = self.dev.board
         lan = self.dev.lan
 
-        lan.sendcontrol('c')
+        lan.sendcontrol("c")
         time.sleep(5)  # Give router a few seconds to recover
         board.parse_stats(dict_to_log=self.logged)
-        avg_cpu = self.logged['mpstat']
+        avg_cpu = self.logged["mpstat"]
         msg = "ApacheBench measured %s connections/second, CPU use = %s%%." % (
-            self.reqs_per_sec, avg_cpu)
+            self.reqs_per_sec,
+            avg_cpu,
+        )
         self.result_message = msg
 
 
 class Connection_Stress_Lite(Connection_Stress):
-    '''Measured CPU use while creating thousands of connections.'''
+    """Measured CPU use while creating thousands of connections."""
 
     concurrency = 5
     num_conn = 500
 
 
 class Connection_Stress_Intense(Connection_Stress):
-    '''Measured CPU use while creating thousands of connections.'''
+    """Measured CPU use while creating thousands of connections."""
 
     concurrency = 25
     num_conn = 20000
