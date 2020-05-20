cb88264b075062267cfd8597a31be636a2caa18e
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-20 07:30:21 +0000

    boardfarm:opkg_conf: Fixed Flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: I5daf1ca1413a057244dbf4b6927c614724f2e145
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5863
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/opkg_conf.py
+++ boardfarm/tests/opkg_conf.py
@@ -10,122 +10,122 @@ from boardfarm.tests import rootfs_boot
 
 
 class OpkgConfUpdateMD5(rootfs_boot.RootFSBootTest):
-    '''Check that opkg will overwrite old configuration files with known MD5.'''
+    """Check that opkg will overwrite old configuration files with known MD5."""
     def runTest(self):
         board = self.dev.board
 
-        board.sendline('\ncp /etc/config/ulogd /etc/config/ulogd.bak')
+        board.sendline("\ncp /etc/config/ulogd /etc/config/ulogd.bak")
         board.expect(prompt)
-        board.sendline('echo boardfarmteststring > /etc/config/ulogd')
+        board.sendline("echo boardfarmteststring > /etc/config/ulogd")
         board.expect(prompt)
-        board.sendline('touch -r /etc/config/ulogd.bak /etc/config/ulogd')
+        board.sendline("touch -r /etc/config/ulogd.bak /etc/config/ulogd")
         board.expect(prompt)
         board.sendline(
             'sed -i "s|/etc/config/ulogd .*|/etc/config/ulogd 48b1215c8d419a33818fc1f42c118aed|" /usr/lib/opkg/status'
         )
         board.expect(prompt)
-        board.sendline('opkg install --force-reinstall ulogd')
+        board.sendline("opkg install --force-reinstall ulogd")
         board.expect(prompt)
-        board.sendline('grep boardfarmteststring /etc/config/ulogd || uname')
-        board.expect('Linux')
-        board.sendline('\nmv /etc/config/ulogd.bak /etc/config/ulogd')
+        board.sendline("grep boardfarmteststring /etc/config/ulogd || uname")
+        board.expect("Linux")
+        board.sendline("\nmv /etc/config/ulogd.bak /etc/config/ulogd")
         board.expect(prompt)
 
     def recover(self):
         board = self.dev.board
 
-        board.sendline('\nmv /etc/config/ulogd.bak /etc/config/ulogd')
+        board.sendline("\nmv /etc/config/ulogd.bak /etc/config/ulogd")
         board.expect(prompt)
 
 
 class OpkgConfNotUpdateMD5(rootfs_boot.RootFSBootTest):
-    '''Check that opkg will not overwrite old modified configuration files with known MD5.'''
+    """Check that opkg will not overwrite old modified configuration files with known MD5."""
     def runTest(self):
         board = self.dev.board
 
-        board.sendline('\ncp /etc/config/ulogd /etc/config/ulogd.bak')
+        board.sendline("\ncp /etc/config/ulogd /etc/config/ulogd.bak")
         board.expect(prompt)
-        board.sendline('echo boardfarmteststring > /etc/config/ulogd')
+        board.sendline("echo boardfarmteststring > /etc/config/ulogd")
         board.expect(prompt)
-        board.sendline('touch -r /etc/config/ulogd.bak /etc/config/ulogd')
+        board.sendline("touch -r /etc/config/ulogd.bak /etc/config/ulogd")
         board.expect(prompt)
         board.sendline(
             'sed -i "s|/etc/config/ulogd .*|/etc/config/ulogd aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa|" /usr/lib/opkg/status'
         )
         board.expect(prompt)
-        board.sendline('opkg install --force-reinstall ulogd')
+        board.sendline("opkg install --force-reinstall ulogd")
         board.expect(
-            'Existing conffile /etc/config/ulogd is different from the conffile in the new package'
+            "Existing conffile /etc/config/ulogd is different from the conffile in the new package"
         )
         board.expect(prompt)
-        board.sendline('grep boardfarmteststring /etc/config/ulogd && uname')
-        board.expect('Linux')
-        board.sendline('\nmv /etc/config/ulogd.bak /etc/config/ulogd')
+        board.sendline("grep boardfarmteststring /etc/config/ulogd && uname")
+        board.expect("Linux")
+        board.sendline("\nmv /etc/config/ulogd.bak /etc/config/ulogd")
         board.expect(prompt)
 
     def recover(self):
         board = self.dev.board
 
-        board.sendline('\nmv /etc/config/ulogd.bak /etc/config/ulogd')
+        board.sendline("\nmv /etc/config/ulogd.bak /etc/config/ulogd")
         board.expect(prompt)
 
 
 class OpkgConfUpdateSHA256(rootfs_boot.RootFSBootTest):
-    '''Check that opkg will overwrite old configuration files with known MD5.'''
+    """Check that opkg will overwrite old configuration files with known MD5."""
     def runTest(self):
         board = self.dev.board
 
-        board.sendline('\ncp /etc/config/ulogd /etc/config/ulogd.bak')
+        board.sendline("\ncp /etc/config/ulogd /etc/config/ulogd.bak")
         board.expect(prompt)
-        board.sendline('echo boardfarmteststring > /etc/config/ulogd')
+        board.sendline("echo boardfarmteststring > /etc/config/ulogd")
         board.expect(prompt)
-        board.sendline('touch -r /etc/config/ulogd.bak /etc/config/ulogd')
+        board.sendline("touch -r /etc/config/ulogd.bak /etc/config/ulogd")
         board.expect(prompt)
         board.sendline(
             'sed -i "s|/etc/config/ulogd .*|/etc/config/ulogd 4212ae0a86f553b7aac741a734a0b973193a9fbe179b28a5d8c2a50cc51e25f0|" /usr/lib/opkg/status'
         )
         board.expect(prompt)
-        board.sendline('opkg install --force-reinstall ulogd')
+        board.sendline("opkg install --force-reinstall ulogd")
         board.expect(prompt)
-        board.sendline('grep boardfarmteststring /etc/config/ulogd || uname')
-        board.expect('Linux')
-        board.sendline('\nmv /etc/config/ulogd.bak /etc/config/ulogd')
+        board.sendline("grep boardfarmteststring /etc/config/ulogd || uname")
+        board.expect("Linux")
+        board.sendline("\nmv /etc/config/ulogd.bak /etc/config/ulogd")
         board.expect(prompt)
 
     def recover(self):
         board = self.dev.board
 
-        board.sendline('\nmv /etc/config/ulogd.bak /etc/config/ulogd')
+        board.sendline("\nmv /etc/config/ulogd.bak /etc/config/ulogd")
         board.expect(prompt)
 
 
 class OpkgConfNotUpdateSHA256(rootfs_boot.RootFSBootTest):
-    '''Check that opkg will not overwrite old modified configuration files with known MD5.'''
+    """Check that opkg will not overwrite old modified configuration files with known MD5."""
     def runTest(self):
         board = self.dev.board
 
-        board.sendline('\ncp /etc/config/ulogd /etc/config/ulogd.bak')
+        board.sendline("\ncp /etc/config/ulogd /etc/config/ulogd.bak")
         board.expect(prompt)
-        board.sendline('echo boardfarmteststring > /etc/config/ulogd')
+        board.sendline("echo boardfarmteststring > /etc/config/ulogd")
         board.expect(prompt)
-        board.sendline('touch -r /etc/config/ulogd.bak /etc/config/ulogd')
+        board.sendline("touch -r /etc/config/ulogd.bak /etc/config/ulogd")
         board.expect(prompt)
         board.sendline(
             'sed -i "s|/etc/config/ulogd .*|/etc/config/ulogd aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa|" /usr/lib/opkg/status'
         )
         board.expect(prompt)
-        board.sendline('opkg install --force-reinstall ulogd')
+        board.sendline("opkg install --force-reinstall ulogd")
         board.expect(
-            'Existing conffile /etc/config/ulogd is different from the conffile in the new package'
+            "Existing conffile /etc/config/ulogd is different from the conffile in the new package"
         )
         board.expect(prompt)
-        board.sendline('grep boardfarmteststring /etc/config/ulogd && uname')
-        board.expect('Linux')
-        board.sendline('\nmv /etc/config/ulogd.bak /etc/config/ulogd')
+        board.sendline("grep boardfarmteststring /etc/config/ulogd && uname")
+        board.expect("Linux")
+        board.sendline("\nmv /etc/config/ulogd.bak /etc/config/ulogd")
         board.expect(prompt)
 
     def recover(self):
         board = self.dev.board
 
-        board.sendline('\nmv /etc/config/ulogd.bak /etc/config/ulogd')
+        board.sendline("\nmv /etc/config/ulogd.bak /etc/config/ulogd")
         board.expect(prompt)
