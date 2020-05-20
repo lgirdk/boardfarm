dd326ab0cec749619b3e34140fff6bed0dd1e60e
sdutta.contractor <sdutta.contractor@libertyglobal.com>
2020-05-20 07:29:57 +0000

    tests: igmpv3_basic: resolved flake8 issue
    Signed-off-by: sdutta.contractor <sdutta.contractor@libertyglobal.com>
    Change-Id: Iaac282b78e2bd548894b8bf41aabd029256ec937
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5864
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/igmpv3_basic.py
+++ boardfarm/tests/igmpv3_basic.py
@@ -10,43 +10,46 @@ from boardfarm.tests import rootfs_boot
 
 
 class IGMPv3_Running(rootfs_boot.RootFSBootTest):
-    '''IGMP Proxy daemon mcproxy is up and running.'''
+    """IGMP Proxy daemon mcproxy is up and running."""
     def runTest(self):
+        """Runtest implementation."""
         board = self.dev.board
 
-        board.sendline('\nps | grep mcproxy')
-        board.expect('/usr/sbin/mcproxy -f /etc/mcproxy.conf', timeout=5)
+        board.sendline("\nps | grep mcproxy")
+        board.expect("/usr/sbin/mcproxy -f /etc/mcproxy.conf", timeout=5)
         board.expect(prompt)
 
 
 class IGMPv3_Config(rootfs_boot.RootFSBootTest):
-    '''IGMP Proxy daemon mcproxy config is set correctly.'''
+    """IGMP Proxy daemon mcproxy config is set correctly."""
     def runTest(self):
+        """Runtest implementation."""
         board = self.dev.board
 
-        board.sendline('\nuci show mcproxy')
-        board.expect_exact('mcproxy.config=mcproxy', timeout=5)
-        board.expect_exact('mcproxy.config.protocol=IGMPv3', timeout=5)
-        board.expect_exact('mcproxy.@pinstance[0]=pinstance', timeout=5)
-        board.expect_exact('mcproxy.@pinstance[0].name=mcproxy1', timeout=5)
+        board.sendline("\nuci show mcproxy")
+        board.expect_exact("mcproxy.config=mcproxy", timeout=5)
+        board.expect_exact("mcproxy.config.protocol=IGMPv3", timeout=5)
+        board.expect_exact("mcproxy.@pinstance[0]=pinstance", timeout=5)
+        board.expect_exact("mcproxy.@pinstance[0].name=mcproxy1", timeout=5)
         board.expect(prompt)
-        board.sendline('cat /etc/mcproxy.conf')
-        board.expect('protocol IGMPv3;', timeout=5)
+        board.sendline("cat /etc/mcproxy.conf")
+        board.expect("protocol IGMPv3;", timeout=5)
         board.expect('pinstance mcproxy1: "eth0" ==> "br-lan";')
         board.expect(prompt)
 
 
 class IGMPv3_StopStart(rootfs_boot.RootFSBootTest):
-    '''IGMP Proxy daemon mcproxy can be stopped and started without rebooting.'''
+    """IGMP Proxy daemon mcproxy can be stopped and started without rebooting."""
     def runTest(self):
+        """Runtest implementation."""
         board = self.dev.board
 
-        board.sendline('\n/etc/init.d/mcproxy stop')
+        board.sendline("\n/etc/init.d/mcproxy stop")
         board.expect(prompt)
-        board.sendline('ps | grep mcproxy')
+        board.sendline("ps | grep mcproxy")
         board.expect(prompt)
-        assert '/usr/sbin/mcproxy' not in board.before
-        board.sendline('/etc/init.d/mcproxy start')
-        board.sendline('ps | grep mcproxy')
-        board.expect('/usr/sbin/mcproxy -f /etc/mcproxy.conf', timeout=5)
+        assert "/usr/sbin/mcproxy" not in board.before
+        board.sendline("/etc/init.d/mcproxy start")
+        board.sendline("ps | grep mcproxy")
+        board.expect("/usr/sbin/mcproxy -f /etc/mcproxy.conf", timeout=5)
         board.expect(prompt)
