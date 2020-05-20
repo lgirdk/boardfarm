c503b9407785353247fead5219b5d0c0e3e8996c
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-20 10:53:15 +0000

    tests: ipv6_curl.py: Fixed flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: I58d87b3050118fd2c4f3073be9d4f135bbb3fc1d
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5879
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/ipv6_curl.py
+++ boardfarm/tests/ipv6_curl.py
@@ -10,7 +10,7 @@ from boardfarm.tests import rootfs_boot
 
 
 class IPv6_File_Download(rootfs_boot.RootFSBootTest):
-    '''Downloaded file through router using IPv6.'''
+    """Downloaded file through router using IPv6."""
     def runTest(self):
         wan = self.dev.wan
         lan = self.dev.lan
@@ -19,13 +19,13 @@ class IPv6_File_Download(rootfs_boot.RootFSBootTest):
         fname = "/var/www/20mb.txt"
         wan.sendline('\n[ -e "%s" ] || head -c 20971520 /dev/urandom > %s' %
                      (fname, fname))
-        wan.expect('/var')
+        wan.expect("/var")
         wan.expect(prompt)
         # LAN Device: download the file
         lan.sendline(
-            '\ncurl -m 57 -g -6 http://[5aaa::6]/20mb.txt > /dev/null')
-        lan.expect('Total', timeout=5)
-        i = lan.expect(["couldn't connect", '20.0M  100 20.0M'], timeout=60)
+            "\ncurl -m 57 -g -6 http://[5aaa::6]/20mb.txt > /dev/null")
+        lan.expect("Total", timeout=5)
+        i = lan.expect(["couldn't connect", "20.0M  100 20.0M"], timeout=60)
         if i == 0:
             assert False
         lan.expect(prompt)
