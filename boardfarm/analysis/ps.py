3c2a8d4dc92dba7b9f31553b7b153ef9dc562607
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-21 07:49:31 +0000

    analysis: ps: Fixed flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: I621e477460748097bca0eb7c2c2cc7a60d45dae2
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5918
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/analysis/ps.py
+++ boardfarm/analysis/ps.py
@@ -13,7 +13,7 @@ from . import analysis
 
 
 class PSAnalysis(analysis.Analysis):
-    '''Parse top for ps commands, and create graph of process memory usage over time'''
+    """Parse top for ps commands, and create graph of process memory usage over time."""
     def analyze(self, console_log, output_dir):
         regex = "root\\@OpenWrt:[^#]+# ps.*?(?=root@OpenWrt)"
         results = re.findall(regex, repr(console_log))
@@ -22,33 +22,33 @@ class PSAnalysis(analysis.Analysis):
         data = collections.defaultdict(list)
         timestamps = collections.defaultdict(list)
         for ps_dump in results:
-            for line in ps_dump.split('\\r\\n')[2:]:
-                line = re.sub('](?=[^\s])', '] ', line)
+            for line in ps_dump.split("\\r\\n")[2:]:
+                line = re.sub(r"](?=[^\s])", "] ", line)
                 e = line.split()
                 if len(e) < 4:
                     continue
-                ts = float(e.pop(0).strip('[]'))
+                ts = float(e.pop(0).strip("[]"))
                 pid = e.pop(0)
                 _ = e.pop(0)  # user
                 mem = e.pop(0)
                 while e[0] in [
-                        'S', 'R', 'SW', 'SW<', 'DW', 'N', '<', 'D', 'Z'
+                        "S", "R", "SW", "SW<", "DW", "N", "<", "D", "Z"
                 ]:
                     e.pop(0)
                 cmdline = " ".join(e)
-                if cmdline[0] == '[' and cmdline[-1] == ']':
+                if cmdline[0] == "[" and cmdline[-1] == "]":
                     cmd = cmdline
                 else:
                     cmd = os.path.basename(cmdline.split()[0])
-                key = pid + '-' + cmd
+                key = pid + "-" + cmd
                 data[key].append(mem)
                 timestamps[key].append(ts)
 
         for k in data:
             if len(data[k]) > 1:
                 fname = k
-                for c in r'[]/\;,><&*:%=+@!#^()|?^':
-                    fname = fname.replace(c, '')
+                for c in r"[]/\;,><&*:%=+@!#^()|?^":
+                    fname = fname.replace(c, "")
                 self.make_graph(data[k],
                                 k,
                                 fname,
