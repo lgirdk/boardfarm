3647bca0dbcc9db69a9ab3f4e22c893d839daebe
sdutta.contractor <sdutta.contractor@libertyglobal.com>
2020-05-21 08:52:10 +0000

    tests: oe.py: Resolved flake8 issue
    Signed-off-by: sdutta.contractor <sdutta.contractor@libertyglobal.com>
    Change-Id: I3bbd3db58640c6bc7155a7306dd4baa83e90f484
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5921
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/devices/oe.py
+++ boardfarm/devices/oe.py
@@ -2,15 +2,14 @@ from . import linux
 
 
 class OpenEmbedded(linux.LinuxDevice):
-    """OE core implementation extends LinuxDevice
-    """
+    """OE core implementation extends LinuxDevice."""
     def install_package(self, pkg):
-        """Installs packages over OE
+        """Install packages over OE.
 
         :param self: self object
         :type self: object
         :param pkg: package to be installed.
         :type pkg: string
-        :raises : Exception Not implemented!
+        :raises: Exception Not implemented!
         """
         raise Exception("Not implemented!")
