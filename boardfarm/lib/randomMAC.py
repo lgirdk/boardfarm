1926143a789c2b20598b2bb0e1b6f20f5fc4aea9
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-21 12:08:28 +0000

    lib:randomMAC.py: Fixed flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: I57a30f34aea698515ecfb8e8233a074754a1969a
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5942
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/lib/randomMAC.py
+++ boardfarm/lib/randomMAC.py
@@ -4,21 +4,21 @@ import random
 
 
 def randomMAC():
-    """generates and returns random mac address
+    """Generate and returns random mac address.
 
     :return : random mac address
     :rtype : string
     """
     mac = [
-        (random.randint(0x00, 0xff)
-         & 0xfe),  # the lsb is 0, i.e. no multicat bit
-        random.randint(0x00, 0xff),
-        random.randint(0x00, 0xff),
-        random.randint(0x00, 0xff),
-        random.randint(0x00, 0xff),
-        random.randint(0x00, 0xff)
+        (random.randint(0x00, 0xFF)
+         & 0xFE),  # the lsb is 0, i.e. no multicat bit
+        random.randint(0x00, 0xFF),
+        random.randint(0x00, 0xFF),
+        random.randint(0x00, 0xFF),
+        random.randint(0x00, 0xFF),
+        random.randint(0x00, 0xFF),
     ]
-    mac_to_be_decided = ':'.join(
+    mac_to_be_decided = ":".join(
         map(lambda x: hex(x)[2:].lstrip("0x").zfill(2), mac))
 
-    return (mac_to_be_decided)
+    return mac_to_be_decided
