a776b51076a37ae4ba5a4f508ad2f2af481bc546
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-21 04:33:39 +0000

    tests: wifi: Fixed flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: I48db27902c32a63b881ab21087738347d9f8079b
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5900
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/wifi.py
+++ boardfarm/tests/wifi.py
@@ -2,7 +2,7 @@ from boardfarm.tests import rootfs_boot
 
 
 class WifiScan(rootfs_boot.RootFSBootTest):
-    '''Simple test to run a wifi scan'''
+    """Simple test to run a wifi scan."""
     def runTest(self):
         wlan = self.dev.wlan
 
