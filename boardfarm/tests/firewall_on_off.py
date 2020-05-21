77dc783b94ba7c4f800342f872f49b97f4e00add
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-21 04:36:17 +0000

    tests:firewall_on_off.py: Fixed Flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: I1e65c5901cd1f8fff9e2e41348eda75ed26a756f
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5901
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/firewall_on_off.py
+++ boardfarm/tests/firewall_on_off.py
@@ -10,38 +10,38 @@ from boardfarm.tests import rootfs_boot
 
 
 class FirewallOFF(rootfs_boot.RootFSBootTest):
-    '''Turned router firewall off.'''
+    """Turned router firewall off."""
     def runTest(self):
         board = self.dev.board
 
-        board.sendline('\nuci set firewall.@defaults[0].forward=ACCEPT')
-        board.expect('uci set firewall')
+        board.sendline("\nuci set firewall.@defaults[0].forward=ACCEPT")
+        board.expect("uci set firewall")
         board.expect(prompt)
-        board.sendline('uci set firewall.@zone[0].forward=ACCEPT')
+        board.sendline("uci set firewall.@zone[0].forward=ACCEPT")
         board.expect(prompt)
-        board.sendline('uci set firewall.@zone[1].input=ACCEPT')
+        board.sendline("uci set firewall.@zone[1].input=ACCEPT")
         board.expect(prompt)
-        board.sendline('uci set firewall.@zone[1].forward=ACCEPT')
+        board.sendline("uci set firewall.@zone[1].forward=ACCEPT")
         board.expect(prompt)
-        board.sendline('uci commit firewall')
+        board.sendline("uci commit firewall")
         board.expect(prompt)
         board.firewall_restart()
 
 
 class FirewallON(rootfs_boot.RootFSBootTest):
-    '''Turned router firewall on.'''
+    """Turned router firewall on."""
     def runTest(self):
         board = self.dev.board
 
-        board.sendline('\nuci set firewall.@defaults[0].forward=REJECT')
-        board.expect('uci set firewall')
+        board.sendline("\nuci set firewall.@defaults[0].forward=REJECT")
+        board.expect("uci set firewall")
         board.expect(prompt)
-        board.sendline('uci set firewall.@zone[0].forward=REJECT')
+        board.sendline("uci set firewall.@zone[0].forward=REJECT")
         board.expect(prompt)
-        board.sendline('uci set firewall.@zone[1].input=REJECT')
+        board.sendline("uci set firewall.@zone[1].input=REJECT")
         board.expect(prompt)
-        board.sendline('uci set firewall.@zone[1].forward=REJECT')
+        board.sendline("uci set firewall.@zone[1].forward=REJECT")
         board.expect(prompt)
-        board.sendline('uci commit firewall')
+        board.sendline("uci commit firewall")
         board.expect(prompt)
         board.firewall_restart()
