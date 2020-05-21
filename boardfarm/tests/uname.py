9a6dc107f6455979291fa6e0e29460fdac48d7b0
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-21 05:18:51 +0000

    tests: uname: Fixed flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: I0ab9109d9459a368aa24a8a9e9db5dafd181bfb7
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5905
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/tests/uname.py
+++ boardfarm/tests/uname.py
@@ -9,23 +9,23 @@ from boardfarm.tests import rootfs_boot
 
 
 class Uname(rootfs_boot.RootFSBootTest):
-    '''Checked board system information.'''
+    """Checked board system information."""
     def runTest(self):
         board = self.dev.board
-        board.sendline('\nuname -a')
-        board.expect('uname -a', timeout=6)
+        board.sendline("\nuname -a")
+        board.expect("uname -a", timeout=6)
         board.expect(board.prompt)
         info = board.before.lstrip().rstrip()
         self.result_message = info
-        board.sendline('uname -m')
-        board.expect('uname -m')
+        board.sendline("uname -m")
+        board.expect("uname -m")
         board.expect(board.prompt)
-        self.logged['machine'] = board.before.lstrip().rstrip()
-        board.sendline('uname -r')
-        board.expect('uname -r')
+        self.logged["machine"] = board.before.lstrip().rstrip()
+        board.sendline("uname -r")
+        board.expect("uname -r")
         board.expect(board.prompt)
-        self.logged['release'] = board.before.lstrip().rstrip()
-        board.sendline('uname -s')
-        board.expect('uname -s')
+        self.logged["release"] = board.before.lstrip().rstrip()
+        board.sendline("uname -s")
+        board.expect("uname -s")
         board.expect(board.prompt)
-        self.logged['kernel'] = board.before.lstrip().rstrip()
+        self.logged["kernel"] = board.before.lstrip().rstrip()
