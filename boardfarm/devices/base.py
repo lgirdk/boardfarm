dd4a32b5ddb20058abc6c5516774eae81dde3c52
sdutta.contractor <sdutta.contractor@libertyglobal.com>
2020-05-21 05:18:52 +0000

    tests: base.py: Resolved flake8 issue
    Signed-off-by: sdutta.contractor <sdutta.contractor@libertyglobal.com>
    Change-Id: Ib7061e769d2bf61155ad03d5ebc459cf955fa9e9
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5904
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/devices/base.py
+++ boardfarm/devices/base.py
@@ -17,30 +17,30 @@ class BaseDevice(bft_pexpect_helper):
     log_calls = ""
 
     prompt = [
-        'root\\@.*:.*#',
+        "root\\@.*:.*#",
     ]
     delaybetweenchar = None
 
     def get_interface_ipaddr(self, interface):
-        '''Get ipv4 address of interface '''
+        """Get ipv4 address of interface."""
         raise Exception("Not implemented!")
 
     def get_interface_ip6addr(self, interface):
-        '''Get ipv6 address of interface '''
+        """Get ipv6 address of interface."""
         raise Exception("Not implemented!")
 
     def get_interface_macaddr(self, interface):
-        '''Get the interface mac address '''
+        """Get the interface mac address."""
         raise Exception("Not implemented!")
 
     def get_seconds_uptime(self):
-        '''Return seconds since last reboot. Stored in /proc/uptime'''
+        """Return seconds since last reboot. Stored in /proc/uptime."""
         raise Exception("Not implemented!")
 
     # perf related
     def parse_sar_iface_pkts(self, wan, lan):
         self.expect(
-            r'Average.*idle\r\nAverage:\s+all(\s+[0-9]+.[0-9]+){6}\r\n')
+            r"Average.*idle\r\nAverage:\s+all(\s+[0-9]+.[0-9]+){6}\r\n")
         idle = float(self.match.group(1))
         self.expect("Average.*rxmcst/s.*\r\n")
 
@@ -65,14 +65,14 @@ class BaseDevice(bft_pexpect_helper):
         return idle, wan_pps, client_pps
 
     def check_perf(self):
-        self.sendline('uname -r')
-        self.expect('uname -r')
+        self.sendline("uname -r")
+        self.expect("uname -r")
         self.expect(self.prompt)
 
         self.kernel_version = self.before
 
-        self.sendline('\nperf --version')
-        i = self.expect(['not found', 'perf version'])
+        self.sendline("\nperf --version")
+        i = self.expect(["not found", "perf version"])
         self.expect(self.prompt)
 
         if i == 0:
@@ -90,10 +90,10 @@ class BaseDevice(bft_pexpect_helper):
         ret = []
 
         for e in mapping:
-            if e['name'] not in events:
+            if e["name"] not in events:
                 continue
-            self.expect(r"(\d+) %s" % e['expect'])
-            e['value'] = int(self.match.group(1))
+            self.expect(r"(\d+) %s" % e["expect"])
+            e["value"] = int(self.match.group(1))
             ret.append(e)
 
         return ret
@@ -101,19 +101,19 @@ class BaseDevice(bft_pexpect_helper):
     # end perf related
 
     def enable_ipv6(self, interface):
-        '''Enable ipv6 in interface '''
+        """Enable ipv6 in interface."""
         raise Exception("Not implemented!")
 
     def disable_ipv6(self, interface):
-        '''Disable IPv6 in interface '''
+        """Disable IPv6 in interface."""
         raise Exception("Not implemented!")
 
     def set_printk(self, CUR=1, DEF=1, MIN=1, BTDEF=7):
-        '''Print the when debug enabled '''
+        """Print the when debug enabled."""
         raise Exception("Not implemented!")
 
     def prefer_ipv4(self, pref=True):
-        """Edits the /etc/gai.conf file
+        """Edits the /etc/gai.conf file.
 
         This is to give/remove ipv4 preference (by default ipv6 is preferred)
         See /etc/gai.conf inline comments for more details
@@ -121,26 +121,26 @@ class BaseDevice(bft_pexpect_helper):
         raise Exception("Not implemented!")
 
     def ping(self, ping_ip, source_ip=None, ping_count=4, ping_interface=None):
-        '''Check Ping verification from device '''
+        """Check Ping verification from device."""
         raise Exception("Not implemented!")
 
     def reset(self, break_into_uboot=False):
-        '''Power-cycle this device.'''
+        """Power-cycle this device."""
         if not break_into_uboot:
             self.power.reset()
             return
         for _ in range(3):
             try:
                 self.power.reset()
-                self.expect('U-Boot', timeout=30)
-                self.expect('Hit any key ')
-                self.sendline('\n\n\n\n\n\n\n')  # try really hard
+                self.expect("U-Boot", timeout=30)
+                self.expect("Hit any key ")
+                self.sendline("\n\n\n\n\n\n\n")  # try really hard
                 self.expect(self.uprompt, timeout=4)
                 # Confirm we are in uboot by typing any command.
                 # If we weren't in uboot, we wouldn't see the command
                 # that we type.
-                self.sendline('echo FOO')
-                self.expect('echo FOO', timeout=4)
+                self.sendline("echo FOO")
+                self.expect("echo FOO", timeout=4)
                 self.expect(self.uprompt, timeout=4)
                 return
             except Exception as e:
@@ -148,47 +148,47 @@ class BaseDevice(bft_pexpect_helper):
                 print("\nWe appeared to have failed to break into U-Boot...")
 
     def check_memory_addresses(self):
-        '''Check/set memory addresses and size for proper flashing.'''
+        """Check/set memory addresses and size for proper flashing."""
         raise Exception("Not implemented!")
 
     def flash_uboot(self, uboot):
         raise Exception(
-            'Code not written for flash_uboot for this board type, %s' %
+            "Code not written for flash_uboot for this board type, %s" %
             self.model)
 
     def flash_rootfs(self, ROOTFS):
         raise Exception(
-            'Code not written for flash_rootfs for this board type, %s' %
+            "Code not written for flash_rootfs for this board type, %s" %
             self.model)
 
     def flash_linux(self, KERNEL):
         raise Exception(
-            'Code not written for flash_linux for this board type, %s.' %
+            "Code not written for flash_linux for this board type, %s." %
             self.model)
 
     def flash_meta(self, META_BUILD, wan, lan):
         raise Exception(
-            'Code not written for flash_meta for this board type, %s.' %
+            "Code not written for flash_meta for this board type, %s." %
             self.model)
 
     def prepare_nfsroot(self, NFSROOT):
         raise Exception(
-            'Code not written for prepare_nfsroot for this board type, %s.' %
+            "Code not written for prepare_nfsroot for this board type, %s." %
             self.model)
 
     def kill_console_at_exit(self):
-        '''killing console '''
+        """Killing console."""
         self.kill(signal.SIGKILL)
 
     def get_dns_server(self):
-        '''Getting dns server ip address '''
+        """Get dns server ip address."""
         raise Exception("Not implemented!")
 
     def touch(self):
-        '''Keeps consoles active, so they don't disconnect for long running activities'''
+        """Keep consoles active, so they don't disconnect for long running activities."""
         self.sendline()
 
     def boot_linux(self, rootfs=None, bootargs=""):
         raise Exception(
-            "\nWARNING: We don't know how to boot this board to linux "
+            "\nWARNING: We don't know how to boot this board to linux."
             "please write the code to do so.")
