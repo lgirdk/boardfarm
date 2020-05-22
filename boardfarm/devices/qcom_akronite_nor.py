a49d1bbe2e5fa5b9f9d95de906eb2187e87f9f66
sdutta.contractor <sdutta.contractor@libertyglobal.com>
2020-05-22 12:42:27 +0000

    devices: qcom_akronite_nor: Resolved flake8 issue
    Signed-off-by: sdutta.contractor <sdutta.contractor@libertyglobal.com>
    Change-Id: I5273a85d73e269870ed33fe8f62fdf3a2e3f5e6b
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5989
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/devices/qcom_akronite_nor.py
+++ boardfarm/devices/qcom_akronite_nor.py
@@ -11,13 +11,12 @@ from . import qcom_arm_base
 
 
 class QcomAkroniteRouterNOR(qcom_arm_base.QcomArmBase):
-    """QcomAkroniteRouter board loader/configuration class derived from QcomArmBase
-    """
+    """QcomAkroniteRouter board loader/configuration class derived from QcomArmBase."""
     model = ("ap148-nor")
 
     def __init__(self, *args, **kwargs):
-        """The constructor initializes all the related arguement in the parent class QcomArmBase also initializes the uboot ddr address.
-        Also validates if the model passed from json actually matches the mach_id table.
+        """Initialize all the related arguement in the parent class QcomArmBase also initialize the uboot ddr address.\
+        Also validate if the model passed from json actually matches the mach_id table.
 
         :param args: arguements to be used
         :type args: list
@@ -34,7 +33,7 @@ class QcomAkroniteRouterNOR(qcom_arm_base.QcomArmBase):
             raise Exception("Unknown machid for %s, please add to table")
 
     def flash_rootfs(self, ROOTFS):
-        """This method flashes the Qcom Akronite board with the ROOTFS (which in general is a patch update on the firmware).
+        """Flash the Qcom Akronite board with the ROOTFS (which in general is a patch update on the firmware).
 
         :param ROOTFS: Indicates the absolute location of the file to be used to flash.
         :type ROOTFS: string
@@ -47,7 +46,7 @@ class QcomAkroniteRouterNOR(qcom_arm_base.QcomArmBase):
                            "0x1920000")
 
     def flash_linux(self, KERNEL):
-        """This method flashes the Qcom Akronite board by copying file to the board.
+        """Flash the Qcom Akronite board by copying file to the board.
 
         :param KERNEL: Indicates the absolute location of the file to be used to flash.
         :type KERNEL: string
@@ -60,7 +59,7 @@ class QcomAkroniteRouterNOR(qcom_arm_base.QcomArmBase):
                            "0x400000")
 
     def boot_linux(self, rootfs=None, bootargs=""):
-        """This method boots Qcom Akronite board.
+        """Boot Qcom Akronite board.
 
         :param rootfs: Indicates the rootsfs image path if needs to be loaded (parameter to be used at later point), defaults to None.
         :type rootfs: NA
