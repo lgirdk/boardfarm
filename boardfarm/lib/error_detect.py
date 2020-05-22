e35bff8d727a093b4497ce110f3cd67b55e93929
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-22 12:38:41 +0000

    lib:error_detect.py: Fixed flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: I4b0b891d4705736e5a2c6b27da9205b2f942f9bb
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5987
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/lib/error_detect.py
+++ boardfarm/lib/error_detect.py
@@ -12,7 +12,8 @@ import termcolor
 
 
 def print_bold(msg):
-    termcolor.cprint(msg, None, attrs=['bold'])
+    """Print the message in bold."""
+    termcolor.cprint(msg, None, attrs=["bold"])
 
 
 # Add this to your env if you need to disable this for some reason
@@ -20,20 +21,22 @@ BFT_DISABLE_ERROR_DETECT = "BFT_DISABLE_ERROR_DETECT" in os.environ
 
 
 def detect_kernel_panic(console, s):
+    """Detect the Kernel panic."""
     if re.findall("Kernel panic - not syncing", s):
         console.close()
 
-        raise Exception('Kernel panic detected')
+        raise Exception("Kernel panic detected")
 
 
 def detect_crashdump_error(console, s):
+    """Detect crashdump error."""
     if re.findall("Crashdump magic found", s):
         print_bold("Crashdump magic found, trying to save data...")
 
-        console.sendcontrol('c')
-        console.sendcontrol('c')
-        console.sendcontrol('c')
-        console.expect('<INTERRUPT>')
+        console.sendcontrol("c")
+        console.sendcontrol("c")
+        console.sendcontrol("c")
+        console.expect("<INTERRUPT>")
         console.expect(console.uprompt)
         console.setup_uboot_network()
         console.sendline("dumpipq_data")
@@ -51,16 +54,18 @@ def detect_crashdump_error(console, s):
                 i = console.expect(tftp_expect +
                                    ["Resetting with watch dog!"] +
                                    console.uprompt)
-        except:
+        except Exception as error:
+            print(error)
             print_bold("Crashdump upload failed")
         else:
             print_bold("Crashdump upload succeeded")
 
         # TODO: actually parse data too?
-        raise Exception('Crashdump detected')
+        raise Exception("Crashdump detected")
 
 
 def detect_fatal_error(console):
+    """Detect fatal error."""
     if BFT_DISABLE_ERROR_DETECT:
         return
 
