cda0ed9675fbf2f325a6d014aaccb8472dc50d7e
mmurugesh <mmurugesh.contractor@libertyglobal.com>
2020-05-22 12:31:10 +0000

    boardfarm: local_cmd-flake8 issue fixing
    Signed-off-by: mmurugesh <mmurugesh.contractor@libertyglobal.com>
    Change-Id: I3da58b40cdd57c75ecabef728525a9292514b730
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/5983
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/devices/local_cmd.py
+++ boardfarm/devices/local_cmd.py
@@ -3,51 +3,50 @@ import pexpect
 from boardfarm.lib.bft_pexpect_helper import bft_pexpect_helper
 
 
-class LocalCmd():
-    """This class is meant to be used to connect to a device
+class LocalCmd:
+    """This class is meant to be used to connect to a device.
     using a custom Linux command instead of telnet/SSH.
 
     Sets connection_type to local_cmd, ignores all output for now
     """
     def __init__(self, device=None, conn_cmd=None, **kwargs):
-        """Initializes instance of LocalCmd class
+        """Initialize instance of LocalCmd class.
 
         :param device: the device on which the command is to be executed, defaults to None
         :type device: object
         :param conn_cmd: the command to be used to connect to the device, defaults to None
         :type conn_cmd: string
-        :param **kwargs: extra args to be used if any
-        :type **kwargs: dict
+        :param ``**kwargs``: extra args to be used if any
+        :type ``**kwargs``: dict
         """
         self.device = device
         self.conn_cmd = conn_cmd
         self.device.conn_cmd = conn_cmd
 
     def connect(self):
-        """This method is used to connect to the device
+        """Connect method is used to connect to the device.
            It spawns a pexpect session for the device using the local cmd.
 
         :raises: Exception Board is in use (connection refused).
         """
         try:
             bft_pexpect_helper.spawn.__init__(self.device,
-                                              command='/bin/bash',
-                                              args=['-c', self.conn_cmd])
+                                              command="/bin/bash",
+                                              args=["-c", self.conn_cmd])
             self.device.expect(pexpect.TIMEOUT, timeout=5)
         except pexpect.EOF:
             raise boardfarm.exceptions.ConnectionRefused(
                 "Board is in use (connection refused).")
 
     def close(self, force=True):
-        """closes the pexpect session to the device
-        """
+        """Close the pexpect session to the device."""
         try:
-            if 'telnet' in self.conn_cmd:
-                self.sendcontrol(']')
-                self.sendline('q')
+            if "telnet" in self.conn_cmd:
+                self.sendcontrol("]")
+                self.sendline("q")
             else:
-                self.sendcontrol('c')
-        except:
+                self.sendcontrol("c")
+        except Exception:
             pass
         finally:
             super(type(self), self).close()
