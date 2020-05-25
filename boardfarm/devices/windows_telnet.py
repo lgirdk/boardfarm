1122b55f57509e35b6c7d03432b7162e2ea07670
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-25 04:50:04 +0000

    devices:windows_telnet.py: fixed flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: If41cc82bedf0660cd346f1702baca0ea5e624555
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/6002
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/devices/windows_telnet.py
+++ boardfarm/devices/windows_telnet.py
@@ -9,24 +9,20 @@ from . import base, connection_decider
 
 
 class WindowsTelnet(base.BaseDevice):
-    """Class to connect and verify windows telnet
-    """
+    """Class to connect and verify windows telnet."""
 
-    model = ('windows-telnet')
+    model = "windows-telnet"
     # This prompt regex could use more work
-    prompt = ['[a-zA-Z]:\\\\.*>$']
+    prompt = ["[a-zA-Z]:\\\\.*>$"]
 
     def __init__(self, *args, **kwargs):
-        """Constructor method used to login the host connected to board
-        using their credentials
-        """
-
+        """Login the host connected to board using their credentials."""
         self.args = args
         self.kwargs = kwargs
 
-        self.ip = self.kwargs['ipaddr']
-        self.username = self.kwargs.get('username', 'Administrator')
-        self.password = self.kwargs.get('password', 'bigfoot1')
+        self.ip = self.kwargs["ipaddr"]
+        self.username = self.kwargs.get("username", "Administrator")
+        self.password = self.kwargs.get("password", "bigfoot1")
 
         conn_cmd = "telnet %s" % self.ip
 
@@ -34,11 +30,11 @@ class WindowsTelnet(base.BaseDevice):
                                                         device=self,
                                                         conn_cmd=conn_cmd)
         self.connection.connect()
-        self.linesep = '\r'
+        self.linesep = "\r"
 
-        self.expect('login: ')
+        self.expect("login: ")
         self.sendline(self.username)
-        self.expect('password: ')
+        self.expect("password: ")
         self.sendline(self.password)
         self.expect(self.prompt)
 
@@ -46,19 +42,19 @@ class WindowsTelnet(base.BaseDevice):
         self.logfile_read = sys.stdout
 
     def get_ip(self, wifi_interface):
-        """Function to get wifi interface ip from windows client
+        """Get wifi interface ip from windows client.
 
         :param wifi_interface : Interface of wifi client
         :type wifi_interface : string
         :return : Matched pattern or None
         :rtype : string or boolean
         """
-        self.sendline('netsh interface ip show config ' + wifi_interface)
+        self.sendline("netsh interface ip show config " + wifi_interface)
 
         self.expect("(.+)>", timeout=30)
         Wifi_log = self.match.group(1)
 
-        match = re.search(r'IP Address:\s+([\d.]+)', str(Wifi_log))
+        match = re.search(r"IP Address:\s+([\d.]+)", str(Wifi_log))
         if match:
             return match.group(1)
         else:
@@ -70,7 +66,7 @@ class WindowsTelnet(base.BaseDevice):
              ping_count=4,
              ping_interface=None,
              wait_time=30):
-        """Function to check the ping is successfull from the windows client
+        """Check the ping is successfull from the windows client.
 
         :param ping_ip : IP to check the ping
         :type ping_ip : string
@@ -85,9 +81,8 @@ class WindowsTelnet(base.BaseDevice):
         :return : If matched return True else False
         :rtype : string
         """
-
-        if source_ip == None:
-            self.sendline('ping -n %s %s' % (ping_count, ping_ip))
+        if source_ip is None:
+            self.sendline("ping -n %s %s" % (ping_count, ping_ip))
         else:
             self.sendline("ping -S %s -n %s %s" %
                           (source_ip, ping_count, ping_ip))
@@ -96,7 +91,7 @@ class WindowsTelnet(base.BaseDevice):
         Wifi_log = self.match.group(1)
 
         match = re.search(
-            r'Reply from .+: bytes=.+ TTL=|Reply from .* time=.*',
+            r"Reply from .+: bytes=.+ TTL=|Reply from .* time=.*",
             str(Wifi_log))
         if match:
             return True
@@ -104,17 +99,17 @@ class WindowsTelnet(base.BaseDevice):
             return False
 
     def set_dhcp(self, wifi_interface):
-        """Function to set the dhcp for the wifi interface in windows client
+        """Set the dhcp for the wifi interface in windows client.
 
         :param wifi_interface : Interface of wifi
         :type wifi_interface : string
         """
-        self.sendline('netsh interface ip set address ' + wifi_interface +
+        self.sendline("netsh interface ip set address " + wifi_interface +
                       " dhcp")
         self.expect(self.prompt)
 
     def set_static_ip(self, wifi_interface, fix_ip, fix_mark, fix_gateway):
-        """Function to set the static ip for the wifi interface in windows client
+        """Set the static ip for the wifi interface in windows client.
 
         :param wifi_interface : Interface of wifi
         :type wifi_interface : string
@@ -125,32 +120,32 @@ class WindowsTelnet(base.BaseDevice):
         :param fix_gateway : gateway ip address
         :type fix_gateway :  string
         """
-        self.sendline('netsh interface ip set address ' + wifi_interface +
+        self.sendline("netsh interface ip set address " + wifi_interface +
                       " static " + fix_ip + " " + fix_mark + " " +
                       fix_gateway + " 1")
         self.expect(self.prompt)
 
     def get_default_gateway(self, wifi_interface):
-        """Function to get the default gateway using wifi interface in windows client
+        """Get the default gateway using wifi interface in windows client.
 
         :param wifi_interface : Interface of wifi
         :type wifi_interface : string
         :return : Matched pattern or None
         :rtype : string or boolean
         """
-        self.sendline('netsh interface ip show config ' + wifi_interface)
+        self.sendline("netsh interface ip show config " + wifi_interface)
 
         self.expect("(.+)>", timeout=30)
         Wifi_log = self.match.group(1)
 
-        match = re.search(r'Default Gateway:\s+([\d.]+)', str(Wifi_log))
+        match = re.search(r"Default Gateway:\s+([\d.]+)", str(Wifi_log))
         if match:
             return match.group(1)
         else:
             return None
 
     def get_interface_ipaddr(self, interface):
-        """Function to get the ipv4 address in windows client using interface
+        """Get the ipv4 address in windows client using interface.
 
         :param interface : Interface of wifi
         :type interface : string
@@ -160,13 +155,13 @@ class WindowsTelnet(base.BaseDevice):
         """
         ip = self.get_ip(interface)
 
-        if ip != None:
+        if ip is not None:
             return ip
         else:
             assert False, "Can't get interface ip"
 
     def get_interface_ip6addr(self, interface):
-        """Function to get the ipv6 address in windows client using interface
+        """Get the ipv6 address in windows client using interface.
 
         :param interface : Interface of wifi
         :type interface : string
@@ -181,19 +176,18 @@ class WindowsTelnet(base.BaseDevice):
                 return ipv6addr
 
     def get_interface_macaddr(self, interface):
-        """Function to get the mac address in windows client using interface
+        """Get the mac address in windows client using interface.
 
         :param interface : Interface of wifi
         :type interface : string
-        :param /NH : Specifies that the "Column Header" should
-                    not be displayed in the output.
-                    Valid only for TABLE and CSV formats. Not mandatory
+        :param /NH : Specifies that the "Column Header" should not be displayed in the output.
+        Valid only for TABLE and CSV formats. Not mandatory
         :param /V  : Specifies that verbose output is displayed. Not mandatory
         :return : mac address
         :rtype : string
         """
-        self.sendline('getmac /V /NH')
+        self.sendline("getmac /V /NH")
         self.expect("{!s}.*({!s}).*\r\n".format(interface, WindowsMacFormat))
-        macaddr = self.match.group(1).replace('-', ':')
+        macaddr = self.match.group(1).replace("-", ":")
         self.expect(self.prompt)
         return macaddr
