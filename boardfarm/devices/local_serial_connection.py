9573b0ded948a4510369a39e12d6264e2d0cf1db
asraj.contractor <asraj.contractor@libertyglobal.com>
2020-05-25 06:00:25 +0000

    devices:local_serial_connection.py: Fixed flake8 issues
    Signed-off-by: asraj.contractor <asraj.contractor@libertyglobal.com>
    Change-Id: I868d300f5904069d76e04fd3d64d3467b4f15e56
    Reviewed-on: https://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/6005
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/devices/local_serial_connection.py
+++ boardfarm/devices/local_serial_connection.py
@@ -3,12 +3,14 @@ from boardfarm.lib.bft_pexpect_helper import bft_pexpect_helper
 from boardfarm.lib.regexlib import telnet_ipv4_conn
 
 
-class LocalSerialConnection():
-    """To use, set conn_cmd in your json to "cu -s <port_speed> -l <path_to_serialport>"
-       and set connection_type to "local_serial"
+class LocalSerialConnection:
+    """LocalSerialConnection.
+
+    To use, set conn_cmd in your json to "cu -s <port_speed> -l <path_to_serialport>".
+    and set connection_type to "local_serial"
     """
     def __init__(self, device=None, conn_cmd=None, **kwargs):
-        """Initializes the instance of LocalSerialConnection class
+        """Initialize the instance of LocalSerialConnection class.
 
         Parameters initialized will later be used to connect to a device
         serially via a COM/TTY port
@@ -17,31 +19,31 @@ class LocalSerialConnection():
         :type device: object
         :param conn_cmd: conn_cmd to connect to device, defaults to None
         :type conn_cmd: string
-        :param **kwargs: args to be used
-        :type **kwargs: dict
+        :param ``**kwargs``: args to be used
+        :type ``**kwargs``: dict
         """
         self.device = device
         self.conn_cmd = conn_cmd
 
     def connect(self):
-        """Initializes a pexpect session using the serial command
+        """Initialize a pexpect session using the serial command.
+
         Command can be a screen/cu command to connect to a TTY/COM port.
 
         :raises: Exception Board is in use (connection refused).
         """
         bft_pexpect_helper.spawn.__init__(self.device,
-                                          command='/bin/bash',
-                                          args=['-c', self.conn_cmd])
+                                          command="/bin/bash",
+                                          args=["-c", self.conn_cmd])
         try:
             self.device.expect([
                 telnet_ipv4_conn,
-                "----------------------------------------------------"
+                "----------------------------------------------------",
             ])
         except pexpect.EOF:
             raise Exception("Board is in use (connection refused).")
 
     def close(self):
-        """closes the pexpect session to the device
-        """
+        """Close the pexpect session to the device."""
         self.sendline("~.")
         super(type(self), self).close()
