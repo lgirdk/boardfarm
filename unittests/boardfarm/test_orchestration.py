fe306df6ba0645e0816862ffc39b491fb22d0500
ketan.d.luffy <ktewari.contractor@libertyglobal.com>
2020-06-18 11:22:37 +0200

    test_orchestration: updated unittest code
    Signed-off-by: ketan.d.luffy <ktewari.contractor@libertyglobal.com>
    Change-Id: I5f16152619cce24101570137b7a11d68485d47e0
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm/+/6499
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    (cherry picked from commit e16545ed47fcf23f4712c5e3fc160f2be3c75cb9)
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm/+/6323

--- unittests/boardfarm/test_orchestration.py
+++ unittests/boardfarm/test_orchestration.py
@@ -1,21 +1,12 @@
 import pytest
 from boardfarm.orchestration import TestStep as TS
-from boardfarm.tests.bft_base_test import BftBaseTest
-from unittest2 import TestCase
 
 
-# Assumption1 : later test won't define runTest
-# Assumption2 : BF pytest pluging won't call a wrapper to execute runTest
-# Assumption3 : BF pytest will directly used test prefixed methods
-class BfPyTestCls(TestCase, BftBaseTest):
-    def __init__(self, *args, **kwargs):
-        # This is just to ensure the pytest runs with BF
-        # this is done so that base prefixed/suffixed test method don't run
-        TestCase.__init__(self, *args, **kwargs)
-        BftBaseTest.__init__(self, None, None, None)
-
-
-class test_orchestration(BfPyTestCls):
+class Test(object):
+    """Sample Test class."""
+    steps = []
+    log_to_file = None
+    dev = None
 
     # this will always raise a ValueError exception
     def action(self, code=200):
@@ -23,64 +14,73 @@ class test_orchestration(BfPyTestCls):
         e.code = code
         raise e
 
-    def test_positive_scenario1(self):
-        with TS(self, "Negative Test Scenario 1", type(self).__name__) as ts:
-            with ts.assertRaises(ValueError) as e:
-                ts.call(self.action)
-            exc = e.exception
-            assert exc.code == 200
 
-    def test_positive_scenario2(self):
-        with TS(self, "Negative Test Scenario 2", type(self).__name__) as ts:
-            exc = ts.assertRaises(ValueError, self.action, code=300).exception
-            assert exc.code == 300
+@pytest.fixture(scope='module')
+def obj():
+    yield Test()
 
-    def test_positive_scenario3(self):
-        # should be able to continue remaining execution after with, if any.
-        with TS(self, "Negative Test Scenario 3", type(self).__name__) as ts:
-            with ts.assertRaises(ValueError) as e:
-                ts.call(self.action)
-            ts.call(print, "Yes I work")
-            exc = e.exception
-            assert exc.code == 200
 
-    def test_positive_scenario4(self):
-        # should be able to continue remaining execution after with, if any.
-        with TS(self, "Negative Test Scenario 4", type(self).__name__) as ts:
-            exc = ts.assertRaises(ValueError, self.action, code=300).exception
-            ts.call(print, "Yes I work")
-            assert exc.code == 300
+def test_positive_scenario1(obj):
+    with TS(obj, "Negative Test Scenario 1", type(obj).__name__) as ts:
+        with ts.assertRaises(ValueError) as e:
+            ts.call(obj.action)
+        exc = e.exception
+        assert exc.code == 200
 
-    def test_negative_scenario1(self):
-        # this scenario will throw an exception Code Error
-        with pytest.raises(AssertionError) as exc:
-            with TS(self, "Negative Test Scenario 5",
-                    type(self).__name__) as ts:
-                with ts.assertRaises(KeyError):
-                    ts.call(self.action)
-        assert exc.type is AssertionError
 
-    def test_negative_scenario2(self):
-        # this scenario will throw an exception Code Error
-        with pytest.raises(AssertionError) as exc:
-            with TS(self, "Negative Test Scenario 6",
-                    type(self).__name__) as ts:
-                ts.assertRaises(KeyError, self.action, code=100)
-        assert exc.type is AssertionError
+def test_positive_scenario2(obj):
+    with TS(obj, "Negative Test Scenario 2", type(obj).__name__) as ts:
+        exc = ts.assertRaises(ValueError, obj.action, code=300).exception
+        assert exc.code == 300
 
-    def test_negative_scenario3(self):
-        # this scenario will throw an exception Code Error as no exception got raised
-        with pytest.raises(AssertionError) as exc:
-            with TS(self, "Negative Test Scenario 7",
-                    type(self).__name__) as ts:
-                with ts.assertRaises(KeyError):
-                    ts.call(print, "I won't throw an exception")
-        assert "No exception caught" in str(exc.value)
 
-    def test_negative_scenario4(self):
-        # this scenario will throw an exception Code Error as no exception got raised
-        with pytest.raises(AssertionError) as exc:
-            with TS(self, "Negative Test Scenario 8",
-                    type(self).__name__) as ts:
-                ts.assertRaises(KeyError, print, "I won't throw an exception")
-        assert "No exception caught" in str(exc.value)
+def test_positive_scenario3(obj):
+    # should be able to continue remaining execution after with, if any.
+    with TS(obj, "Negative Test Scenario 3", type(obj).__name__) as ts:
+        with ts.assertRaises(ValueError) as e:
+            ts.call(obj.action)
+        ts.call(print, "Yes I work")
+        exc = e.exception
+        assert exc.code == 200
+
+
+def test_positive_scenario4(obj):
+    # should be able to continue remaining execution after with, if any.
+    with TS(obj, "Negative Test Scenario 4", type(obj).__name__) as ts:
+        exc = ts.assertRaises(ValueError, obj.action, code=300).exception
+        ts.call(print, "Yes I work")
+        assert exc.code == 300
+
+
+def test_negative_scenario1(obj):
+    # this scenario will throw an exception Code Error
+    with pytest.raises(AssertionError) as exc:
+        with TS(obj, "Negative Test Scenario 5", type(obj).__name__) as ts:
+            with ts.assertRaises(KeyError):
+                ts.call(obj.action)
+    assert exc.type is AssertionError
+
+
+def test_negative_scenario2(obj):
+    # this scenario will throw an exception Code Error
+    with pytest.raises(AssertionError) as exc:
+        with TS(obj, "Negative Test Scenario 6", type(obj).__name__) as ts:
+            ts.assertRaises(KeyError, obj.action, code=100)
+    assert exc.type is AssertionError
+
+
+def test_negative_scenario3(obj):
+    # this scenario will throw an exception Code Error as no exception got raised
+    with pytest.raises(AssertionError) as exc:
+        with TS(obj, "Negative Test Scenario 7", type(obj).__name__) as ts:
+            with ts.assertRaises(KeyError):
+                ts.call(print, "I won't throw an exception")
+    assert "No exception caught" in str(exc.value)
+
+
+def test_negative_scenario4(obj):
+    # this scenario will throw an exception Code Error as no exception got raised
+    with pytest.raises(AssertionError) as exc:
+        with TS(obj, "Negative Test Scenario 8", type(obj).__name__) as ts:
+            ts.assertRaises(KeyError, print, "I won't throw an exception")
+    assert "No exception caught" in str(exc.value)
