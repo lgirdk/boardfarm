7cd577778be0560ad9994f6b72dd2b6f5e6a4a13
kmallapuramprasannak <kmallapuramprasannak.contractor@libertyglobal.com>
2020-07-31 15:26:32 +0200

    lib: webgui: Added script to take full page screenshot for GUI
    Added snippet to take full page screenshot for GUI
    
    Signed-off-by: kmallapuramprasannak <kmallapuramprasannak.contractor@libertyglobal.com>
    Change-Id: I4b6aa75b914c53016bd2ffb3a84a01f101d0f008
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/7439
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm/lib/webgui.py
+++ boardfarm/lib/webgui.py
@@ -545,6 +545,10 @@ class ScreenshotListener(AbstractEventListener):
             + "."
             + ext
         )
+        gui_page_size = lambda dimension: driver.execute_script(
+            "return document.body.parentNode.scroll" + dimension
+        )
+        driver.set_window_size(gui_page_size("Width"), gui_page_size("Height"))
         driver.get_screenshot_as_file(abs_path)
         print("Screenshot saved as '{}'".format(abs_path))
 
