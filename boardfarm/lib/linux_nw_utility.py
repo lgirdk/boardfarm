16771182953690b324f748535835fe5bb3006023
Kottesvaran Murugan <mkottesvaran.contractor@libertyglobal.com>
2020-10-06 10:18:51 +0200

    lib: nw_utility_stub: network firewall class with stub
    lib: linux_nw_utility: Generic linux utilities
    
    Depends-on: I34f71c5200b6c5252ca4a9813bb4d99d0c15d421
    
    Signed-off-by: Kottesvaran Murugan <mkottesvaran.contractor@libertyglobal.com>
    Change-Id: I926893ba49def9c6c084b0d1e90e8fc6e590a04a
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/8491
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm/lib/linux_nw_utility.py
+++ boardfarm/lib/linux_nw_utility.py
@@ -1,5 +1,6 @@
+from boardfarm.lib.firewall_parser import iptable_parser
 from boardfarm.lib.netstat_parser import NetstatParser
-from boardfarm.lib.nw_utility_stub import NwUtilityStub
+from boardfarm.lib.nw_utility_stub import NwFirewallStub, NwUtilityStub
 
 
 class DeviceNwUtility(NwUtilityStub):
@@ -9,3 +10,32 @@ class DeviceNwUtility(NwUtilityStub):
     def netstat(self, opts="", extra_opts=""):
         out = self.dev.check_output(f"netstat {opts} {extra_opts}")
         return NetstatParser().parse_inet_output_linux(out)
+
+
+class NwFirewall(NwFirewallStub):
+    def __init__(self, parent_device):
+        self.dev = parent_device
+
+    def get_iptables_list(self, opts="", extra_opts=""):
+        out = self.dev.check_output(f"iptables {opts} {extra_opts}")
+        return iptable_parser().ip6tables(out)
+
+    def is_iptable_empty(self, opts="", extra_opts=""):
+        out = self.dev.check_output(f"iptables {opts} {extra_opts}")
+        check_out = iptable_parser().ip6tables(out)
+        check_empty = (
+            True if len([True for i in check_out.values() if i]) == 0 else False
+        )
+        return check_empty
+
+    def get_ip6tables_list(self, opts="", extra_opts=""):
+        out = self.dev.check_output(f"ip6tables {opts} {extra_opts}")
+        return iptable_parser().ip6tables(out)
+
+    def is_ip6table_empty(self, opts="", extra_opts=""):
+        out = self.dev.check_output(f"ip6tables {opts} {extra_opts}")
+        check_out = iptable_parser().ip6tables(out)
+        check_empty = (
+            True if len([True for i in check_out.values() if i]) == 0 else False
+        )
+        return check_empty
--- boardfarm/lib/nw_utility_stub.py
+++ boardfarm/lib/nw_utility_stub.py
@@ -5,3 +5,21 @@ class NwUtilityStub:
     @abc.abstractmethod
     def netstat(self, opts="", extra_opts=""):
         raise Exception("Method not implemented")
+
+
+class NwFirewallStub:
+    @abc.abstractmethod
+    def get_iptables_list(self, opts="", extra_opts=""):
+        raise Exception("Method not implemented")
+
+    @abc.abstractmethod
+    def is_iptable_empty(self, opts="", extra_opts=""):
+        raise Exception("Method not implemented")
+
+    @abc.abstractmethod
+    def get_ip6tables_list(self, opts="", extra_opts=""):
+        raise Exception("Method not implemented")
+
+    @abc.abstractmethod
+    def is_ip6table_empty(self, opts="", extra_opts=""):
+        raise Exception("Method not implemented")
