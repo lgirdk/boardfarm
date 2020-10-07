9dd184f07ff5c5f891cfef2130d9f00c8a1be521
Michele Gualco <mgualco.contractor@libertyglobal.com>
2020-10-07 08:20:47 +0200

    lib: env_helper: updated version
    TBH this should not be hardcoded in the baseclass.
    
    Signed-off-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    Change-Id: I148ddc6565f4b27c56b0388e38b228864d72396a
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/8602
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/lib/env_helper.py
+++ boardfarm/lib/env_helper.py
@@ -35,6 +35,7 @@ class EnvHelper(object):
             2.0,
             2.1,
             2.2,
+            2.3,
         ], "Unknown environment version!"
         self.env = env
         self.mirror = ""
