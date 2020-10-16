c681bc26d8feb86a2f87aa4db3b9b1aec89e7229
Filippo Carra <fcarra@libertyglobal.com>
2020-10-16 14:35:52 +0200

    Replaced generic Exception with specific one
    Signed-off-by: Filippo Carra <fcarra@libertyglobal.com>
    Change-Id: I0106472311857cd40cb4a18a776815bb0307227a
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/8788
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/lib/nw_utility_stub.py
+++ boardfarm/lib/nw_utility_stub.py
@@ -4,28 +4,28 @@ import abc
 class NwUtilityStub:
     @abc.abstractmethod
     def netstat(self, opts="", extra_opts=""):
-        raise Exception("Method not implemented")
+        raise NotImplementedError
 
 
 class NwFirewallStub:
     @abc.abstractmethod
     def get_iptables_list(self, opts="", extra_opts=""):
-        raise Exception("Method not implemented")
+        raise NotImplementedError
 
     @abc.abstractmethod
     def is_iptable_empty(self, opts="", extra_opts=""):
-        raise Exception("Method not implemented")
+        raise NotImplementedError
 
     @abc.abstractmethod
     def get_ip6tables_list(self, opts="", extra_opts=""):
-        raise Exception("Method not implemented")
+        raise NotImplementedError
 
     @abc.abstractmethod
     def is_ip6table_empty(self, opts="", extra_opts=""):
-        raise Exception("Method not implemented")
+        raise NotImplementedError
 
 
 class NwDnsLookupStub:
     @abc.abstractmethod
     def nslookup(self, domain_name, opts="", extra_opts=""):
-        raise Exception("Method not implemented")
+        raise NotImplementedError
