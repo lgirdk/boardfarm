42d7017f21e00cf1d01111c4487cb6a634d304a0
Kottesvaran Murugan <mkottesvaran.contractor@libertyglobal.com>
2020-11-26 13:01:10 +0100

    lib: env_helper: enviornment version 2.6 is added
    Signed-off-by: Kottesvaran Murugan <mkottesvaran.contractor@libertyglobal.com>
    Change-Id: I8f5bae970bc6f6ccd65ba9f3ea7cab27f880e801
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/9561
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Filippo Carra <fcarra@libertyglobal.com>

--- boardfarm/lib/env_helper.py
+++ boardfarm/lib/env_helper.py
@@ -38,6 +38,7 @@ class EnvHelper(object):
             2.3,
             2.4,
             2.5,
+            2.6,
         ], "Unknown environment version!"
         self.env = env
         self.mirror = ""
