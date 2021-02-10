8baf02a7cb1b6b2eaeada03fcc9c8e22a6742c9d
Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
2021-02-10 18:52:36 +0100

    lib: common: replaced spaces to _ in test name
    Signed-off-by: Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
    Change-Id: I6e19e31931def7a15526c14a182c23da2013bb3c
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/10527
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm/lib/common.py
+++ boardfarm/lib/common.py
@@ -2182,4 +2182,8 @@ def get_pytest_name():
     Returns:
         [string]: test name
     """
-    return (os.environ.get("PYTEST_CURRENT_TEST").split(" (setup)")[0]).split("::")[1]
+    return (
+        (os.environ.get("PYTEST_CURRENT_TEST").split(" (setup)")[0])
+        .split("::")[1]
+        .replace(" ", "_")
+    )
