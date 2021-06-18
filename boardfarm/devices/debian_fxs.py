d8e7c9668c60bee77a5cb9da410f9645a8376932
Ketan Tewari <ktewari@libertyglobal.com>
2021-06-18 11:56:05 +0200

    devices: debian_fxs: independent device class
    No longer a profile.
    Moved the parent class to SIPPhoneTemplate.
    
    Signed-off-by: Ketan Tewari <ktewari@libertyglobal.com>
    Change-Id: I934d04fcdabf37e3c3a4a76ed58843380bc67153
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/12146
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm/devices/debian_fxs.py
+++ boardfarm/devices/debian_fxs.py
@@ -1,10 +1,12 @@
-import functools
+import atexit
+from contextlib import suppress
 
 from debtcollector import deprecate
+from pexpect import TIMEOUT
 
 from boardfarm.exceptions import CodeError
 
-from .base_devices.fxs_modem import FXSModemTemplate
+from .base_devices.sip_template import SIPPhoneTemplate
 from .platform.debian import DebianBox
 
 
@@ -13,23 +15,38 @@ class PythonExecutor:
 
     def __init__(self, device):
         self.device = device
-        self.prompt = ">>>"
+        self.py_prompt = ">>>"
 
+        self.device.check_output("kill -9 $(pgrep python3)")
         self.run("python3")
 
     def run(self, cmd, expect=""):
         self.device.sendline(cmd)
         if not expect:
-            return self.device.expect(self.prompt)
+            self.device.expect(self.py_prompt)
+            out = self.device.before
         else:
-            return self.device.expect(expect)
+            self.device.expect(expect)
+            out = self.device.before
+            self.device.expect(self.py_prompt)
+
+        if "Traceback" in out:
+            raise CodeError(
+                "Failed to execute on python console command:"
+                + f"\n{cmd}\nOutput:{out}"
+            )
+
+        return out
 
     def exit(self):
-        self.device.sendline("exit()")
-        self.device.expect(self.device.prompt)
+        with suppress(TIMEOUT):
+            self.device.sendcontrol("c")
+            self.device.expect(self.py_prompt)
+            self.device.sendcontrol("d")
+            self.device.expect(self.device.prompt)
 
 
-class DebianFXS(FXSModemTemplate, DebianBox):
+class DebianFXS(SIPPhoneTemplate, DebianBox):
     """Fax modem."""
 
     model = "debian_fxs"
@@ -38,9 +55,19 @@ class DebianFXS(FXSModemTemplate, DebianBox):
         """Instance initialization."""
         self.args = args
         self.kwargs = kwargs
-        self.own_number = self.kwargs.pop("number")
-        self.tcid = self.kwargs.pop("tcid")
-        self.line = self.kwargs.pop("line")
+
+        # legacy approach specify TCID, Serial Line via INV JSON
+        self.own_number = self.kwargs.get("number")
+        self.tcid = self.kwargs.get("tcid")
+        self.line = self.kwargs.get("line")
+
+        self.usb_port = self.kwargs.get("usb_port")
+        self.fxs_port = self.kwargs.get("fxs_port")
+
+        if not (self.line or self.usb_port):
+            raise CodeError("Please provide either a TTY Line or USB port")
+        if not (self.own_number or self.fxs_port):
+            raise CodeError("Please provide either a FXS EP or Phone number")
 
         # pexpect session handled by a parent method
         self.parse_device_options(*args, **kwargs)
@@ -48,20 +75,31 @@ class DebianFXS(FXSModemTemplate, DebianBox):
             self.legacy_add = True
             self.dev_array = "FXS"
 
-        # verify tty lines
-        if not self._tty_line_exists():
+        # if usb_port exists: fetch tty line from /sys filesystem
+        # else validate if the tty line provided by kwargs exists
+        if self.usb_port:
+            self.line = self.check_output(
+                f"ls /sys/bus/usb/devices/{self.usb_port}:1.0/tty"
+            )
+            if not self.line:
+                raise CodeError(
+                    f"Failed to find Serial Line for USB port - {self.usb_port}"
+                )
+        elif not self._tty_line_exists():
             raise CodeError("Device does not have a TTY line to speak to!")
 
-        # NOTE: this will go away with custom Dockerfiles for serial phones
-        # moving this out from phone_start method.
-        self.sendline("pip3 show pyserial")
-        self.expect(self.prompt)
-        if "Package(s) not found" in self.before:
-            self.sendline("pip install pyserial")
-            self.expect(self.prompt)
+        self.py = PythonExecutor(self)
+        atexit.register(self.close)
 
     def __str__(self):
-        return "serialmodem %s" % self.line
+        return f"FXS Phone {self.name}/{self.usb_port}/{self.fxs_ep}"
+
+    @property
+    def number(self):
+        return self.own_number
+
+    def close(self):
+        self.py.exit()
 
     def _tty_line_exists(self):
         """To check if tty dev exists.
@@ -69,31 +107,19 @@ class DebianFXS(FXSModemTemplate, DebianBox):
         rvalue: TRUE/FALSE
         rtype: Boolean
         """
-        self.sendline(f"ls /dev/tty{self.line}")
+        self.line = f"tty{self.line}"
+        self.sendline(f"ls /dev/{self.line}")
         self.expect(self.prompt)
         return "No such file or directory" not in self.before
 
-    def _python_console_check(func):
-        """Decorator to check python console before execution.
-
-        Note - Only to validate instance methods."""
-
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            instance = args[0]
-            if hasattr(instance, "py"):
-                return func(*args, **kwargs)
-            raise CodeError("Function was called without opening python console!")
-
-        return wrapper
+    def phone_config(self, sipserver):
+        super().phone_config(sipserver)
 
     def phone_start(self, baud="115200", timeout="1"):
         """To start the softphone session."""
-
-        self.py = PythonExecutor(self)
         self.py.run("import serial,time")
         self.py.run(
-            f"set = serial.Serial('/dev/tty{self.line}', {baud} ,timeout= {timeout})"
+            f"set = serial.Serial('/dev/{self.line}', {baud} ,timeout= {timeout})"
         )
         self.py.run("set.write(b'ATZ\\r')")
         self.mta_readlines(search="OK")
@@ -102,8 +128,7 @@ class DebianFXS(FXSModemTemplate, DebianBox):
         self.py.run("set.write(b'AT+FCLASS=1\\r')")
         self.mta_readlines(search="OK")
 
-    # maintaining backward compatibility for tests.
-    @_python_console_check
+    # maintaining backward compatibility for legacy tests.
     def mta_readlines(self, time="3", search=""):
         """To readlines from serial console."""
         self.py.run("set.flush()")
@@ -111,37 +136,41 @@ class DebianFXS(FXSModemTemplate, DebianBox):
         self.py.run("l=set.readlines()")
         self.py.run("print(l)", expect=search)
 
-    # this is bad
+    # this is bad, maintaining just to support legacy.
     # breaking this down below
-    @_python_console_check
     def offhook_onhook(self, hook_value):
         """To generate the offhook/onhook signals."""
         self.py.run(f"set.write(b'ATH{hook_value}\\r')")
         self.mta_readlines(search="OK")
 
-    @_python_console_check
     def on_hook(self):
+        """Execute on_hook procedure to disconnect to a line.
+
+        On hook. Hangs up the phone, ending any call in progress.
+        Need to send ATH0 command over FXS modem
+        """
         self.py.run("set.write(b'ATH0\\r')")
         self.mta_readlines(search="OK")
 
-    @_python_console_check
     def off_hook(self):
+        """Execute off_hook procedure to connect to a line.
+
+        Off hook. Picks up the phone line (typically you'll hear a dialtone)
+        Need to send ATH1 command over FXS modem
+        """
         self.py.run("set.write(b'ATH1\\r')")
         self.mta_readlines(search="OK")
 
-    @_python_console_check
     def answer(self):
-        """To answer the incoming call."""
-        deprecate(
-            "Warning!",
-            message="answer() is deprecated. Use off_hook() and on_hook() method to answer the calls",
-            category=UserWarning,
-        )
+        """To answer a call on RING state.
+
+        Answer. Picks up the phone line and answer the call manually.
+        Need to send ATA command over FXS modem
+        """
         self.mta_readlines(time="10", search="RING")
         self.py.run("set.write(b'ATA\\r')")
         self.mta_readlines(search="ATA")
 
-    @_python_console_check
     def dial(self, number, receiver_ip=None):
         """To dial to another number.
 
@@ -151,20 +180,20 @@ class DebianFXS(FXSModemTemplate, DebianBox):
         self.py.run(f"set.write(b'ATDT{number};\\r')")
         self.mta_readlines(search="ATDT")
 
-    @_python_console_check
+    # maintaining backward compatibility for legacy tests.
     def hangup(self):
         """To hangup the ongoing call."""
-        self.py.run("set.write(b'ATH0\\r')")
-        self.mta_readlines(search="OK")
+        deprecate(
+            "Warning!",
+            message="hangup() is deprecated. use on_hook() method to hangup the calls",
+            category=UserWarning,
+        )
+        self.on_hook()
 
-    @_python_console_check
     def phone_kill(self):
         """To kill the serial port console session."""
         self.py.run("set.close()")
-        self.py.exit()
-        delattr(self, "py")
 
-    @_python_console_check
     def validate_state(self, state):
         """Read the mta_line message to validate the call state
 
