b9ec4467117b534dd62c1fb7d69d60668a60f9ae
Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
2021-08-09 14:31:55 +0000

    Place scaffolhding for Flit
    Signed-off-by: Shakthi Ravichandran <sravichandran.contractor@libertyglobal.com>
    Change-Id: I81db4f54e25f2c4ed4eac68cea74a1b49313c6a4
    Reviewed-on: http://cicd.lgirdk.com/gerrit/c/github/boardfarm/+/12525
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Filippo Carra <fcarra@libertyglobal.com>

--- boardfarm/__init__.py
+++ boardfarm/__init__.py
@@ -1,15 +1,10 @@
-# Copyright (c) 2019
-#
-# All rights reserved.
-#
-# This file is distributed under the Clear BSD license.
-# The full text can be found in LICENSE in the root directory.
+"""Automated testing of network devices"""
+__version__ = "1.1.1"
 
 import uuid
 
 from .Boardfarm import Boardfarm  # noqa: F401
 from .plugins import find_plugins, walk_library  # noqa: F401
-from .version import __version__  # noqa: F401
 
 uniqid = uuid.uuid4().hex[:15]  # Random, unique ID and use first 15 bytes
 
