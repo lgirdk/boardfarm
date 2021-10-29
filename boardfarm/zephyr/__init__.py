295cbbf64394ab154658a535e8e25f2290b3188d
Filippo Carra <karrukola@users.noreply.github.com>
2021-10-29 16:04:32 +0200

    feat: remove zephyr dead code
    Code interacting with Zephyr is not being used
    
    BREAKING CHANGE:
    
    Signed-off-by: Filippo Carra <karrukola@users.noreply.github.com>
    Change-Id: I5b70fea314c892667d4e0c2f0f362215f7709ef5
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm/+/13900
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Musthafa Kariyaparambil <mkariyaparambil.contractor@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm/zephyr/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Yet to be implemented."""
-# Copyright (c) 2019
-#
-# All rights reserved.
-#
-# This file is distributed under the Clear BSD license.
-# The full text can be found in LICENSE in the root directory.
