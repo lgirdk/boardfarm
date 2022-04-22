142569d562055d1bb80ddcbe2063f4afab16f031
Ayush Singhal <aysinghal.contractor@libertyglobal.com>
2022-04-22 16:15:12 +0200

    refactor(lib:env_helper.py): segregate docsis and non-docsis env_helper
    Depends-on: Ifc641127e3f1cde3780a1cf0ce7f82ca2dc83b58
    Depends-on: Icb7fa6d109375fbebd41e6ad14ed552373d542c1
    Depends-on: I1022e5a9dc2f69a1e638120d668d94032b80518a
    Depends-on: I0febd6b0333c907c5764b70c32ad50a0b872484a
    Depends-on: Ic07418939fd95e2360bbafff3b56f08676a5db31
    
    BOARDFARM-1860
    
    Signed-off-by: Ayush Singhal <aysinghal.contractor@libertyglobal.com>
    Change-Id: Ic5d70e2257b9463cddcb735143c141daded59cae
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm/+/15450
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm/lib/env_helper.py
+++ boardfarm/lib/env_helper.py
@@ -1,4 +1,6 @@
 import logging
+from collections import ChainMap
+from typing import Dict, List, Optional, Union
 
 from boardfarm.exceptions import BftEnvExcKeyError, BftEnvMismatch
 
@@ -75,6 +77,23 @@ class EnvHelper:
             if mirror:
                 return (
                     self.mirror
+                    + self.env["environment_def"]["board"]["software"]["load_image"]
+                )
+            else:
+                return self.env["environment_def"]["board"]["software"]["load_image"]
+        except (KeyError, AttributeError):
+            return self.get_image_uri(mirror=mirror)
+
+    def get_image_uri(self, mirror=True):
+        """Get image URI.
+
+        returns the desired image for this to run against concatenated with the
+        site mirror for automated flashing without passing args to bft
+        """
+        try:
+            if mirror:
+                return (
+                    self.mirror
                     + self.env["environment_def"]["board"]["software"]["image_uri"]
                 )
             else:
@@ -94,7 +113,11 @@ class EnvHelper:
         returns true or false depending if the environment has specified a
         provisioning mode
         """
-        pass
+        try:
+            self.get_prov_mode()
+            return True
+        except (KeyError, AttributeError):
+            return False
 
     def has_tr069(self) -> bool:
         """
@@ -191,10 +214,12 @@ class EnvHelper:
     def get_board_hardware_type(self):
         """Returns board hardware type according to
         ["environment_def"]["board"]["model"]
-        :return: unknown if not found in mapping
+        :return: mv1/mv2/mv2+/mv3 etc. or unknown if not found in mapping
         """
         board_model = self.get_board_model()
-        return {"F5685LGE": "mv3"}.get(board_model, "unknown")
+        return {"F3896LG": "mv2+", "CH7465LG": "mv1", "F5685LGE": "mv3"}.get(
+            board_model, "unknown"
+        )
 
     def env_check(self, test_environment):
         """Test environment check.
@@ -286,3 +311,337 @@ class EnvHelper:
 
         find_device_arrays(env_json)
         return devices
+
+    def get_update_image(self, mirror=True):
+        """Get the image update uri.
+
+        returns the desired image to be updated for this to run against concatenated with the
+        site mirror for software upgrade/downgrade test
+        """
+        try:
+            if mirror:
+                return (
+                    self.mirror
+                    + self.env["environment_def"]["board"]["software_update"][
+                        "load_image"
+                    ]
+                )
+            else:
+                return self.env["environment_def"]["board"]["software_update"][
+                    "load_image"
+                ]
+        except (KeyError, AttributeError):
+            raise BftEnvExcKeyError
+
+    def get_update_image_version(self):
+        """Get the image version name from env for software update.
+
+        returns the image version name for software update uniquely defined for an image
+        """
+        try:
+            return self.env["environment_def"]["board"]["software_update"][
+                "image_version"
+            ]
+        except (KeyError, AttributeError):
+            raise BftEnvExcKeyError
+
+    def get_alternative_image(self, mirror=True):
+        """Get the alternative image uri.
+
+        returns the alternative image to be updated for this to run against concatenated with the
+        site mirror for software upgrade/downgrade test
+        """
+        try:
+            if mirror:
+                return (
+                    self.mirror
+                    + self.env["environment_def"]["board"]["software_alternative"][
+                        "load_image"
+                    ]
+                )
+            else:
+                return self.env["environment_def"]["board"]["software_alternative"][
+                    "load_image"
+                ]
+        except (KeyError, AttributeError):
+            raise BftEnvExcKeyError
+
+    def get_alternative_image_version(self):
+        """Get the alternative image version name from env for software update.
+
+        returns the alternative image version name for software update uniquely defined for an image
+        """
+        try:
+            return self.env["environment_def"]["board"]["software_alternative"][
+                "image_version"
+            ]
+        except (KeyError, AttributeError):
+            raise BftEnvExcKeyError
+
+    def get_ertr_mode(self):
+        return {"max_config": True}
+
+    def get_country(self):
+        """This method returns the country name from env json.
+
+        :return: possible values are NL,AT,CH,CZ,DE,HU,IE,PL,RO,SK
+        :rtype: string
+        """
+        try:
+            return self.env["environment_def"]["board"]["country"]
+        except (KeyError, AttributeError):
+            raise BftEnvExcKeyError
+
+    def voice_enabled(self):
+        """This method returns true if voice is enabled in env JSON.
+
+        :return: possible values are True/False
+        :rtype: boolean
+        """
+        try:
+            return "voice" in self.env["environment_def"]
+        except (KeyError, AttributeError):
+            raise BftEnvExcKeyError
+
+    def wifi_clients(self) -> list:
+        """Returns list of wifi clients from environment definition
+
+        :rtype: list
+        """
+        try:
+            clients = self.env["environment_def"]["board"]["wifi_clients"]
+        except (KeyError, AttributeError):
+            return list()
+        return clients
+
+    def has_lan_advertise_identity(self, idx):
+        """Return lan identity value defined in lan_clients of env else return False.
+
+        :idx: lan client index from lan_clients to return corresponding value
+        :type: integer
+
+        :return: possible values are True/False
+        :rtype: boolean
+        """
+
+        try:
+            return self.env["environment_def"]["board"]["lan_clients"][idx][
+                "advertise_identity"
+            ]
+        except (KeyError, AttributeError):
+            return False
+
+    def get_mitm_devices(self):
+        """
+        returns list of mitm'ed devices of the desired environment.
+        """
+        try:
+            devices = self.env["environment_def"]["mitm"]
+        except (KeyError, AttributeError):
+            return list()
+        return devices
+
+    def mitm_enabled(self):
+        """Flag to see if we have any devices mitm'ed
+
+        :return: True if at least 1 device mitm'ed, False otherwise
+        """
+        return bool(self.get_mitm_devices())
+
+    def get_tr069_provisioning(self):
+        """Return list of ACS APIs to be executed during tr069 provisioning.
+
+        :return: object containing list ACS APIs to call for provisioning
+        :rtype: dictionary
+        """
+
+        try:
+            return self.env["environment_def"]["tr-069"]["provisioning"]
+        except (KeyError, AttributeError):
+            return False
+
+    def get_dns_dict(self):
+        """Returns the dict of reachable and unreachable IP address from DNS.
+
+        :return: number of reachable and unreachable IP address to be fetched from DNS
+        :rtype: dictionary
+        """
+        try:
+            return self.env["environment_def"]["DNS"]
+        except (KeyError, AttributeError):
+            return False
+
+    def get_board_sku(self):
+        """Returns the ["environment_def"]["board"]["SKU"] value
+        :return: SKU values from eval list
+        :rtype: String"""
+
+        try:
+            return self.env["environment_def"]["board"]["SKU"]
+        except (KeyError, AttributeError):
+            raise BftEnvExcKeyError
+
+    def has_board_sku(self):
+        """Returns True if  ["environment_def"]["board"]["SKU"] exists
+        :return: possible values are True/False
+        :rtype: bool"""
+        try:
+            self.get_board_sku()
+            return True
+        except BftEnvExcKeyError:
+            return False
+
+    def is_production_image(self):
+        return (
+            self.env["environment_def"]["board"]["software"]["image_uri"].find("NOSH")
+            != -1
+        )
+
+    def dhcp_options(self):
+        """Returns the ["environment_def"]["provisioner"]["options"].
+
+        :return:  return list of DHCPv4 and DHCPv6 option
+        :rtype: dict
+        """
+        try:
+            return self.env["environment_def"]["provisioner"]["options"]
+        except (KeyError, AttributeError):
+            return dict()
+
+    def vendor_encap_opts(self, ip_proto=None):
+        """Check vendor specific option for ACS URL is specified in env
+
+        :return: return True if dhcp option for acs url is configured
+        :rtype: bool
+        """
+        dhcp_options = self.dhcp_options()
+        if ip_proto == "ipv4" and 125 in dhcp_options.get("dhcpv4", []):
+            return True
+        elif ip_proto == "ipv6" and 17 in dhcp_options.get("dhcpv6", []):
+            return True
+        return False
+
+    def get_board_boot_file_mta(self):
+        """Returns the ["environment_def"]["board"]["emta"]["boot_file_mta"] value
+        :return: the emta boot file value as a string
+        :rtype: String"""
+        try:
+            return self.env["environment_def"]["board"]["emta"]["boot_file_mta"]
+        except (KeyError, AttributeError):
+            raise BftEnvExcKeyError
+
+    def has_board_boot_file_mta(self):
+        """Returns True if  ["environment_def"]["board"]["emta"]["boot_file_mta"] exists
+        :return: possible values are True/False
+        :rtype: bool"""
+        try:
+            self.get_board_boot_file_mta()
+            return True
+        except BftEnvExcKeyError:
+            return False
+
+    def get_external_voip(self):
+        """Return the ["environment_def"]["voice"]["EXT_VOIP"] value
+
+        :return: External VoIP entries
+        :rtype: list
+        """
+        try:
+            return self.env["environment_def"]["voice"]["EXT_VOIP"]
+        except (KeyError, AttributeError):
+            return False
+
+    def get_cwmp_version(self):
+        """Return the ["environment_def"]["board"]["cwmp_version"]
+
+        :return: CWMP version of DUT
+        :rtype: str
+        """
+        try:
+            return self.env["environment_def"]["board"]["cwmp_version"]
+        except (KeyError, AttributeError):
+            return False
+
+    def get_board_model(self) -> str:
+        """Return the ["environment_def"]["board"]["model"]
+
+        :return: Board model
+        """
+        try:
+            return self.env["environment_def"]["board"]["model"]
+        except (KeyError, AttributeError):
+            raise BftEnvExcKeyError("Unable to find board.model entry in env.")
+
+    def get_dhcpv4_options(self) -> Dict[Optional[str], Optional[Union[str, int]]]:
+        """Return Dict of dhcpv4 options from environment definition
+
+        :return: List of dhcpv4 options
+        :rtype: Dict[Optional[str], Optional[Union[str, int]]]
+        """
+        dhcp_options = (
+            self.env.get("environment_def", {})
+            .get("provisioner", {})
+            .get("options", {})
+            .get("dhcpv4", {})
+        )
+
+        return dict(ChainMap(*dhcp_options))
+
+    def is_route_gateway_valid(self) -> bool:
+        """check if valid dhcp gateways ip configurations should be deployed
+
+        :return: return True if dhcp option route_gateway is valid else False
+        :rtype: bool
+        """
+        return self.get_dhcpv4_options().get("route_gateway") != "invalid"
+
+    def get_mta_config(self):
+        """Returns the ["environment_def"]["voice"]["mta_config_boot"]["snmp_mibs"] values
+        :return: the vendor specific mta dict
+        :rtype: list, bool if Key/Attribure Error"""
+
+        try:
+            return self.env["environment_def"]["voice"]["mta_config_boot"]["snmp_mibs"]
+        except (KeyError, AttributeError):
+            return False
+
+    def get_emta_config_template(self):
+        """Return the ["environment_def"]["board"]["emta"]["config_template"] value
+        :return: emta config template ex: "CH_Compal"
+        :rtype: string
+        """
+        try:
+            return self.env["environment_def"]["board"]["emta"]["config_template"]
+        except (KeyError, AttributeError):
+            return False
+
+    def get_emta_interface_status(self):
+        """Return the ["environment_def"]["board"]["emta"]["interface_status"] value
+        :return: emta interface status ex: "down"
+        :rtype: string
+        """
+        try:
+            return self.env["environment_def"]["board"]["emta"]["interface_status"]
+        except (KeyError, AttributeError):
+            return False
+
+    def get_lan_client_options(self) -> List[Dict]:
+        """get lan client options
+
+        :return: client options for all lan client
+        :rtype: dict
+        """
+        return (
+            self.env.get("environment_def", {}).get("board", {}).get("lan_clients", {})
+        )
+
+    def is_set_static_ipv4(self, idx) -> bool:
+        """check if static ipv4 assignment is enabled
+
+        :return: return True if dhcpv4 option static_ipv4 is true else False
+        :rtype: bool
+        """
+        try:
+            return self.get_lan_client_options()[idx].get("static_ipv4")
+        except (IndexError, KeyError, AttributeError):
+            return False
