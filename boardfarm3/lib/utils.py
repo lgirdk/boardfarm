34e8dd74f7bc224b2476582c62ee82f71bbcd0f9
Michele Gualco <msalvatoregualco@libertyglobal.com>
2022-10-03 19:04:32 +0200

    feat: add retry_on_exception
    Signed-off-by: Michele Gualco <msalvatoregualco@libertyglobal.com>
    Change-Id: I49971f6dd92e6c1306bcd3bd76ecb17ebdefd78e
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm/+/17354
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm3/lib/utils.py
+++ boardfarm3/lib/utils.py
@@ -1,11 +1,14 @@
 """Boardfarm common utilities module."""
 
+import logging
 import os
 import time
 from ipaddress import IPv4Address
 from typing import Any, Callable
 
 from netaddr import EUI, mac_unix_expanded
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def get_nth_mac_address(mac_address: str, nth_number: int) -> str:
@@ -37,9 +40,9 @@ def ip_pool_to_list(start_ip: IPv4Address, end_ip: IPv4Address) -> list[IPv4Addr
     :type start_ip: IPv4Address
     :param end_ip: last ip of the pool
     :type end_ip: IPv4Address
-    :return: list of ip address based on min ip address and maximum
-     ip address of the pool
-    :rtype: List[IPv4Address]
+    :return: list of ip addresses based on min ip address and maximum
+             ip address of the pool
+    :rtype: list[IPv4Address]
     """
     ip_list = []
     while end_ip >= start_ip and start_ip != end_ip + 1:
@@ -48,19 +51,19 @@ def ip_pool_to_list(start_ip: IPv4Address, end_ip: IPv4Address) -> list[IPv4Addr
     return ip_list
 
 
-def retry(func_name: Callable, max_retry: int, *args: str) -> Any:
+def retry(func_name: Callable, max_retry: int, *args: str) -> Any:  # type: ignore
     """Retry a function if the output of the function is false.
 
     TODO: consider replacing this with Tenacity or other solutions.
 
     :param func_name: name of the function to retry
-    :type func_name: Object
+    :type func_name: Callable
     :param max_retry: Maximum number of times to be retried
-    :type max_retry: Integer
+    :type max_retry: int
     :param args: Arguments passed to the function
     :type args: args
     :return: Output of the function if function is True
-    :rtype: Boolean (True/False) or None Type(None)
+    :rtype: Any
     """
     output = None
     for _ in range(max_retry):
@@ -69,3 +72,32 @@ def retry(func_name: Callable, max_retry: int, *args: str) -> Any:
             return output
         time.sleep(5)
     return output
+
+
+def retry_on_exception(
+    method: Callable, args: list, retries: int = 10, tout: int = 5  # type: ignore
+) -> Any:
+    """Retry a method if any exception occurs.
+
+    Eventually, at last, throw the exception.
+    NOTE: args must be a tuple, hence a 1 arg tuple is (<arg>,)
+
+    :param method: name of the function to retry
+    :type method: Callable
+    :param args: Arguments passed to the function
+    :type args: list
+    :param retries: Maximum number of retries when a exception occur,defaults
+                    to 10. When negative, no retries are made.
+    :type retries: int
+    :param tout: Sleep time after every exception occur, defaults to 5
+    :type tout: int
+    :return: Output of the function
+    :rtype: Any
+    """
+    for re_try in range(retries):
+        try:
+            return method(*args)
+        except Exception as exc:  # pylint: disable=broad-except
+            _LOGGER.debug("method failed %d time (%s)", (re_try + 1), exc)
+            time.sleep(tout)
+    return method(*args)
