405716440cc8539cf3217ee73ecbdf92b7ba8057
asinghal.contractor <aysinghal.contractor@libertyglobal.com>
2022-12-28 14:31:11 +0000

    feat(lib:boardfarm_config.py): remove wifi devices with incorrect config
    Boardfarm-2610
    
    Change-Id: Ie4a9713568573d2c131d9428ed6d7ad1f43c316d
    Signed-off-by: asinghal.contractor <aysinghal.contractor@libertyglobal.com>
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm/+/18613
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Musthafa Kariyaparambil <mkariyaparambil.contractor@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm3/lib/boardfarm_config.py
+++ boardfarm3/lib/boardfarm_config.py
@@ -6,9 +6,9 @@ from pathlib import Path
 from typing import Any, cast
 
 import requests
-from nested_lookup import nested_lookup
 
 from boardfarm3.exceptions import EnvConfigError
+from boardfarm3.lib.utils import get_value_from_dict
 
 
 class BoardfarmConfig:
@@ -98,14 +98,53 @@ class BoardfarmConfig:
             ) from e
 
 
-def _merge_with_wifi_config(device: Any, wifi_config: Any) -> Any:
-    if device.get("type") == "debian_wifi" and wifi_config:
-        w_config = wifi_config
-        for wifi in w_config:
-            if device["band"] in [wifi["band"], "dual"]:
-                wifi_config.remove(wifi)
-                return wifi, wifi_config
-    return {}, wifi_config
+def _merge_with_wifi_config(
+    wifi_devices: list[dict[str, Any]], env_json_config: dict[str, Any]
+) -> list[dict[str, Any]]:
+    wifi_clients: list[dict[str, Any]] = get_value_from_dict(
+        "wifi_clients", env_json_config
+    )
+    if wifi_clients is None:
+        return []
+    if len(wifi_devices) < len(wifi_clients):
+        raise EnvConfigError(
+            f"Inventory config doesn't have {len(wifi_clients)} wifi clients"
+            " requested by env config"
+        )
+    merged_wifi_devices: list[dict[str, Any]] = []
+    wifi_devices_copy = deepcopy(wifi_devices)
+    wifi_clients = sorted(wifi_clients, key=lambda x: x.get("band"))
+    wifi_devices_copy = sorted(wifi_devices_copy, key=lambda x: x.get("band"))
+    for wifi_client in wifi_clients:
+        for wifi_device in wifi_devices_copy:
+            if wifi_device.get("band") in {wifi_client.get("band"), "dual"}:
+                merged_wifi_devices.append(wifi_device | wifi_client)
+                wifi_devices_copy.remove(wifi_device)
+                break
+        else:
+            raise EnvConfigError(
+                f"Unable to find a wifi device for {wifi_client}"
+                " env config wifi client in inventory config"
+            )
+    return merged_wifi_devices
+
+
+def _merge_with_lan_config(
+    lan_devices: list[dict[str, Any]], env_json_config: dict[str, Any]
+) -> list[dict[str, Any]]:
+    lan_clients: list[dict[str, str]] = get_value_from_dict(
+        "lan_clients", env_json_config
+    )
+    if lan_clients is None:
+        return []
+    if len(lan_devices) < len(lan_clients):
+        raise EnvConfigError(
+            f"Inventory config doesn't have {len(lan_clients)} lan clients"
+            " requested by env config"
+        )
+    return [
+        lan_devices[index] | lan_client for index, lan_client in enumerate(lan_clients)
+    ]
 
 
 def _get_json(resource_name: str) -> dict[str, Any]:
@@ -117,7 +156,7 @@ def _get_json(resource_name: str) -> dict[str, Any]:
     return cast(dict[str, Any], json.loads(json_dict))
 
 
-def parse_boardfarm_config(
+def parse_boardfarm_config(  # pylint: disable=too-many-locals
     resource_name: str, env_json_path: str, inventory_json_path: str
 ) -> BoardfarmConfig:
     """Get environment config from given json files.
@@ -145,14 +184,20 @@ def parse_boardfarm_config(
         env_devices.extend(location_config.get("devices", []))
     env_devices.append(board_config)
     environment_def = env_json_config.get("environment_def")
-    wifi_config: list = nested_lookup("wifi_clients", env_json_config)
-    wifi_config = wifi_config[-1] if wifi_config else []
     merged_devices_config = []
-    for device in env_devices:
+    wifi_devices = [config for config in env_devices if config["type"] == "debian_wifi"]
+    lan_devices = [config for config in env_devices if config["type"] == "debian_lan"]
+    other_devices = [
+        device for device in env_devices if device not in wifi_devices + lan_devices
+    ]
+    if wifi_devices:
+        merged_devices_config += _merge_with_wifi_config(wifi_devices, env_json_config)
+    if lan_devices:
+        merged_devices_config += _merge_with_lan_config(lan_devices, env_json_config)
+    for device in other_devices:
         if device.get("name") in environment_def:
             device = environment_def[device.get("name")] | device
-        wifi, wifi_config = _merge_with_wifi_config(device, wifi_config)
-        merged_devices_config.append(device | wifi)
+        merged_devices_config.append(device)
     return BoardfarmConfig(
         merged_devices_config, env_json_config_copy, inventory_config_copy
     )
