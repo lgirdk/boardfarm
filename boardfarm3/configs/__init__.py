15959fc0f9330b330b9127a4432cc8427f158527
Musthafa Kariyaparambil <musthafakdml@gmail.com>
2023-01-23 18:01:49 +0100

    build(mibs): move generic mibs to the boardfarm3 package
    Change-Id: I565303b73a2e4b473188ec262bc97157c77d48bc
    Signed-off-by: Musthafa Kariyaparambil <musthafakdml@gmail.com>
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm/+/18922
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

--- boardfarm3/configs/__init__.py
+++ boardfarm3/configs/__init__.py
@@ -7,6 +7,4 @@ LOGGING_CONFIG = json.loads(
     (Path(__file__).parent / "logging.json").read_text(encoding="utf-8")
 )
 
-GENERIC_DEVICE_MIBS_PATH = str(
-    (Path(__file__).parent / ".." / ".." / "resources" / "mibs").resolve()
-)
+GENERIC_DEVICE_MIBS_PATH = str((Path(__file__).parent / "mibs").resolve())
