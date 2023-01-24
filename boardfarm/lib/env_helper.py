3687c594d2327882e599fc0f4815a1ec5d54bf88
Nathuriga Sevvel <nsevvel.contractor@libertyglobal.com>
2023-01-24 16:11:59 +0100

    feat(env_helper.py): add support to latest env version
    Change-Id: Iaee505c3ea4c90073de6f63e6662a261123326c9
    Signed-off-by: Nathuriga Sevvel <nsevvel.contractor@libertyglobal.com>
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm/+/18938
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>
    Tested-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm/lib/env_helper.py
+++ boardfarm/lib/env_helper.py
@@ -62,6 +62,12 @@ class EnvHelper:
             "2.22",
             "2.23",
             "2.24",
+            "2.25",
+            "2.26",
+            "2.27",
+            "2.28",
+            "2.29",
+            "2.30",
         ], "Unknown environment version!"
         self.env = env
         self.mirror = ""
