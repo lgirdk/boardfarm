94143f5269f4c9e6ced330c14919f49b116bd51a
Filippo Carra <fcarra@libertyglobal.com>
2023-05-01 14:10:06 +0200

    docs(hookspecs/devices.py): implement review comments
    implemented review comments on top of change #448. This allows the changes to go in before the owner of the changeset comes back from holidays.
    
    BOARDFARM-3243
    
    Change-Id: I07d366b6169caa2ce2d609286706fb1a08688de3
    Signed-off-by: Filippo Carra <fcarra@libertyglobal.com>
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm/+/464
    Reviewed-by: Nathuriga Sevvel <nsevvel.contractor@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>

--- boardfarm3/plugins/hookspecs/devices.py
+++ boardfarm3/plugins/hookspecs/devices.py
@@ -1,29 +1,36 @@
 """
 Boardfarm device hook specifications.
 
+A Boardfarm device should belong to one of the following categories:
+
+    1. server, i.e. DHCP, ACS...
+    2. device, i.e. a connectivity CPE
+    3. attached device, i.e. a LAN client
+
+
 Every device should fulfill the following responsibilities at
 different stages of the deployment.
 
 Hook responsibilities:
 
     1. skip boot
-        - devices can be interacted with as it is without making
-          any changes to them
+        - device can be interacted with as it is without making
+          any changes to it
 
     2. boot
         - device is running with required software
-        - device can be interacted with, Eg: via console
-        - device has a management ip or direct console access
+        - device can be interacted with, e.g. via console
+        - device has a management IP address or direct console access
           if the device has a console
 
     3. configure
-        - all the points in boot
+        - all of the points from boot
         - user driven configurations are applied to the device
-        - device has service ip address if applicable
-            Eg: A CPE device has access management ip,
-                erouter ip except in disabled mode, emta ip
-        - for wifi clients, no connection to the wifi network
-          is made and no ip on service interface
+        - device has a service IP address if applicable
+            Eg: A CPE device has access network IP address,
+                eRouter IP, except in disabled mode, eMTA IP
+        - for Wi-Fi clients, no connection to the Wi-Fi network
+          is made and no IP address on service interface is assigned
 """
 
 from argparse import Namespace
@@ -43,7 +50,7 @@ from boardfarm3.lib.device_manager import DeviceManager
 def boardfarm_register_devices(
     config: BoardfarmConfig, cmdline_args: Namespace, plugin_manager: PluginManager
 ) -> DeviceManager:
-    """Register devices to plugin manager.
+    """Register device to plugin manager.
 
     This hook is responsible to register devices to the device manager after
     initialization based on the given inventory and environment config.
@@ -64,8 +71,9 @@ def boardfarm_add_devices() -> dict[str, type[BoardfarmDevice]]:
     """Add devices to known devices list.
 
     This hook is used to let boardfarm know the devices which are configured
-    in the inventory config. Each repo with boardfarm devices should implement
-    this hook to all them to the know devices list.
+    in the inventory config.
+    Each repo with a boardfarm device should implement this hook to add each
+    devices to the list of known devices.
 
     :return: dictionary with device name and class
     :rtype: dict[str, type[BoardfarmDevice]]
@@ -79,7 +87,8 @@ def validate_device_requirements(
     """Validate device requirements.
 
     This hook is responsible to validate the requirements of a device before
-    deploying devices to the environment. This allow us to fail the deployment early.
+    deploying devices to the environment.
+    This allows us to fail the deployment early.
 
     :param config: boardfarm config instance
     :type config: BoardfarmConfig
@@ -96,8 +105,8 @@ def boardfarm_server_boot(
 ) -> None:
     """Boot boardfarm server device.
 
-    This hook can be used to boot a device which are not dependent on other devices
-    in the environment. Eg: WAN and CMTS
+    This hook should be used to boot a device which is not dependent on other
+    devices in the environment. E.g. WAN and CMTS.
 
     :param config: boardfarm config instance
     :type config: BoardfarmConfig
@@ -114,8 +123,8 @@ def boardfarm_server_configure(
 ) -> None:
     """Configure boardfarm server device.
 
-    This hook can be used to configure a device after boot which are not dependent
-    on other devices in the environment. Eg: WAN and CMTS
+    This hook should be used to configure a device, after having it booted,
+    which is not dependent on other devices in the environment. E.g. WAN and CMTS
 
     :param config: boardfarm config instance
     :type config: BoardfarmConfig
@@ -132,8 +141,8 @@ def boardfarm_device_boot(
 ) -> None:
     """Boot boardfarm device.
 
-    This hook can be used to boot a device which is dependent on one or more servers
-    in the environment. Eg: CPE
+    This hook should be used to boot a device which is dependent on one or more
+    servers in the environment. E.g. CPE.
 
     :param config: boardfarm config instance
     :type config: BoardfarmConfig
@@ -150,8 +159,8 @@ def boardfarm_device_configure(
 ) -> None:
     """Configure boardfarm device.
 
-    This hook can be used to configure a device after boot which is dependent on
-    one or more servers in the environment. Eg: CPE
+    This hook should be used to configure a device, after having it booted,
+    which is dependent on one or more servers in the environment. E.g. CPE.
 
     :param config: boardfarm config instance
     :type config: BoardfarmConfig
@@ -168,8 +177,8 @@ def boardfarm_attached_device_boot(
 ) -> None:
     """Boot boardfarm attached device.
 
-    This hook can be used to boot a device which is attached to a device
-    in the environment. Eg: LAN
+    This hook should be used to boot a device which is attached to a device
+    in the environment. E.g. LAN.
 
     :param config: boardfarm config instance
     :type config: BoardfarmConfig
@@ -186,8 +195,8 @@ def boardfarm_attached_device_configure(
 ) -> None:
     """Configure boardfarm attached device.
 
-    This hook can be used to configure a device after boot which is attached to a device
-    in the environment. Eg: LAN
+    This hook should be used to configure a device, after having it booted,
+    which is attached to a device in the environment. E.g. LAN.
 
     :param config: boardfarm config instance
     :type config: BoardfarmConfig
@@ -202,8 +211,9 @@ def boardfarm_attached_device_configure(
 def contingency_check(env_req: dict[str, Any], device_manager: DeviceManager) -> None:
     """Perform contingency check to make sure the device is working fine before use.
 
-    This hook can be used by any device. It is used by boardfarm pytest plugin to make
-    sure the devices are in good condition before running each test.
+    This hook could be used by any device.
+    It is used by the pytest-boardfarm plugin to make sure the device is in
+    good condition before running each test.
 
     :param env_req: environment request dictionary
     :type env_req: dict[str, Any]
@@ -216,6 +226,7 @@ def contingency_check(env_req: dict[str, Any], device_manager: DeviceManager) ->
 def boardfarm_shutdown_device() -> None:
     """Shutdown boardfarm device after use.
 
-    This hook can be used by a device to perform clean shutdown of a device after
-    releasing all the resources (Eg: open ssh connection) before boardfarm shutdown.
+    This hook should be used by a device to perform a clean shutdown of a device
+    after releasing all the resources (e.g. close all of the open ssh connections)
+    before the shutdown of the framework.
     """
