b521ec11ae912d2915c4bb7f68985486efce3be8
vsubbaram <vignesh.s1@tataelxsi.co.in>
2023-06-30 14:46:28 +0200

    feat(boardfarm3/templates/tftp.py): add restart_lighttpd_and_serve_image and stop_lighttpd methods to tftp device template
    BOARDFARM-3740
    
    Change-Id: I1704ca09ec986de5554266813e662a72db0f5a18
    Signed-off-by: vsubbaram <vignesh.s1@tataelxsi.co.in>
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm/+/1606
    Reviewed-by: Musthafa Kariyaparambil <mkariyaparambil.contractor@libertyglobal.com>
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>

--- boardfarm3/templates/tftp.py
+++ boardfarm3/templates/tftp.py
@@ -1,6 +1,9 @@
 """Boardfarm TFTP device template."""
 
 from abc import ABC, abstractmethod
+from collections.abc import Generator
+from contextlib import contextmanager
+from ipaddress import IPv4Address
 
 
 class TFTP(ABC):
@@ -20,5 +23,27 @@ class TFTP(ABC):
         """Get eth interface ipv4 address.
 
         :returns: IPv4 address of eth interface
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def restart_lighttpd(self) -> None:
+        """Restart lighttpd service."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def stop_lighttpd(self) -> None:
+        """Stop the lighttpd service."""
+        raise NotImplementedError
+
+    @abstractmethod
+    @contextmanager
+    def set_static_ip(self, static_address: IPv4Address) -> Generator[None, None, None]:
+        """Temporarily set a static IPv4 on the DUT connected iface via the `ip` cmd.
+
+        :param static_address: Static IPv4 address to be set
+        :type static_address: IPv4Address
+        :yield: The DUT connected interface with the static ip address applied
+        :rtype: Generator[None, None, None]
         """
         raise NotImplementedError
