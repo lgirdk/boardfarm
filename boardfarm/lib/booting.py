d307eea897a6d5784c7c9d42ddb12e30046ee52c
ahazra.ext <ahazra.ext@libertyglobal.com>
2024-02-09 17:01:36 +0530

    fix(lib/booting.py): update extra configure voice param
    update the exra parameter for configure_voice which is changed in the below change ID
    
    Depends-on: I962bb850db9def96ca345795d67a721911f0c904
    
    BOARDFARM-4443
    
    Change-Id: Ia5ec4bb075108bef1daa1bea82a79694e82cffdf
    Signed-off-by: ahazra.ext <ahazra.ext@libertyglobal.com>
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm/+/5028
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    Tested-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    (cherry picked from commit 688d4d8ae07251e737da1d89833938a8f39b7678)
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm/+/4279

--- boardfarm/lib/booting.py
+++ boardfarm/lib/booting.py
@@ -284,10 +284,7 @@ def post_boot_env(config, env_helper, devices):
             {"1": devices.fxs1.own_number, "2": devices.fxs2.own_number},
             "1234",
             devices.sipcenter.dns.url,
-            {
-                "1": devices.softphone.own_number,
-                "2": devices.softphone2.own_number,
-            },
+            "xxxx",  # TODO: this should come from the env.json
         )
         for fxs in devices.get_device_array("FXS"):
             if devices.sipcenter.fxs_endpoint_transport == "ipv6":
