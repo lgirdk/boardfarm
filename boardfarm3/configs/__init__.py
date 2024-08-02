0db41c4089e06177f4c6bdf954fac50a05019e22
Ketan Tewari <ktewari@libertyglobal.com>
2024-08-03 00:21:42 +0200

    fix(config): update to a generic openwrt board
    Deployment as per double hop scenarion on raikou-net
    
    Change-Id: I7500076fa37e2816fe42e6f72255c666de3e35dc
    Signed-off-by: Ketan Tewari <ktewari@libertyglobal.com>
    Reviewed-on: http://10.64.38.13:8080/gerrit/c/github/boardfarm/+/8069
    Tested-by: Jenkins Automation <jenkins@libertyglobal.com>
    Reviewed-by: Ketan Tewari <ktewari.contractor@libertyglobal.com>

--- boardfarm3/configs/__init__.py
+++ boardfarm3/configs/__init__.py
@@ -8,3 +8,14 @@ LOGGING_CONFIG = json.loads(
 )
 
 GENERIC_DEVICE_MIBS_PATH = str((Path(__file__).parent / "mibs").resolve())
+
+KIA_DHCP_IPV4_CONFIG_TEMPLATE = json.loads(
+    (Path(__file__).parent / "kea_eth_provisioner4.conf").read_text(
+        encoding="utf-8",
+    ),
+)
+KIA_DHCP_IPV6_CONFIG_TEMPLATE = json.loads(
+    (Path(__file__).parent / "kea_eth_provisioner6.conf").read_text(
+        encoding="utf-8",
+    ),
+)
