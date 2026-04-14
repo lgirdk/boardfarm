6e76a4420892d5e658164f3827e44ad0efbab4b9
Saurav Kochar <skochar.contractor@libertyglobal.com>
2026-04-14 15:27:03 +0200

    feat(ntu.py): define templates for ntu device class
    Change-Id: I2146dc468a45e8ccace892cd5dbb39b75627d1d4
    Signed-off-by: Saurav Kochar <skochar.contractor@libertyglobal.com>
    Reviewed-on: http://gerrit.cicd.boardfarm.upclabs.com/c/github/boardfarm/+/4771
    Tested-by: Jenkins Automation
    Reviewed-by: Michele Gualco <mgualco.contractor@libertyglobal.com>
    Reviewed-on: http://gerrit.cicd.boardfarm.upclabs.com/c/github/boardfarm/+/4894
    Tested-by: Michele Gualco <mgualco.contractor@libertyglobal.com>

---
+++ boardfarm3/templates/ntu/__init__.py
@@ -0,0 +1,11 @@
+"""Boardfarm LGI shared NTU templates."""
+
+from boardfarm3.templates.cpe.cpe_hw import CPEHW
+from boardfarm3.templates.cpe.cpe_sw import CPESW
+from boardfarm3.templates.ntu.ntu import NTU
+
+__all__ = [
+    "CPEHW",
+    "CPESW",
+    "NTU",
+]
